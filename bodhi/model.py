c9833f3e239e4d3e5b9f1e6995b0d7c6ba5a071c
lmacken@publictest2.fedora.phx.redhat.com <>
2007-03-18 19:21:29 -0700

    - Point build_dir at Koji - Move FAS specific options into dev-fas.cfg - Add the identity tables back because we need them for our own identity.

--- bodhi/model.py
+++ bodhi/model.py
@@ -29,7 +29,8 @@ log = logging.getLogger(__name__)
 hub = PackageHub("bodhi")
 __connection__ = hub
 
-soClasses=('Release', 'Arch', 'Multilib', 'Package', 'PackageUpdate', 'CVE', 'Bugzilla')
+soClasses=('Release', 'Arch', 'Multilib', 'Package', 'PackageUpdate', 'CVE',
+           'Bugzilla', 'Visit', 'VisitIdentity', 'User', 'Group', 'Permission')
 
 class Release(SQLObject):
     """ Table of releases that we will be pushing updates for """
@@ -308,3 +309,75 @@ class Bugzilla(SQLObject):
 
     def get_url(self):
         return "https://bugzilla.redhat.com/bugzilla/show_bug.cgi?id=%s" % self.bz_id
+
+##
+## Identity tables
+##
+
+class Visit(SQLObject):
+    visit_key = StringCol(length=40, alternateID=True,
+                          alternateMethodName="by_visit_key")
+    created = DateTimeCol(default=datetime.now)
+    expiry = DateTimeCol()
+
+    def lookup_visit(cls, visit_key):
+        try:
+            return cls.by_visit_key(visit_key)
+        except SQLObjectNotFound:
+            return None
+    lookup_visit = classmethod(lookup_visit)
+
+class VisitIdentity(SQLObject):
+    visit_key = StringCol(length=40, alternateID=True,
+                          alternateMethodName="by_visit_key")
+    user_id = IntCol()
+
+class Group(SQLObject):
+    class sqlmeta:
+        table = "tg_group"
+
+    group_name = UnicodeCol(length=16, alternateID=True,
+                            alternateMethodName="by_group_name")
+    display_name = UnicodeCol(length=255)
+    created = DateTimeCol(default=datetime.now)
+    users = RelatedJoin("User", intermediateTable="user_group",
+                        joinColumn="group_id", otherColumn="user_id")
+    permissions = RelatedJoin("Permission", joinColumn="group_id", 
+                              intermediateTable="group_permission",
+                              otherColumn="permission_id")
+
+class User(SQLObject):
+    class sqlmeta:
+        table = "tg_user"
+
+    user_name = UnicodeCol(length=16, alternateID=True,
+                           alternateMethodName="by_user_name")
+    password = UnicodeCol(length=40, default=None)
+    groups = RelatedJoin("Group", intermediateTable="user_group",
+                         joinColumn="user_id", otherColumn="group_id")
+    created = DateTimeCol(default=datetime.now)
+
+    def _get_permissions(self):
+        perms = set()
+        for g in self.groups:
+            perms = perms | set(g.permissions)
+        return perms
+
+    def _set_password(self, cleartext_password):
+        "Runs cleartext_password through the hash algorithm before saving."
+        hash = identity.encrypt_password(cleartext_password)
+        self._SO_set_password(hash)
+
+    def set_password_raw(self, password):
+        "Saves the password as-is to the database."
+        self._SO_set_password(password)
+
+class Permission(SQLObject):
+    permission_name = UnicodeCol(length=16, alternateID=True,
+                                 alternateMethodName="by_permission_name")
+    description = UnicodeCol(length=255)
+
+    groups = RelatedJoin("Group",
+                         intermediateTable="group_permission",
+                         joinColumn="permission_id", 
+                         otherColumn="group_id")
