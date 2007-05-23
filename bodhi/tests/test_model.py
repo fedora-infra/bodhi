37553eebb1d76567d74b39cd5528570717f3d920
Luke Macken <lmacken@fedoraproject.org>
2007-05-23 12:24:48 -0400

    Added verification of updateinfo, moving, and email notifications in our test cases

--- bodhi/tests/test_model.py
+++ bodhi/tests/test_model.py
@@ -130,27 +130,15 @@ class TestPackageUpdate(testutil.DBTest):
                     assert exists(join(repo, arch, filename))
             assert exists(join(repo, arch, 'repodata', 'updateinfo.xml.gz'))
 
-    def test_push(self):
-        from init import init_updates_stage
-        push_stage = tempfile.mkdtemp('bodhi')
-        update = self.get_update()
-        init_updates_stage(stage_dir=push_stage)
-
-        update.request = 'push'
-        print "Pushing to temp stage: %s" % push_stage
-        for msg in update.run_request(stage=push_stage):
-            pass
-
-        # Verify that all files got pushed
-        repo = join(push_stage, update.get_repo())
-        self._assert_files(update, repo)
-
-        # Verify the updateinfo.xml.gz
+    def _verify_updateinfo(self, update, repo):
+        """ Verify that the updateinfo.xml.gz for a given repo matches the
+            data for a given update
+        """
+        print "_verify_updateinfo(%s, %s)" % (update.nvr, repo)
         uinfo = UpdateMetadata()
-        uinfo.add(join(repo, 'i386', 'repodata', 'updateinfo.xml.gz'))
+        uinfo.add(repo)
         notice = uinfo.get_notice(update.nvr)
-        from pprint import pprint
-        pprint(notice._md)
+        assert notice
         assert notice['from'] == 'updates@fedora.redhat.com'
         assert notice['title'] == update.nvr
         assert notice['release'] == update.release.long_name
@@ -174,22 +162,69 @@ class TestPackageUpdate(testutil.DBTest):
                         break
                 assert found
 
-        # TODO: generate metadata! and verify (repoquery ?)
+    def test_push(self):
+        from init import init_updates_stage
+        push_stage = tempfile.mkdtemp('bodhi')
+        update = self.get_update()
+        init_updates_stage(stage_dir=push_stage)
 
-        print "Moving update"
-        update.request = 'move'
+        update.request = 'push'
         print "Pushing to temp stage: %s" % push_stage
         for msg in update.run_request(stage=push_stage):
             pass
 
-        # o check both updateinfo.xml.gz
-        # o make sure old files are gone and new ones exist
-        # o Try another update with broken upgrade paths
+        # Verify that all files got pushed
+        repo = join(push_stage, update.get_repo())
+        self._assert_files(update, repo)
 
-        print "Push stage = ", push_stage
-        #assert False
+        # Generate repo metadata
+        from bodhi.push import generate_metadata
+        for x in generate_metadata(update.release, update.testing,
+                                   stage=push_stage): pass
 
-        shutil.rmtree(push_stage)
+        # Verify the updateinfo.xml.gz
+        for arch in update.filelist.keys():
+            self._verify_updateinfo(update, join(repo, arch, 'repodata',
+                                                 'updateinfo.xml.gz'))
+
+        # Move update from Testing to Final
+        update.request = 'move'
+        print "Pushing to temp stage: %s" % push_stage
+        for msg in update.run_request(stage=push_stage): pass
+
+        # Generate repo metadata
+        from bodhi.push import generate_metadata
+        for x in generate_metadata(update.release, update.testing,
+                                   stage=push_stage): pass
+
+        # Make sure we're in a different repo now
+        newrepo = join(push_stage, update.get_repo())
+        assert newrepo != repo
+
+        # Verify the updateinfo.xml.gz
+        for arch in update.filelist.keys():
+            self._verify_updateinfo(update, join(newrepo, arch, 'repodata',
+                                                 'updateinfo.xml.gz'))
+
+        # Make sure this update has been removed from the old updateinfo
+        for arch in update.filelist.keys():
+            uinfo = UpdateMetadata()
+            uinfo.add(join(repo, arch, 'repodata', 'updateinfo.xml.gz'))
+            notice = uinfo.get_notice(update.nvr)
+            assert notice == None
+
+        # Make sure files exist at new location
+        self._assert_files(update, newrepo)
+
+        # Make sure files don't exist at old location
+        try:
+            self._assert_files(update, repo)
+        except AssertionError:
+            # At this point, everything should be kosher
+            shutil.rmtree(push_stage)
+            return
+
+        assert False
 
     def test_email(self):
         from bodhi import mail
@@ -200,8 +235,9 @@ class TestPackageUpdate(testutil.DBTest):
         update.addCVE(cve)
         update.assign_id()
         template = mail.get_template(update)
-        # TODO: assert template against known correct template
-        #assert False
+        assert template
+        assert template == u"\nSubject: [SECURITY] Fedora 7 Test Update: mutt-1.5.14-1.fc7\n\n--------------------------------------------------------------------------------\nFedora Test Update Notification\nFEDORA-2007-0001\nNone\n--------------------------------------------------------------------------------\n\nName        : mutt\nProduct     : Fedora 7\nVersion     : 1.5.14\nRelease     : 1.fc7\nSummary     : A text mode mail user agent\nDescription :\nMutt is a text-mode mail user agent. Mutt supports color, threading,\narbitrary key remapping, and a lot of customization.\n\nYou should install mutt if you have used it in the past and you prefer\nit, or if you are new to mail programs and have not decided which one\nyou are going to use.\n\n--------------------------------------------------------------------------------\nUpdate Information:\n\nfoobar\n--------------------------------------------------------------------------------\nReferences:\n\n  Bug #1 - https://bugzilla.redhat.com/bugzilla/show_bug.cgi?id=1\n  CVE-2007-0000 - http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2007-0000\n--------------------------------------------------------------------------------\n\nThis update can be downloaded from:\n    http://download.fedoraproject.org/pub/fedora/linux/core/updates/testing/7/\n\nf31e57d24a37a4616c397df221515f1f18d7cc58  ppc/mutt-1.5.14-1.fc7.ppc.rpm\n423bd720bb3c1b095bd67311046a62ccaadfcbc1  ppc/debug/mutt-debuginfo-1.5.14-1.fc7.ppc.rpm\nae07587736a31b7b666e4b09f47ebb7cc842bc8c  x86_64/mutt-1.5.14-1.fc7.x86_64.rpm\n22fa9519ad3a13710ce62b9983e0fe3dde6a9473  x86_64/debug/mutt-debuginfo-1.5.14-1.fc7.x86_64.rpm\n71140a390ab41e62e14fe1939b243afacb7369bf  i386/mutt-1.5.14-1.fc7.i386.rpm\n60e794cb7f99ce640bda6649ec4adc4e9b4f89c0  i386/debug/mutt-debuginfo-1.5.14-1.fc7.i386.rpm\nfe0820de7c5e10b1df28221f3b946892c27800f1  SRPMS/mutt-1.5.14-1.fc7.src.rpm\n\nThis update can be installed with the 'yum' update program.  Use 'yum update\npackage-name' at the command line.  For more information, refer to 'Managing\nSoftware with yum,' available at http://docs.fedoraproject.org/yum/.\n--------------------------------------------------------------------------------\n"
+
 
     def test_no_srpm(self):
         from bodhi.exceptions import RPMNotFound
