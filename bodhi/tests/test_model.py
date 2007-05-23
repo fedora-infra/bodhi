5551122e410a7acf22d8a5dcdf917c79b796b962
Luke Macken <lmacken@fedoraproject.org>
2007-05-22 20:04:01 -0400

    Robustify test_push() by verifying all pushed files and the updateinfo.xml.gz

--- bodhi/tests/test_model.py
+++ bodhi/tests/test_model.py
@@ -7,15 +7,21 @@ import shutil
 import tempfile
 import turbogears
 
-from os.path import join, exists
+from os.path import join, exists, basename
 from turbogears import testutil, database, config
 from bodhi.util import mkmetadatadir
-from bodhi.model import (Release, Package, PackageUpdate, Bugzilla,
-                         Comment, CVE, Arch)
+from bodhi.model import Release, Package, PackageUpdate, Bugzilla, CVE, Arch
+from yum.update_md import UpdateMetadata
+from sqlobject.dberrors import DuplicateEntryError
 
 database.set_db_uri("sqlite:///:memory:")
 turbogears.update_config(configfile='dev.cfg',
                          modulename='bodhi.config')
+
+
+#class TestEPEL(testutil.DBTest):
+    #def test_epel(self):
+
 
 class TestPackageUpdate(testutil.DBTest):
 
@@ -61,6 +67,14 @@ class TestPackageUpdate(testutil.DBTest):
     def get_cve(self):
         return CVE(cve_id="CVE-2007-0000")
 
+    def test_dupe(self):
+        update1 = self.get_update()
+        try:
+            update2 = self.get_update()
+        except DuplicateEntryError:
+            return
+        assert False
+
     def test_creation(self):
         update = self.get_update()
         assert update.nvr == 'mutt-1.5.14-1.fc7'
@@ -103,24 +117,78 @@ class TestPackageUpdate(testutil.DBTest):
         assert join(pkg_dir, update.package.name, '1.5.14', '1.fc7', 'src',
                     '%s.src.rpm' % update.nvr) in update.filelist['SRPMS']
 
+    def _assert_files(self, update, repo):
+        """ Make sure all of the files for this update exist """
+        for (arch, files) in update.filelist.items():
+            for file in files:
+                filename = basename(file)
+                if filename.find('debuginfo') != -1:
+                    assert exists(join(repo, arch, 'debug', filename))
+                elif filename.find('src.rpm') != -1:
+                    assert exists(join(repo, 'SRPMS', filename))
+                else:
+                    assert exists(join(repo, arch, filename))
+            assert exists(join(repo, arch, 'repodata', 'updateinfo.xml.gz'))
+
     def test_push(self):
+        from init import init_updates_stage
         push_stage = tempfile.mkdtemp('bodhi')
         update = self.get_update()
-        for arch in update.release.arches:
-            mkmetadatadir(join(push_stage, update.get_repo(), arch.name))
-            mkmetadatadir(join(push_stage, update.get_repo(),arch.name,'debug'))
-        mkmetadatadir(join(push_stage, update.get_repo(), 'SRPMS'))
+        init_updates_stage(stage_dir=push_stage)
+
         update.request = 'push'
         print "Pushing to temp stage: %s" % push_stage
-        for msg in update.run_request(stage=push_stage): pass
-        for arch in update.release.arches:
-            assert exists(join(push_stage, update.get_repo(), arch.name,
-                               "%s.%s.rpm" % (update.nvr, arch.name)))
-            assert exists(join(push_stage, update.get_repo(), arch.name,
-                               'debug', "mutt-debuginfo-1.5.14-1.fc7.%s.rpm" %
-                               arch.name))
-        assert exists(join(push_stage, update.get_repo(), 'SRPMS',
-                           "%s.src.rpm" % update.nvr))
+        for msg in update.run_request(stage=push_stage):
+            pass
+
+        # Verify that all files got pushed
+        repo = join(push_stage, update.get_repo())
+        self._assert_files(update, repo)
+
+        # Verify the updateinfo.xml.gz
+        uinfo = UpdateMetadata()
+        uinfo.add(join(repo, 'i386', 'repodata', 'updateinfo.xml.gz'))
+        notice = uinfo.get_notice(update.nvr)
+        from pprint import pprint
+        pprint(notice._md)
+        assert notice['from'] == 'updates@fedora.redhat.com'
+        assert notice['title'] == update.nvr
+        assert notice['release'] == update.release.long_name
+        assert notice['type'] == update.type
+        assert notice['status'] == update.testing and 'testing' or 'final'
+        assert notice['update_id'] == update.update_id
+        assert notice['issued'] == str(update.date_pushed)
+        assert notice['description'] == update.notes
+        for collection in notice['pkglist']:
+            numfiles = 0
+            for archfiles in update.filelist.values():
+                for file in archfiles:
+                    numfiles += 1
+            assert len(collection['packages']) == numfiles
+            for pkg in collection['packages']:
+                assert pkg['arch'] in update.filelist.keys()
+                found = False
+                for file in update.filelist[pkg['arch']]:
+                    if pkg['filename'] in file:
+                        found = True
+                        break
+                assert found
+
+        # TODO: generate metadata! and verify (repoquery ?)
+
+        print "Moving update"
+        update.request = 'move'
+        print "Pushing to temp stage: %s" % push_stage
+        for msg in update.run_request(stage=push_stage):
+            pass
+
+        # o check both updateinfo.xml.gz
+        # o make sure old files are gone and new ones exist
+        # o Try another update with broken upgrade paths
+
+        print "Push stage = ", push_stage
+        #assert False
+
         shutil.rmtree(push_stage)
 
     def test_email(self):
@@ -132,7 +200,7 @@ class TestPackageUpdate(testutil.DBTest):
         update.addCVE(cve)
         update.assign_id()
         template = mail.get_template(update)
-        # TODO: assert template against known correct templaet
+        # TODO: assert template against known correct template
         #assert False
 
     def test_no_srpm(self):
