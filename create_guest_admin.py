c3ab75ab21c1e13f32bd7683a1e31a543644bd99
Luke Macken <lmacken@fedoraproject.org>
2007-06-02 13:59:46 -0700

    Make create_guest_admin.py add the user to the releng group

---
+++ create_guest_admin.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from init import load_config
+from bodhi.model import User, Group
+from turbogears.database import PackageHub
+
+hub = PackageHub("bodhi")
+__connection__ = hub
+
+load_config()
+
+hub.begin()
+print "Creating guest user and giving it administrator priviliges"
+guest = User(user_name='guest')
+guest.password = 'guest'
+admin = Group(group_name='releng', display_name='Bodhi administrators')
+guest.addGroup(admin)
+hub.commit()
+print "Done!"
