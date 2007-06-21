04252641dbd53f1aa82d06421b94b4d394e18760
Luke Macken <lmacken@fedoraproject.org>
2007-06-21 03:41:38 -0400

    catch exceptions in the MashTasks

--- bodhi/masher.py
+++ bodhi/masher.py
@@ -226,21 +226,25 @@ class MashTask(Thread):
         Move all of the builds to the appropriate tag, and then run mash.  If
         anything fails, undo any tag moves.
         """
-        if self.move_builds():
-            self.success = True
-            self.mash()
-            if self.success:
-                masher.success(self)
-            else:
-                log.error("Error mashing.. skipping post-request actions")
-                if self.undo_move():
-                    log.info("Tag rollback successful!")
+        try:
+            if self.move_builds():
+                self.success = True
+                self.mash()
+                if self.success:
+                    masher.success(self)
                 else:
-                    log.error("Tag rollback failed!")
-        else:
-            log.error("Error with build moves.. rolling back")
-            self.undo_move()
-            self.success = False
+                    log.error("Error mashing.. skipping post-request actions")
+                    if self.undo_move():
+                        log.info("Tag rollback successful!")
+                    else:
+                        log.error("Tag rollback failed!")
+            else:
+                log.error("Error with build moves.. rolling back")
+                self.undo_move()
+                self.success = False
+        except Exception, e:
+            log.error("Exception thrown in MashTask %d" % self.id)
+            log.error(str(e))
         masher.done(self)
 
     def __str__(self):
