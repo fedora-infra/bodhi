03af6485fcd3a21b0e6e48d4b632c0c8ead9e0c7
Luke Macken <lmacken@fedoraproject.org>
2007-07-01 00:11:34 -0700

    Hack get_template to support multi-build updates

--- bodhi/mail.py
+++ bodhi/mail.py
@@ -18,10 +18,11 @@ import logging
 import turbomail
 
 from os.path import join
+from turbomail import MailNotEnabledException
+from turbogears import config
+
 from bodhi.util import sha1sum, rpm_fileheader
 from bodhi.exceptions import RPMNotFound
-from turbogears import config
-from turbomail import MailNotEnabledException
 
 log = logging.getLogger(__name__)
 
@@ -189,66 +190,66 @@ with yum,' available at http://docs.fedoraproject.org/yum/.
 """
 
 def get_template(update):
-    h = update.get_rpm_header()
-    line = str('-' * 80) + '\n'
-
-    ##
-    ## TODO: get this info from Koji
-    ##
-
-    info = {}
-    info['date'] = str(update.date_pushed)
-    info['name'] = h[rpm.RPMTAG_NAME]
-    info['summary'] = h[rpm.RPMTAG_SUMMARY]
-    info['version'] = h[rpm.RPMTAG_VERSION]
-    info['release'] = h[rpm.RPMTAG_RELEASE]
-    info['url']     = h[rpm.RPMTAG_URL]
-    info['testing'] = update.status == 'testing' and ' Test' or ''
-    info['subject'] = "%s%s%s Update: %s" % (
-            update.type == 'security' and '[SECURITY] ' or '',
-            update.release.long_name, info['testing'], update.title)
-    info['update_id'] = update.update_id
-    info['description'] = h[rpm.RPMTAG_DESCRIPTION]
-    #info['updatepath'] = update.get_repo()
-    info['product'] = update.release.long_name
-    info['notes'] = ""
-    if update.notes and len(update.notes):
-        info['notes'] = "Update Information:\n\n%s\n" % update.notes
-        info['notes'] += line
-
-    # Build the list of SHA1SUMs and packages
-    filelist = []
     from bodhi.buildsys import get_session
+    line = str('-' * 80) + '\n'
+    templates = []
     koji_session = get_session()
-    for pkg in koji_session.listBuildRPMs(update.title):
-        filename = "%s.%s.rpm" % (pkg['nvr'], pkg['arch'])
-        path = join(config.get('build_dir'), info['name'], info['version'],
-                    info['release'], pkg['arch'])
-        filelist.append("%s %s" % (sha1sum(join(path, filename)), filename))
-    info['filelist'] = '\n'.join(filelist)
 
-    # Add this updates referenced Bugzillas and CVEs
-    i = 1
-    info['references'] = ""
-    if len(update.bugs) or len(update.cves):
-        info['references'] = "References:\n\n"
-        for bug in update.bugs:
-            info['references'] += "  [ %d ] Bug #%d\n        %s\n" % (i,
-                    bug.bz_id, bug.get_url())
-            i += 1
-        for cve in update.cves:
-            info['references'] += "  [ %d ] %s\n        %s\n" % (i, cve.cve_id,
-                                                                  cve.get_url())
-            i += 1
-        info['references'] += line
+    for build in update.builds:
+        h = build.get_rpm_header()
+        info = {}
+        info['date'] = str(update.date_pushed)
+        info['name'] = h[rpm.RPMTAG_NAME]
+        info['summary'] = h[rpm.RPMTAG_SUMMARY]
+        info['version'] = h[rpm.RPMTAG_VERSION]
+        info['release'] = h[rpm.RPMTAG_RELEASE]
+        info['url']     = h[rpm.RPMTAG_URL]
+        info['testing'] = update.status == 'testing' and ' Test' or ''
+        info['subject'] = "%s%s%s Update: %s" % (
+                update.type == 'security' and '[SECURITY] ' or '',
+                update.release.long_name, info['testing'], update.title)
+        info['update_id'] = update.update_id
+        info['description'] = h[rpm.RPMTAG_DESCRIPTION]
+        #info['updatepath'] = update.get_repo()
+        info['product'] = update.release.long_name
+        info['notes'] = ""
+        if update.notes and len(update.notes):
+            info['notes'] = "Update Information:\n\n%s\n" % update.notes
+            info['notes'] += line
 
-    # Find the most recent update for this package, other than this one
-    lastpkg = update.get_latest()
-    log.debug("lastpkg = %s" % lastpkg)
+        # Build the list of SHA1SUMs and packages
+        filelist = []
+        for pkg in koji_session.listBuildRPMs(build.nvr):
+            filename = "%s.%s.rpm" % (pkg['nvr'], pkg['arch'])
+            path = join(config.get('build_dir'), info['name'], info['version'],
+                        info['release'], pkg['arch'])
+            filelist.append("%s %s" % (sha1sum(join(path, filename)), filename))
+        info['filelist'] = '\n'.join(filelist)
 
-    # Grab the RPM header of the previous update, and generate a ChangeLog
-    info['changelog'] = ""
-    if lastpkg:
+        # Add this updates referenced Bugzillas and CVEs
+        i = 1
+        info['references'] = ""
+        if len(update.bugs) or len(update.cves):
+            info['references'] = "References:\n\n"
+            for bug in update.bugs:
+                title = (bug.title != 'Unable to fetch title' and
+                         bug.title != 'Invalid bug number') and \
+                        ' - %s' % bug.title or ''
+                info['references'] += "  [ %d ] Bug #%d%s\n        %s\n" % \
+                                      (i, bug.bz_id, title, bug.get_url())
+                i += 1
+            for cve in update.cves:
+                info['references'] += "  [ %d ] %s\n        %s\n" % \
+                                      (i, cve.cve_id, cve.get_url())
+                i += 1
+            info['references'] += line
+
+        # Find the most recent update for this package, other than this one
+        lastpkg = build.get_latest()
+        log.debug("lastpkg = %s" % lastpkg)
+
+        # Grab the RPM header of the previous update, and generate a ChangeLog
+        info['changelog'] = ""
         try:
             oldh = rpm_fileheader(lastpkg)
             oldtime = oldh[rpm.RPMTAG_CHANGELOGTIME]
@@ -259,16 +260,18 @@ def get_template(update):
             elif len(text) != 1:
                 oldtime = oldtime[0]
             info['changelog'] = "ChangeLog:\n\n%s%s" % \
-                    (str(update.get_changelog(oldtime)), line)
+                    (str(build.get_changelog(oldtime)), line)
         except RPMNotFound:
             log.error("Cannot find 'latest' RPM for generating ChangeLog: %s" %
                       lastpkg)
 
-    for (key, value) in info.items():
-        if value:
-            info[key] = value.decode('utf8')
+        for (key, value) in info.items():
+            if value:
+                info[key] = value.decode('utf8')
 
-    return (info['subject'], errata_template % info)
+        templates.append((info['subject'], errata_template % info))
+
+    return templates
 
 def send_mail(sender, to, subject, body):
     message = turbomail.Message(sender, to, subject)
