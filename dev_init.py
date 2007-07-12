634378d7eeb866a33867b84582aa3c7608b88140
Luke Macken <lmacken@fedoraproject.org>
2007-07-11 21:18:42 -0400

    Rename create_guest_admin.py to dev_init.py, which creates a guest user and a fake update for testing/development purposes

---
+++ dev_init.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from init import load_config
+from turbogears.database import PackageHub
+from sqlobject.dberrors import DuplicateEntryError
+from bodhi.model import (User, Group, PackageUpdate, Package, Release,
+                         PackageBuild)
+
+hub = PackageHub("bodhi")
+__connection__ = hub
+
+load_config()
+
+hub.begin()
+print "Creating guest user and giving it administrator priviliges"
+try:
+    guest = User(user_name='guest')
+    guest.password = 'guest'
+    admin = Group(group_name='releng', display_name='Bodhi administrators')
+    guest.addGroup(admin)
+    hub.commit()
+except DuplicateEntryError:
+    print "guest account already created"
+
+package = Package(name='yum')
+build = PackageBuild(nvr='yum-3.2.1-1.fc7', package=package)
+update = PackageUpdate(title='yum-3.2.1-1.fc7', release=Release.select()[0],
+                       submitter='bodhi', status='pending',
+                       notes='This is a test update created by bodhi',
+                       type='enhancement')
+update.addPackageBuild(build)
+
+print "Done!"
