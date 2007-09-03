cc5916f8efc65cf94f387de03fc68e9d0d74dd9b
Luke Macken <lewk@csh.rit.edu>
2007-09-02 23:28:52 -0400

    Allow for authenticated controller test cases, and write a couple to start off with

--- bodhi/tests/test_controllers.py
+++ bodhi/tests/test_controllers.py
@@ -1,16 +1,5 @@
 # $Id: test_controllers.py,v 1.3 2006/12/31 09:10:25 lmacken Exp $
 
-#def test_method():
-#    "the index method should return a string called now"
-#    import types
-#    result = testutil.call(cherrypy.root.index)
-#    assert type(result["now"]) == types.StringType
-#
-#def test_indextitle():
-#    "The mainpage should have the right title"
-#    testutil.createRequest("/")
-#    assert "<TITLE>Welcome to TurboGears</TITLE>" in cherrypy.response.body[0]
-
 import urllib
 import cherrypy
 import turbogears
@@ -22,18 +11,23 @@ from bodhi.model import Release, PackageUpdate, User
 from bodhi.controllers import Root
 
 database.set_db_uri("sqlite:///:memory:")
-cherrypy.root = Root()
-
 turbogears.update_config(configfile='dev.cfg',
                          modulename='bodhi.config')
 
+cherrypy.root = Root()
+
 class TestControllers(testutil.DBTest):
 
-    def save_update(self, params):
+    def save_update(self, params, session={}):
         pairs = urllib.urlencode(params)
         url = '/updates/save?' + pairs
         print url
-        testutil.createRequest(url)
+        testutil.createRequest(url, headers=session)
+
+    def create_release(self):
+        rel = Release(name='fc7', long_name='Fedora 7', id_prefix='FEDORA',
+                      dist_tag='dist-fc7')
+        assert rel
 
     def login(self):
         guest = User(user_name='guest')
@@ -41,17 +35,26 @@ class TestControllers(testutil.DBTest):
         print guest
         testutil.createRequest('/updates/login?tg_format=json&login=Login&forward_url=/updates/&user_name=guest&password=guest', method='POST')
         assert cherrypy.response.status == '200 OK'
-        cookies = filter(lambda x: x[0] == 'Set-Cookie', cherrypy.response.header_list)
-        return cookies
+        cookies = filter(lambda x: x[0] == 'Set-Cookie',
+                         cherrypy.response.header_list)
+        cookiehdr = ""
+        for cookie in zip(cookies[0], cookies[1])[1]:
+            cookiehdr += cookie.split(';')[0] + '\r\n '
+        cookiehdr = cookiehdr.strip()
+        return { 'Cookie' : cookiehdr }
 
     def test_bad_password(self):
-        x = testutil.createRequest('/updates/login?tg_format=json&login=Login&forward_url=/updates/&user_name=guest&password=foo', method='POST')
+        x = testutil.createRequest('/updates/login?tg_format=json&login=Login&&user_name=guest&password=foo', method='POST')
         assert "The credentials you supplied were not correct or did not grant access to this resource." in cherrypy.response.body[0]
         assert cherrypy.response.status == '403 Forbidden'
 
+    def test_good_password(self):
+        guest = User(user_name='guest')
+        guest.password = 'guest'
+        x = testutil.createRequest('/updates/login?tg_format=json&login=Login&user_name=guest&password=guest', method='POST')
+        assert cherrypy.response.status == '200 OK'
+
     def test_unauthenticated_update(self):
-        cookies = self.login()
-        print cookies
         params = {
                 'builds'  : 'TurboGears-1.0.2.2-2.fc7',
                 'release' : 'Fedora 7',
@@ -62,3 +65,26 @@ class TestControllers(testutil.DBTest):
         }
         self.save_update(params)
         assert "You must provide your credentials before accessing this resource." in cherrypy.response.body[0]
+
+    def test_new_update(self):
+        session = self.login()
+        self.create_release()
+        params = {
+                'builds'  : 'TurboGears-1.0.2.2-2.fc7',
+                'release' : 'Fedora 7',
+                'type'    : 'enhancement',
+                'bugs'    : '1234',
+                'cves'    : 'CVE-2020-0001',
+                'notes'   : 'foobar'
+        }
+        self.save_update(params, session)
+        update = PackageUpdate.byTitle('TurboGears-1.0.2.2-2.fc7')
+        assert update
+        assert update.title == params['builds']
+        assert update.builds[0].nvr == params['builds']
+        assert update.release.long_name == params['release']
+        assert update.bugs[0].bz_id == int(params['bugs'])
+        assert update.cves[0].cve_id == params['cves']
+        assert update.notes == params['notes']
+        # we specified a CVE, so bodhi will automatically change the type
+        assert update.type == 'security'
