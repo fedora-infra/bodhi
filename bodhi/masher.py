47a63abc263271915369e5af2784c00d7b18e15a
Luke Macken <lmacken@fedoraproject.org>
2007-09-15 03:04:02 -0400

    Add some more timers in the masher

--- bodhi/masher.py
+++ bodhi/masher.py
@@ -250,9 +250,13 @@ class MashTask(Thread):
         anything fails, undo any tag moves.
         """
         try:
+            t0 = time.time()
             if self.move_builds():
+                log.debug("Moved builds in %f seconds" % time.time() - t0)
                 self.success = True
+                t0 = time.time()
                 self.mash()
+                log.debug("Mashed for %f seconds" % time.time() - t0)
                 if self.success:
                     masher.success(self)
                 else:
