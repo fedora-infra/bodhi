dd96a6b18b7704c7d379d51034c52a4eda9807b1
Luke Macken <lmacken@fedoraproject.org>
2008-02-20 17:00:15 -0500

    Add a test case for updateinfo updating

--- bodhi/tests/test_metadata.py
+++ bodhi/tests/test_metadata.py
@@ -35,6 +35,7 @@ class TestExtendedMetadata(testutil.DBTest):
                                notes='foobar',
                                type='bugfix')
         build = PackageBuild(nvr=builds[0]['nvr'], package=package, update=update)
+
         bug = Bugzilla(bz_id=1)
         update.addBugzilla(bug)
         cve = CVE(cve_id="CVE-2007-0000")
@@ -88,3 +89,110 @@ class TestExtendedMetadata(testutil.DBTest):
 
         ## Clean up
         shutil.rmtree(temprepo)
+
+
+    def test_extended_metadata_updating(self):
+        # grab the name of a build in updates-testing, and create it in our db
+        koji = get_session()
+        builds = koji.listTagged('dist-fc7-updates-testing', latest=True)
+
+        # Create all of the necessary database entries
+        release = Release(name='fc7', long_name='Fedora 7', id_prefix='FEDORA',
+                          dist_tag='dist-fc7')
+        package = Package(name=builds[0]['package_name'])
+        update = PackageUpdate(title=builds[0]['nvr'],
+                               release=release,
+                               submitter=builds[0]['owner_name'],
+                               status='testing',
+                               notes='foobar',
+                               type='bugfix')
+        build = PackageBuild(nvr=builds[0]['nvr'], package=package, update=update)
+
+        bug = Bugzilla(bz_id=1)
+        update.addBugzilla(bug)
+        cve = CVE(cve_id="CVE-2007-0000")
+        update.addCVE(cve)
+        update.assign_id()
+
+        ## Initialize our temporary repo
+        temprepo = join(tempfile.mkdtemp('bodhi'), 'f7-updates-testing')
+        print "Inserting updateinfo into temprepo: %s" % temprepo
+        mkmetadatadir(join(temprepo, 'i386'))
+        repodata = join(temprepo, 'i386', 'repodata')
+        assert exists(join(repodata, 'repomd.xml'))
+
+        ## Generate the XML
+        md = ExtendedMetadata(temprepo)
+
+        ## Insert the updateinfo.xml into the repository
+        md.insert_updateinfo()
+        updateinfo = join(repodata, 'updateinfo.xml.gz')
+        assert exists(updateinfo)
+
+        ## Read an verify the updateinfo.xml.gz
+        uinfo = UpdateMetadata()
+        uinfo.add(updateinfo)
+        notice = uinfo.get_notice(('mutt', '1.5.14', '1.fc7'))
+        assert not notice
+        notice = uinfo.get_notice(get_nvr(update.title))
+        assert notice
+        assert notice['status'] == update.status
+        assert notice['updated'] == update.date_modified
+        assert notice['from'] == str(config.get('release_team_address'))
+        assert notice['description'] == update.notes
+        assert notice['issued'] == 'None'
+        assert notice['update_id'] == update.update_id
+        cve = notice['references'][0]
+        assert cve['type'] == 'cve'
+        assert cve['href'] == update.cves[0].get_url()
+        assert cve['id'] == update.cves[0].cve_id
+        bug = notice['references'][1]
+        assert bug['href'] == update.bugs[0].get_url()
+        assert bug['id'] == '1'
+        assert bug['type'] == 'bugzilla'
+
+        # FC6's yum update metadata parser doesn't know about some stuff
+        from yum import __version__
+        if __version__ != '3.0.6':
+            assert notice['title'] == update.title
+            assert notice['release'] == update.release.long_name
+            assert cve['title'] == None
+
+        ## Test out updateinfo.xml updating via our ExtendedMetadata
+        md = ExtendedMetadata(temprepo, updateinfo)
+        md.insert_updateinfo()
+        updateinfo = join(repodata, 'updateinfo.xml.gz')
+        assert exists(updateinfo)
+
+        ## Read an verify the updateinfo.xml.gz
+        uinfo = UpdateMetadata()
+        uinfo.add(updateinfo)
+        notice = uinfo.get_notice(('mutt', '1.5.14', '1.fc7'))
+        assert not notice
+        notice = uinfo.get_notice(get_nvr(update.title))
+        assert notice
+        assert notice['status'] == update.status
+        assert notice['updated'] == update.date_modified
+        assert notice['from'] == str(config.get('release_team_address'))
+        assert notice['description'] == update.notes
+        assert notice['issued'] == 'None'
+        assert notice['update_id'] == update.update_id
+        cve = notice['references'][0]
+        assert cve['type'] == 'cve'
+        assert cve['href'] == update.cves[0].get_url()
+        assert cve['id'] == update.cves[0].cve_id
+        bug = notice['references'][1]
+        assert bug['href'] == update.bugs[0].get_url()
+        assert bug['id'] == '1'
+        assert bug['type'] == 'bugzilla'
+
+        # FC6's yum update metadata parser doesn't know about some stuff
+        from yum import __version__
+        if __version__ != '3.0.6':
+            assert notice['title'] == update.title
+            assert notice['release'] == update.release.long_name
+            assert cve['title'] == None
+
+        ## Clean up
+        shutil.rmtree(temprepo)
+
