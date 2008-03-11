1ecce34ab7dfd802c09bb72b78f8c4a98146db19
Luke Macken <lmacken@fedoraproject.org>
2008-03-11 16:35:11 -0400

    Return a dictionary instead of a PackageUpdate.__str__ from list()

--- bodhi/controllers.py
+++ bodhi/controllers.py
@@ -248,11 +248,6 @@ class Root(controllers.RootController):
             if self.jsonRequest():
                 return dict(updates=[])
 
-        # If we're called via JSON, then simply return the PackageUpdate.__str__
-        # else, we return a list of PackageUpdate objects to our template
-        if self.jsonRequest():
-            updates = map(unicode, updates)
-
         if isinstance(updates, list): num_items = len(updates)
         else: num_items = updates.count()
 
