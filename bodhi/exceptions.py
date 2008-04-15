96d7bc13f59e075ed26299c02a578ba9e9fa6ef9
Luke Macken <lmacken@redhat.com>
2008-04-14 22:46:15 -0400

    Create a BodhiException master class

--- bodhi/exceptions.py
+++ bodhi/exceptions.py
@@ -12,13 +12,19 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-class RPMNotFound(Exception):
+class BodhiException(Exception):
     pass
 
-class RepositoryLocked(Exception):
+class RPMNotFound(BodhiException):
     pass
 
-class RepositoryNotFound(Exception):
+class RepositoryLocked(BodhiException):
+    pass
+
+class RepositoryNotFound(BodhiException):
+    pass
+
+class InvalidRequest(BodhiException):
     pass
 
 try:
