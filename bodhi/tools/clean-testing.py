8c3e1a4399ea05e2f141ef77460a0ae7cf2c7e32
Luke Macken <lmacken@redhat.com>
2008-07-27 00:47:48 -0400

    Update our clean-testing tool to properly handle things via bodhi if possible

--- bodhi/tools/clean-testing.py
+++ bodhi/tools/clean-testing.py
@@ -4,21 +4,27 @@
 A tool that untags testing builds that have a newer build tagged as stable.
 """
 
+import os
 import sys
 import rpm
+import turbomail
+import turbogears
 
-from bodhi.util import load_config
-from bodhi.model import Release
+from sqlobject import SQLObjectNotFound
+from turbogears.database import PackageHub
+
+from bodhi.model import Release, PackageBuild
 from bodhi.buildsys import get_session
 
 def clean_testing_builds(untag=False):
     koji = get_session()
     for release in Release.select():
-        stable_builds = koji.listTagged('%s-updates' % release.dist_tag, latest=True)
-        print "Fetched %d builds tagged with %s-updates" % (len(stable_builds),
-                '%s-updates' % release.dist_tag)
-        testing_builds = koji.listTagged('%s-updates-testing' % release.dist_tag,
-                                         latest=True)
+        stable_builds = koji.listTagged('%s-updates' % release.dist_tag,
+                                        latest=True)
+        print "Fetched %d builds tagged with %s-updates" % (
+                len(stable_builds), '%s-updates' % release.dist_tag)
+        testing_builds = koji.listTagged('%s-updates-testing' %
+                                         release.dist_tag, latest=True)
         print "Fetched %d builds tagged with %s-updates-testing" % (
                 len(testing_builds), release.dist_tag)
         for testing_build in testing_builds:
@@ -32,13 +38,30 @@ def clean_testing_builds(untag=False):
                                          stable_build['release'])) < 0:
                         print "%s is older than %s" % (testing_build['nvr'],
                                                        stable_build['nvr'])
-                        if untag:
-                            print "Untagging koji build %s" % testing_build['nvr']
-                            koji.untagBuild('%s-updates-testing',
-                                            testing_build['nvr'])
+                        try:
+                            build = PackageBuild.byNvr(testing_build['nvr'])
+                            if build.update.status != 'testing':
+                                print "%s not testing in bodhi!" % build.update
+                                raise SQLObjectNotFound
+                            else:
+                                if untag:
+                                    print "Obsoleting via bodhi"
+                                    build.update.obsolete(newer=stable_build['nvr'])
+                                else:
+                                     print "Need to obsolete via bodhi"
+                        except SQLObjectNotFound:
+                            if untag:
+                                print "Untagging via koji"
+                                koji.untagBuild('%s-updates-testing' %
+                                                release.dist_tag,
+                                                testing_build['nvr'],
+                                                force=True)
+                            else:
+                                print "Need to untag koji build %s" % testing_build['nvr']
 
         print
 
 if __name__ == '__main__':
     load_config()
+    turbomail.start_extension()
     clean_testing_builds('--untag' in sys.argv)
