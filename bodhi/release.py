c90fc84dd1bc76525334720292f1d575b9b06998
Luke Macken <lmacken@redhat.com>
2009-04-06 11:12:53 -0400

    0.5.19

--- bodhi/release.py
+++ bodhi/release.py
@@ -1,4 +1,4 @@
-VERSION = '0.5.18'
+VERSION = '0.5.19'
 NAME = 'bodhi'
 DESCRIPTION = 'Bodhi is a modular web-system that facilitates the process of publishing updates for a Fedora-based software distribution'
 AUTHOR = 'Luke Macken'
