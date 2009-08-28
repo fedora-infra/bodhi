c4b6ee6c9c64280fbcec0d1c229d836ac7ec85db
Luke Macken <lmacken@redhat.com>
2009-08-28 14:33:07 -0400

    Disable the auto-obsoleting feature, which tends to cause more problems than it is worth

--- bodhi/tests/test_controllers.py
+++ bodhi/tests/test_controllers.py
@@ -671,45 +671,45 @@ class TestControllers(testutil.DBTest):
         self.save_update(params, session)
         assert "This resource resides temporarily" in cherrypy.response.body[0], cherrypy.response.body[0]
 
-    def test_obsoleting(self):
-        session = login()
-        create_release()
-        params = {
-                'builds'  : 'TurboGears-1.0.2.2-2.fc7',
-                'release' : 'Fedora 7',
-                'type_'   : 'enhancement',
-                'bugs'    : '1234',
-                'cves'    : 'CVE-2020-0001',
-                'notes'   : 'foobar',
-                'request' : None
-        }
-        self.save_update(params, session)
-        print cherrypy.response.body[0]
-        assert "This resource resides temporarily at <a href='http://localhost/updates/TurboGears-1.0.2.2-2.fc7'>http://localhost/updates/TurboGears-1.0.2.2-2.fc7</a>" in cherrypy.response.body[0]
-        update = PackageUpdate.byTitle(params['builds'])
-        assert update.status == 'pending'
+    #def test_obsoleting(self):
+    #    session = login()
+    #    create_release()
+    #    params = {
+    #            'builds'  : 'TurboGears-1.0.2.2-2.fc7',
+    #            'release' : 'Fedora 7',
+    #            'type_'   : 'enhancement',
+    #            'bugs'    : '1234',
+    #            'cves'    : 'CVE-2020-0001',
+    #            'notes'   : 'foobar',
+    #            'request' : None
+    #    }
+    #    self.save_update(params, session)
+    #    print cherrypy.response.body[0]
+    #    assert "This resource resides temporarily at <a href='http://localhost/updates/TurboGears-1.0.2.2-2.fc7'>http://localhost/updates/TurboGears-1.0.2.2-2.fc7</a>" in cherrypy.response.body[0]
+    #    update = PackageUpdate.byTitle(params['builds'])
+    #    assert update.status == 'pending'
 
-        # Throw a newer build in, which should obsolete the previous
-        newparams = {
-                'builds'  : 'TurboGears-1.0.2.2-3.fc7',
-                'release' : 'Fedora 7',
-                'type_'    : 'enhancement',
-                'bugs'    : '4321',
-                'cves'    : 'CVE-2020-0001',
-                'notes'   : 'bizbaz'
-        }
-        self.save_update(newparams, session)
-        newupdate = PackageUpdate.byTitle(newparams['builds'])
-        assert newupdate.status == 'pending'
-        update = PackageUpdate.byTitle(params['builds'])
-        assert update.status == 'obsolete'
+    #    # Throw a newer build in, which should obsolete the previous
+    #    newparams = {
+    #            'builds'  : 'TurboGears-1.0.2.2-3.fc7',
+    #            'release' : 'Fedora 7',
+    #            'type_'    : 'enhancement',
+    #            'bugs'    : '4321',
+    #            'cves'    : 'CVE-2020-0001',
+    #            'notes'   : 'bizbaz'
+    #    }
+    #    self.save_update(newparams, session)
+    #    newupdate = PackageUpdate.byTitle(newparams['builds'])
+    #    assert newupdate.status == 'pending'
+    #    update = PackageUpdate.byTitle(params['builds'])
+    #    assert update.status == 'obsolete'
 
-        # The newer build should also inherit the obsolete updates bugs
-        bugz = [bug.bz_id for bug in newupdate.bugs]
-        assert 1234 in bugz and 4321 in bugz
+    #    # The newer build should also inherit the obsolete updates bugs
+    #    bugz = [bug.bz_id for bug in newupdate.bugs]
+    #    assert 1234 in bugz and 4321 in bugz
 
-        # The newer update should also inherit the obsolete updates notes
-        assert newupdate.notes == "%s\n%s" % (newparams['notes'], params['notes'])
+    #    # The newer update should also inherit the obsolete updates notes
+    #    assert newupdate.notes == "%s\n%s" % (newparams['notes'], params['notes'])
 
     def test_obsoleting_request(self):
         session = login()
