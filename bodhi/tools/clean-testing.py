56181d8a585ebfc1bff947cecff9e51c3e1bbca9
Luke Macken <lmacken@redhat.com>
2010-04-30 14:35:24 -0400

    Fix our clean-testing.py script to work with our new tag properties

--- bodhi/tools/clean-testing.py
+++ bodhi/tools/clean-testing.py
@@ -20,14 +20,12 @@ from bodhi.buildsys import get_session
 def clean_testing_builds(untag=False):
     koji = get_session()
     for release in Release.select():
-        stable_builds = koji.listTagged('%s-updates' % release.dist_tag,
-                                        latest=True)
-        print "Fetched %d builds tagged with %s-updates" % (
-                len(stable_builds), '%s-updates' % release.dist_tag)
-        testing_builds = koji.listTagged('%s-updates-testing' %
-                                         release.dist_tag, latest=True)
-        print "Fetched %d builds tagged with %s-updates-testing" % (
-                len(testing_builds), release.dist_tag)
+        stable_builds = koji.listTagged(release.stable_tag, latest=True)
+        print "Fetched %d builds tagged with %s" % (
+                len(stable_builds), release.stable_tag)
+        testing_builds = koji.listTagged(release.testing_tag, latest=True)
+        print "Fetched %d builds tagged with %s" % (
+                len(testing_builds), release.testing_tag)
         for testing_build in testing_builds:
             for stable_build in stable_builds:
                 if stable_build['package_name'] == testing_build['package_name']:
