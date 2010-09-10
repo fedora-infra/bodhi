50e3c79180635b1a24479477963153b95b43d7b6
Luke Macken <lmacken@redhat.com>
2010-09-10 18:07:18 -0400

    Allow for normal updates hit stable after meeting cripath requirements.

--- bodhi/model.py
+++ bodhi/model.py
@@ -466,7 +466,7 @@ class PackageUpdate(SQLObject):
         flash_notes = '' 
         if action == 'stable' and not self.critpath:
             # Check if we've met the karma requirements
-            if self.karma >= self.stable_karma:
+            if self.karma >= self.stable_karma or self.critpath_approved:
                 pass
             else:
                 # If we haven't met the stable karma requirements, check if it has met
