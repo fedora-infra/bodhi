356bdce860f47aa68fd07a247a5cdce5d126bb69
Luke Macken <lmacken@redhat.com>
2011-05-17 13:53:55 -0400

    Get our views.py back up to 100% coverage

--- pyramid/bodhi/bodhi/tests/views/test_views.py
+++ pyramid/bodhi/bodhi/tests/views/test_views.py
@@ -18,9 +18,24 @@ class ViewTests(unittest.TestCase):
         info = view_model_instance_json(context, request)
         self.assertEqual(info['context'], context.__json__())
 
-    def test_view_model(self):
+    def test_view_model_json(self):
         from bodhi.resources import ReleaseResource
         from bodhi.views import view_model_json
         request = testing.DummyRequest()
         info = view_model_json(ReleaseResource, request)
         self.assertEqual(len(info['entries']), 2)
+
+    def test_view_model_instance(self):
+        from bodhi.views import view_model_instance
+        request = testing.DummyRequest()
+        context = DBSession.query(Release).first()
+        info = view_model_instance(context, request)
+        self.assertEqual(type(info['context']), context.__class__)
+
+    def test_view_model(self):
+        from bodhi.resources import ReleaseResource
+        from bodhi.views import view_model
+        request = testing.DummyRequest()
+        info = view_model(ReleaseResource, request)
+        self.assertEqual(len(info['page']), 2)
+        self.assertEqual(info['caption'], 'Releases')
