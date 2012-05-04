b261a0f3db903d13cd90c6823a25064f7b1fafbd
Luke Macken <lmacken@redhat.com>
2012-05-04 15:41:31 -0400

    We need WebTest

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(name='bodhi',
       include_package_data=True,
       zip_safe=False,
       install_requires = requires,
-      tests_require = ['nose', 'nose-cov',],
+      tests_require = ['nose', 'nose-cov', 'webtest'],
       test_suite="nose.collector",
       message_extractors = { '.': [
           ('**.py', 'lingua_python', None),
