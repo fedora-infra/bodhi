a904ba366f0864a6f17d0ffcd29cd7df43d3f6e9
Luke Macken <lmacken@redhat.com>
2012-05-11 17:31:23 -0400

    Update our build validator to ensure the build is not old

--- bodhi/validators.py
+++ bodhi/validators.py
@@ -1,6 +1,7 @@
+import rpm
 import tw2.core as twc
 
-from bodhi.models import DBSession, Release
+from bodhi.models import DBSession, Release, Package, Build, Update
 from bodhi.util import get_nvr
 from bodhi import buildsys
 
@@ -24,13 +25,13 @@ class BuildValidator(twc.Validator):
             except:
                 raise twc.ValidationError('badbuild', self)
 
-        # Ensure everything is tagged properly.
         # TODO: if we're editing and update, allow testing tags
         session = DBSession()
         candidate_tags = [r.candidate_tag for r in session.query(Release).all()]
-        print("candidate_tags = %r" % candidate_tags)
         koji = buildsys.get_session()
+
         for build in builds:
+            # Ensure everything is tagged properly.
             tags = koji.listTags(build)
             valid = False
             for tag in tags:
@@ -40,3 +41,15 @@ class BuildValidator(twc.Validator):
             if not valid:
                 raise twc.ValidationError('Invalid tag: %s tagged with %s' %
                         (build, candidate_tags), self)
+
+            # Ensure no builds are older than any that we know of
+            nvr = get_nvr(build)
+            pkg = session.query(Package).filter_by(name=nvr[0]).first()
+            if pkg:
+                last = session.query(Build).filter_by(package=pkg) \
+                              .order_by(Build.id.desc()).limit(1).first()
+                if last:
+                    if rpm.labelCompare(nvr, get_nvr(last.nvr)) < 0:
+                        raise twc.ValidationError(
+                                'Invalid build: %s is older than %s' %
+                                (build, last.nvr))
