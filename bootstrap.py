dcf210bc733e2f1dfb8c5f454a99c0e371e9b480
Luke Macken <lmacken@redhat.com>
2013-08-02 14:10:05 -0400

    Link the krbV module into our env to suppress a koji warning

--- bootstrap.py
+++ bootstrap.py
@@ -54,7 +54,8 @@ def _link_system_lib(lib):
 
 def link_system_libs():
     for mod in ('koji', 'rpm', 'OpenSSL', 'urlgrabber', 'pycurl', 'yum',
-                'rpmUtils', 'sqlitecachec', '_sqlitecache', 'psycopg2'):
+                'rpmUtils', 'sqlitecachec', '_sqlitecache', 'psycopg2',
+                'krbVmodule'):
         _link_system_lib(mod)
 
 
