74642938a15498aff3a12248839562c8cd095f9a
Luke Macken <lmacken@redhat.com>
2013-08-01 22:52:56 -0400

    Add a get_db_from_config test

--- bodhi/tests/test_utils.py
+++ bodhi/tests/test_utils.py
@@ -1,7 +1,13 @@
-from bodhi.util import load_config
+from bodhi.util import load_config, get_db_from_config
 
 class TestUtils(object):
 
     def test_load_config(self):
         config = load_config()
         assert config.get('sqlalchemy.url'), config
+
+    def test_get_db_from_config(self):
+        from bodhi.models import Update
+        db = get_db_from_config(dev=True)
+        num = db.query(Update).count()
+        assert num == 0, num
