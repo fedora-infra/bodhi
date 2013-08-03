f14fc23a7d8d7c0e61623650e2fbf541ca4ac0ef
Luke Macken <lmacken@redhat.com>
2013-08-02 22:29:58 -0400

    Add a memoization decorator

--- bodhi/util.py
+++ bodhi/util.py
@@ -24,6 +24,9 @@ import tempfile
 import subprocess
 import urlgrabber
 import ConfigParser
+import collections
+import functools
+
 
 from yum import repoMDObject
 from yum.misc import checksum
@@ -289,6 +292,38 @@ def get_db_from_config(dev=False):
     Base.metadata.bind = engine
     Base.metadata.create_all(engine)
     return DBSession()
+
+
+class memoized(object):
+    '''Decorator. Caches a function's return value each time it is called.  If
+    called later with the same arguments, the cached value is returned (not
+    reevaluated).
+
+    http://wiki.python.org/moin/PythonDecoratorLibrary#Memoize
+    '''
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+
+    def __call__(self, *args):
+        if not isinstance(args, collections.Hashable):
+            # uncacheable. a list, for instance.
+            # better to not cache than blow up.
+            return self.func(*args)
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+
+    def __repr__(self):
+        '''Return the function's docstring.'''
+        return self.func.__doc__
+
+    def __get__(self, obj, objtype):
+        '''Support instance methods.'''
+        return functools.partial(self.__call__, obj)
 
 
 class Singleton(object):
