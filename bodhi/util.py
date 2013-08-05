d1b052ed6b41749cf75270148f064386a1bdc644
Luke Macken <lmacken@redhat.com>
2013-08-05 14:27:28 -0400

    memoize load_config

--- bodhi/util.py
+++ bodhi/util.py
@@ -242,6 +242,38 @@ def link(text, href):
     return '<a href="%s">%s</a>' % (url(href), text)
 
 
+class memoized(object):
+    '''Decorator. Caches a function's return value each time it is called.  If
+    called later with the same arguments, the cached value is returned (not
+    reevaluated).
+
+    http://wiki.python.org/moin/PythonDecoratorLibrary#Memoize
+    '''
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+
+    def __call__(self, *args):
+        if not isinstance(args, collections.Hashable):
+            # uncacheable. a list, for instance.
+            # better to not cache than blow up.
+            return self.func(*args)
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+
+    def __repr__(self):
+        '''Return the function's docstring.'''
+        return self.func.__doc__
+
+    def __get__(self, obj, objtype):
+        '''Support instance methods.'''
+        return functools.partial(self.__call__, obj)
+
+
 def get_configfile():
     configfile = None
     setupdir = os.path.dirname(os.path.dirname(__file__))
@@ -261,6 +293,7 @@ def get_configfile():
     return configfile
 
 
+@memoized
 def load_config(configfile=None):
     """ Load bodhi's configuration.
 
@@ -293,38 +326,6 @@ def get_db_from_config(dev=False):
     Base.metadata.bind = engine
     Base.metadata.create_all(engine)
     return DBSession()
-
-
-class memoized(object):
-    '''Decorator. Caches a function's return value each time it is called.  If
-    called later with the same arguments, the cached value is returned (not
-    reevaluated).
-
-    http://wiki.python.org/moin/PythonDecoratorLibrary#Memoize
-    '''
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
-
-    def __call__(self, *args):
-        if not isinstance(args, collections.Hashable):
-            # uncacheable. a list, for instance.
-            # better to not cache than blow up.
-            return self.func(*args)
-        if args in self.cache:
-            return self.cache[args]
-        else:
-            value = self.func(*args)
-            self.cache[args] = value
-            return value
-
-    def __repr__(self):
-        '''Return the function's docstring.'''
-        return self.func.__doc__
-
-    def __get__(self, obj, objtype):
-        '''Support instance methods.'''
-        return functools.partial(self.__call__, obj)
 
 
 @memoized
