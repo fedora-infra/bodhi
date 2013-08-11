5228a764400b489a466d1fbbd67524027da1ad94
Mathieu Bridon <bochecha@fedoraproject.org>
2013-08-11 15:46:20 -0400

    tests: Check proper filtering by username

--- bodhi/tests/functional/test_wsgi.py
+++ bodhi/tests/functional/test_wsgi.py
@@ -405,6 +405,44 @@ class TestWSGIApp(unittest.TestCase):
         self.assertEquals(res.json_body['errors'][0]['description'],
                           "Invalid type specified: not_my")
 
+    def test_list_updates_by_username(self):
+        res = self.app.get('/updates', {"username": "guest"})
+        body = res.json_body
+        self.assertEquals(len(body['updates']), 1)
+
+        up = body['updates'][0]
+        self.assertEquals(up['title'], u'bodhi-2.0-1')
+        self.assertEquals(up['status'], u'pending')
+        self.assertEquals(up['request'], u'testing')
+        self.assertEquals(up['user']['name'], u'guest')
+        self.assertEquals(up['release']['name'], u'F17')
+        self.assertEquals(up['type'], u'bugfix')
+        self.assertEquals(up['severity'], None)
+        self.assertEquals(up['suggest'], None)
+        self.assertEquals(up['close_bugs'], True)
+        self.assertEquals(up['notes'], u'Useful details!')
+        self.assertEquals(up['date_submitted'], u'1984-11-02 00:00:00')
+        self.assertEquals(up['date_modified'], None)
+        self.assertEquals(up['date_approved'], None)
+        self.assertEquals(up['date_pushed'], None)
+        self.assertEquals(up['qa_approved'], False)
+        self.assertEquals(up['qa_approval_date'], None)
+        self.assertEquals(up['security_approved'], False)
+        self.assertEquals(up['security_approval_date'], None)
+        self.assertEquals(up['releng_approval_date'], None)
+        self.assertEquals(up['locked'], False)
+        self.assertEquals(up['alias'], None)
+        self.assertEquals(up['karma'], 0)
+
+    def test_list_updates_by_unexisting_username(self):
+        res = self.app.get('/updates', {"username": "santa"},
+                           status=400)
+        body = res.json_body
+        self.assertEquals(len(body.get('updates', [])), 0)
+        self.assertEquals(res.json_body['errors'][0]['name'], 'username')
+        self.assertEquals(res.json_body['errors'][0]['description'],
+                          "Invalid username specified: santa")
+
     def test_put_json_update(self):
         self.app.put_json('/updates', self.get_update(), status=405)
 
