4353ca143539b81d5c31ac90c23c8ffd4ce0fb35
Luke Macken <lmacken@redhat.com>
2014-02-07 13:11:28 -0700

    Bump version to 0.9.8

--- bodhi/tools/client.py
+++ bodhi/tools/client.py
@@ -15,7 +15,7 @@
 # Copyright 2007-2010  Red Hat, Inc
 # Authors: Luke Macken <lmacken@redhat.com>
 
-__version__ = '0.9.7.4'
+__version__ = '0.9.8'
 __description__ = 'Command line tool for interacting with Bodhi'
 
 import os
