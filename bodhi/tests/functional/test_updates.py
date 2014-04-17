3b629adb5ad2a0938f9a5a46441fca4a231a31ae
Ralph Bean <rbean@redhat.com>
2014-04-17 15:11:38 -0400

    Break out functional tests into multiple modules.

--- bodhi/tests/functional/test_updates.py
+++ bodhi/tests/functional/test_updates.py
@@ -1,8 +1,7 @@
-import unittest
-
 from datetime import datetime, timedelta
 from webtest import TestApp
-from sqlalchemy import create_engine
+
+import bodhi.tests.functional.base
 
 from bodhi import main
 from bodhi.models import (
@@ -22,93 +21,7 @@ from bodhi.models import (
 )
 
 
-app_settings = {
-    'sqlalchemy.url': 'sqlite://',
-    'mako.directories': 'bodhi:templates',
-    'session.type': 'memory',
-    'session.key': 'testing',
-    'session.secret': 'foo',
-    'dogpile.cache.backend': 'dogpile.cache.memory',
-    'dogpile.cache.expiration_time': 0,
-    'cache.type': 'memory',
-    'cache.regions': 'default_term, second, short_term, long_term',
-    'cache.second.expire': '1',
-    'cache.short_term.expire': '60',
-    'cache.default_term.expire': '300',
-    'cache.long_term.expire': '3600',
-    'acl_system': 'dummy',
-    'buildsystem': 'dummy',
-    'important_groups': 'proventesters provenpackager releng',
-    'admin_packager_groups': 'provenpackager',
-    'mandatory_packager_groups': 'packager',
-    'critpath_pkgs': 'kernel',
-    'bugtracker': 'dummy',
-    'stats_blacklist': 'bodhi autoqa',
-}
-
-
-def populate():
-    session = DBSession()
-    user = User(name=u'guest')
-    session.add(user)
-    provenpackager = Group(name=u'provenpackager')
-    session.add(provenpackager)
-    packager = Group(name=u'packager')
-    session.add(packager)
-    session.flush()
-    user.groups.append(packager)
-    release = Release(
-        name=u'F17', long_name=u'Fedora 17',
-        id_prefix=u'FEDORA', dist_tag=u'f17', version='17')
-    session.add(release)
-    pkg = Package(name=u'bodhi')
-    session.add(pkg)
-    build = Build(nvr=u'bodhi-2.0-1.fc17', release=release, package=pkg)
-    session.add(build)
-    update = Update(
-        title=u'bodhi-2.0-1.fc17',
-        builds=[build], user=user,
-        request=UpdateRequest.testing,
-        notes=u'Useful details!', release=release,
-        date_submitted=datetime(1984, 11, 02))
-    update.type = UpdateType.bugfix
-    bug = Bug(bug_id=12345)
-    session.add(bug)
-    update.bugs.append(bug)
-    cve = CVE(cve_id="CVE-1985-0110")
-    session.add(cve)
-    update.cves.append(cve)
-    session.add(update)
-    session.flush()
-
-
-class TestWSGIApp(unittest.TestCase):
-
-    def setUp(self):
-        engine = create_engine('sqlite://')
-        DBSession.configure(bind=engine)
-        Base.metadata.create_all(engine)
-        populate()
-        self.app = TestApp(main({}, testing=u'guest', **app_settings))
-
-    def tearDown(self):
-        DBSession.remove()
-
-    def get_update(self, builds=u'bodhi-2.0-1.fc17', stable_karma=3, unstable_karma=-3):
-        if isinstance(builds, list):
-            builds = u','.join(builds)
-        return {
-            'builds': builds,
-            'bugs': u'',
-            'notes': u'this is a test update',
-            'type': u'bugfix',
-            'stable_karma': stable_karma,
-            'unstable_karma': unstable_karma,
-        }
-
-    def test_home(self):
-        res = self.app.get('/', status=200)
-        assert 'Logout' in res, res
+class TestUpdatesService(bodhi.tests.functional.base.BaseWSGICase):
 
     def test_invalid_build_name(self):
         res = self.app.post_json('/updates/', self.get_update(u'bodhi-2.0-1.fc17,invalidbuild-1.0'),
@@ -166,7 +79,7 @@ class TestWSGIApp(unittest.TestCase):
         user = User(name=u'bodhi')
         session.add(user)
         session.flush()
-        app = TestApp(main({}, testing=u'bodhi', **app_settings))
+        app = TestApp(main({}, testing=u'bodhi', **self.app_settings))
         res = app.post_json('/updates/', self.get_update(u'bodhi-2.1-1.fc17'),
                             status=400)
         assert 'bodhi does not have commit access to bodhi' in res, res
@@ -180,7 +93,7 @@ class TestWSGIApp(unittest.TestCase):
         group = session.query(Group).filter_by(name=u'provenpackager').one()
         user.groups.append(group)
 
-        app = TestApp(main({}, testing=u'bodhi', **app_settings))
+        app = TestApp(main({}, testing=u'bodhi', **self.app_settings))
         res = app.post_json('/updates/', self.get_update(u'bodhi-2.1-1.fc17'))
         assert 'bodhi does not have commit access to bodhi' not in res, res
         # TODO; uncomment once we're actually creating updates properly
@@ -189,7 +102,7 @@ class TestWSGIApp(unittest.TestCase):
 
     def test_pkgdb_outage(self):
         "Test the case where our call to the pkgdb throws an exception"
-        settings = app_settings.copy()
+        settings = self.app_settings.copy()
         settings['acl_system'] = 'pkgdb'
         settings['pkgdb_url'] = 'invalidurl'
         app = TestApp(main({}, testing=u'guest', **settings))
@@ -198,7 +111,7 @@ class TestWSGIApp(unittest.TestCase):
         assert "Unable to access the Package Database. Please try again later." in res, res
 
     def test_invalid_acl_system(self):
-        settings = app_settings.copy()
+        settings = self.app_settings.copy()
         settings['acl_system'] = 'null'
         app = TestApp(main({}, testing=u'guest', **settings))
         res = app.post_json('/updates/', self.get_update(u'bodhi-2.0-2.fc17'),
