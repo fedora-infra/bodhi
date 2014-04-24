ca81fe3fb35314a44367923d12894d28f79d6e29
Luke Macken <lmacken@redhat.com>
2014-04-24 10:39:07 -0600

    Remove the legacy header from util.py
    This header is GPLv2+, but then mentions the GNU Library General Public
    License, which is incorrect.

--- bodhi/util.py
+++ bodhi/util.py
@@ -1,17 +1,3 @@
-# $Id: util.py,v 1.2 2006/12/31 09:10:14 lmacken Exp $
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 2 of the License.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Library General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-
 """
 Random functions that don't fit elsewhere
 """
