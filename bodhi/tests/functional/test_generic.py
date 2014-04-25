3cda38a1ce7416342ff14c98464f44bb9859e99f
Luke Macken <lmacken@redhat.com>
2014-04-25 12:02:56 -0600

    100% test coverage of the metrics view

--- bodhi/tests/functional/test_generic.py
+++ bodhi/tests/functional/test_generic.py
@@ -90,3 +90,7 @@ class TestGenericViews(bodhi.tests.functional.base.BaseWSGICase):
     def test_markdown(self):
         res = self.app.get('/markdown', {'text': 'wat'}, status=200)
         self.assertEquals(res.json_body['html'], '<p>wat</p>')
+
+    def test_metrics(self):
+        res = self.app.get('/metrics')
+        self.assertIn('$.plot', res)
