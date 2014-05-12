5becdbbef6b2ad0631f49a4357209bb737139eeb
Luke Macken <lmacken@redhat.com>
2014-05-12 13:24:47 -0600

    Add an initial bodhi python client

---
+++ bodhi/client.py
@@ -0,0 +1,18 @@
+from fedora.client import OpenIdBaseClient
+
+BASE_URL = 'http://127.0.0.1:6543'
+
+
+class BodhiClient(OpenIdBaseClient):
+
+    def __init__(self, base_url=BASE_URL, **kwargs):
+        super(BodhiClient, self).__init__(base_url, **kwargs)
+
+    def new(self, **kwargs):
+        return self.send_request('/updates/', verb='POST', auth=True, data=kwargs)
+
+    def query(self, **kwargs):
+        return self.send_request('query', verb='GET', **kwargs)
+
+    def authed_query(self, **kwargs):
+        return self.send_request('query', verb='POST', auth=True, params=kwargs)
