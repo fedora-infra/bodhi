4d5a0b85a27e107c0beacf7351f981193a2dd1cc
Ralph Bean <rbean@redhat.com>
2014-06-01 11:33:15 -0500

    Add forgotten search.js file.

---
+++ bodhi/static/js/search.js
@@ -0,0 +1,80 @@
+
+$(document).ready(function() {
+    var updates = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            url: '/updates/?like=%QUERY',
+            filter: function(response) { return response.updates; },
+        }
+    });
+    var users = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            url: '/users/?like=%QUERY',
+            filter: function(response) { return response.users; },
+        }
+    });
+    var comments = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            url: '/comments/?like=%QUERY',
+            filter: function(response) { return response.comments; },
+        }
+    });
+
+    updates.initialize();
+    users.initialize();
+    comments.initialize();
+
+    $('#bloodhound .typeahead').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 2,
+    },
+    {
+        name: 'updates',
+        displayKey: 'title',
+        source: updates.ttAdapter(),
+        templates: {
+            header: '<h3 class="search">Updates</h3>',
+            empty: [
+                '<div class="empty-message">',
+                'unable to find any updates that match the current query',
+                '</div>'
+            ].join('\n'),
+        },
+    },
+    {
+        name: 'users',
+        displayKey: 'name',
+        source: users.ttAdapter(),
+        templates: {
+            header: '<h3 class="search">Users</h3>',
+            empty: [
+                '<div class="empty-message">',
+                'unable to find any users that match the current query',
+                '</div>'
+            ].join('\n'),
+        },
+    },
+    {
+        name: 'comments',
+        displayKey: 'text',
+        source: comments.ttAdapter(),
+        templates: {
+            header: '<h3 class="search">Comments</h3>',
+            empty: [
+                '<div class="empty-message">',
+                'unable to find any comments that match the current query',
+                '</div>'
+            ].join('\n'),
+        },
+    });
+
+    // Our ajaxy search is hidden by default.  Only show it if this is running
+    // (and therefore if the user has javascript enabled).
+    $("form#search").removeClass('hidden');
+});
