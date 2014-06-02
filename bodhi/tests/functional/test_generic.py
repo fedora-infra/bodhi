d7e42a44d89032f53270206c77d6ad9ba0f2b94f
Ralph Bean <rbean@redhat.com>
2014-06-02 12:01:17 -0600

    Fix test for new frontpage.

--- bodhi/tests/functional/test_generic.py
+++ bodhi/tests/functional/test_generic.py
@@ -99,7 +99,7 @@ class TestGenericViews(bodhi.tests.functional.base.BaseWSGICase):
     def test_home(self):
         res = self.app.get('/', status=200)
         self.assertIn('Logout', res)
-        self.assertIn('Help test Fedora', res)
+        self.assertIn('Fedora Update System', res)
 
     def test_markdown(self):
         res = self.app.get('/markdown', {'text': 'wat'}, status=200)
