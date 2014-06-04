ecf4cf89b2be1be65daa42e455277e635aadb45b
Mathieu Bridon <bochecha@fedoraproject.org>
2014-06-03 19:06:47 -0600

    managereleases: Create releases
    This uses the web API instead of accessing the DB directly, which will
    allow releng to run the command from any machine.

--- tools/managereleases.py
+++ tools/managereleases.py
@@ -12,12 +12,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-import click
-import transaction
+import sys
 
-from pyramid.paster import bootstrap
-from bodhi.config import get_configfile
-from bodhi.models import Release
+import click
+
+from bodhi.client import BodhiClient
 
 
 @click.group()
@@ -26,28 +25,69 @@ def main():
 
 
 @main.command()
+@click.option('--username', envvar='USERNAME')
+@click.option('--password', prompt=True, hide_input=True)
 @click.option('--name', help='Release name (eg: F20)')
 @click.option('--long-name', help='Long release name (eg: "Fedora 20")')
 @click.option('--id-prefix', help='Release prefix (eg: FEDORA)')
 @click.option('--version', help='Release version number (eg: 20)')
-@click.option('--dist-tag', help='Koji dist tag (eg: dist-5E-epel)')
 @click.option('--branch', help='Git branch name (eg: f20)')
-def create_release(**kwargs):
-    env = bootstrap(get_configfile())
-    db = env['request'].db
+@click.option('--dist-tag', help='Koji dist tag (eg: f20)')
+@click.option('--stable-tag', help='Koji stable tag (eg: f20-updates)')
+@click.option('--testing-tag',
+              help='Koji testing tag (eg: f20-updates-testing)')
+@click.option('--candidate-tag',
+              help='Koji candidate tag (eg: f20-updates-candidate)')
+@click.option('--pending-stable-tag',
+              help='Koji pending tag (eg: f20-updates-pending)')
+@click.option('--pending-testing-tag',
+              help='Koji pending testing tag (eg: f20-updates-testing-testing)')
+@click.option('--override-tag', help='Koji override tag (eg: f20-override)')
+@click.option('--state', type=click.Choice(['disabled', 'pending', 'current',
+                                            'archived']),
+              help='The state of the release')
+def create(username, password, **kwargs):
+    client = BodhiClient()
+    client.login(username, password)
 
-    dist_tag = kwargs['dist_tag']
-    kwargs['stable_tag'] = "%s-updates" % dist_tag
-    kwargs['testing_tag'] = "%s-updates-testing" % dist_tag
-    kwargs['candidate_tag'] = "%s-updates-candidate" % dist_tag
-    kwargs['pending_testing_tag'] = "%s-updates-testing-pending" % dist_tag
-    kwargs['pending_stable_tag'] = "%s-updates-pending" % dist_tag
-    kwargs['override_tag'] = "%s-override" % dist_tag
+    save(client, **kwargs)
 
-    with transaction.manager:
-        release = Release(**kwargs)
-        db.add(release)
-        print(release)
+
+def save(client, **kwargs):
+    res = client.send_request('/releases/', verb='POST', auth=True,
+                              data=kwargs)
+
+    data = res.json()
+
+    if 'errors' in data:
+        print_errors(data)
+
+    else:
+        print("Saved release:")
+        print_release(data)
+
+
+def print_release(release):
+        print("  Name:                %s" % release['name'])
+        print("  Long Name:           %s" % release['long_name'])
+        print("  Version:             %s" % release['version'])
+        print("  Branch:              %s" % release['branch'])
+        print("  ID Prefix:           %s" % release['id_prefix'])
+        print("  Dist Tag:            %s" % release['dist_tag'])
+        print("  Stable Tag:          %s" % release['stable_tag'])
+        print("  Testing Tag:         %s" % release['testing_tag'])
+        print("  Candidate Tag:       %s" % release['candidate_tag'])
+        print("  Pending Testing Tag: %s" % release['pending_testing_tag'])
+        print("  Pending Stable Tag:  %s" % release['pending_stable_tag'])
+        print("  Override Tag:        %s" % release['override_tag'])
+        print("  State:               %s" % release['state'])
+
+
+def print_errors(data):
+    for error in data['errors']:
+        print("ERROR: %s" % error['description'])
+
+    sys.exit(1)
 
 
 if __name__ == '__main__':
