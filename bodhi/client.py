e911ad87b36f4bcc7f7206238b62c4c60c8f9183
Mathieu Bridon <bochecha@fedoraproject.org>
2014-06-03 21:57:24 -0600

    cli: Allow submitting updates from a text file
    This is needed for fedpkg to submit updates.

--- bodhi/client.py
+++ bodhi/client.py
@@ -28,3 +28,44 @@ class BodhiClient(OpenIdBaseClient):
 
     def query(self, **kwargs):
         return self.send_request('/updates/', verb='GET', params=kwargs)
+
+    def parse_file(self, input_file):
+        """ Parse an update template file.
+
+        :arg input_file: The filename of the update template.
+
+        Returns an array of dictionaries of parsed update values which
+        can be directly passed to the ``save`` method.
+
+        """
+        from ConfigParser import SafeConfigParser
+        import os
+
+        if not os.path.exists(input_file):
+            raise ValueError("No such file or directory: %s" % input_file)
+
+        config = SafeConfigParser()
+        read = config.read(input_file)
+
+        if len(read) != 1 or read[0] != input_file:
+            raise ValueError("Invalid input file: %s" % input_file)
+
+        updates = []
+
+        for section in config.sections():
+            update = {
+                'builds': section, 'bugs': config.get(section, 'bugs'),
+                'close_bugs': config.getboolean(section, 'close_bugs'),
+                'type': config.get(section, 'type'),
+                'request': config.get(section, 'request'),
+                'severity': config.get(section, 'severity'),
+                'notes': config.get(section, 'notes'),
+                'autokarma': config.get(section, 'autokarma'),
+                'stable_karma': config.get(section, 'stable_karma'),
+                'unstable_karma': config.get(section, 'unstable_karma'),
+                'suggest': config.get(section, 'suggest'),
+                }
+
+            updates.append(update)
+
+        return updates
