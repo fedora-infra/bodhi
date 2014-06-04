cf7388049226bfca10f678a62f8eb575b0a78455
Mathieu Bridon <bochecha@fedoraproject.org>
2014-06-03 19:00:09 -0600

    Rename the createrelease tool
    It is about to be able to do much more than that...

--- tools/managereleases.py
+++ tools/managereleases.py
@@ -1,3 +1,17 @@
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 import click
 import transaction
 
@@ -6,7 +20,12 @@ from bodhi.config import get_configfile
 from bodhi.models import Release
 
 
-@click.command()
+@click.group()
+def main():
+    pass
+
+
+@main.command()
 @click.option('--name', help='Release name (eg: F20)')
 @click.option('--long-name', help='Long release name (eg: "Fedora 20")')
 @click.option('--id-prefix', help='Release prefix (eg: FEDORA)')
@@ -32,4 +51,4 @@ def create_release(**kwargs):
 
 
 if __name__ == '__main__':
-    create_release()
+    main()
