a6fd9ed09dd4bf03d0a19bf3413c572c8df21b47
Luke Macken <lmacken@redhat.com>
2014-06-19 12:40:25 -0600

    Remove the Build.get_source_path method

--- bodhi/models/models.py
+++ bodhi/models/models.py
@@ -470,10 +470,6 @@ class Build(Base):
             i += 1
         return str
 
-    def get_source_path(self):
-        """ Return the path of this built update """
-        return os.path.join(config.get('build_dir'), *get_nvr(self.nvr))
-
     def get_tags(self):
         koji = buildsys.get_session()
         return [tag['name'] for tag in koji.listTags(self.nvr)]
