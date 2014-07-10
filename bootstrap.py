edafc401a148e3cb769dccf8d0d958fe170c2373
Luke Macken <lmacken@redhat.com>
2014-07-09 19:05:10 -0600

    Link createrepo and deltarpm into our virtualenv

--- bootstrap.py
+++ bootstrap.py
@@ -85,7 +85,7 @@ def _link_system_lib(lib):
 def link_system_libs():
     for mod in ('koji', 'rpm', 'OpenSSL', 'urlgrabber', 'pycurl', 'yum',
                 'rpmUtils', 'sqlitecachec', '_sqlitecache', 'psycopg2',
-                'krbVmodule'):
+                'krbVmodule', 'createrepo', 'deltarpm', '_deltarpmmodule'):
         _link_system_lib(mod)
 
 
