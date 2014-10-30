fedora-infra/bodhi

commit c62fb862056de8a537f9d4edf2f1db59cb9fc887

Bugfix - it seems like "submitter" was never a thing here...

bodhi/models/models.py
@@ -786,7 +786,8 @@ class Update(Base):
                 # Ensure the same number of builds are present
                 if len(oldBuild.update.builds) != len(self.builds):
                     obsoletable = False
-                    if oldBuild.update.submitter.name != self.user.name:
+                    submitter = oldBuild.update.user
+                    if submitter and submitter.name != self.user.name:
                         request.session.flash('Please be aware that %s is'
                                 'part of a multi-build update that is currently '
                                 'in testing' % oldBuild.nvr)

commit 2a50664fb483cae240aab94c41c2ebbaede34674

Fix thing that I broke in c62fb862056de8a537f9d4edf2f1db59cb9fc887
I don't know how I did that, or why I thought it was broken back then.

bodhi/models/models.py
@@ -793,7 +793,7 @@ class Update(Base):
                 # Ensure the same number of builds are present
                 if len(oldBuild.update.builds) != len(self.builds):
                     obsoletable = False
-                    submitter = oldBuild.update.user
+                    submitter = oldBuild.update.submitter
                     if submitter and submitter.name != self.user.name:
                         request.session.flash('Please be aware that %s is'
                                 'part of a multi-build update that is currently '