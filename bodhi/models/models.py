a0226139f06a340a8bb48a580936e72a071cbcfe
Ralph Bean <rbean@redhat.com>
2015-01-27 15:59:27 -0500

    Fix a stray bug.

--- bodhi/models/models.py
+++ bodhi/models/models.py
@@ -810,7 +810,7 @@ class Update(Base):
                 # Ensure the same number of builds are present
                 if len(oldBuild.update.builds) != len(self.builds):
                     obsoletable = False
-                    submitter = oldBuild.update.submitter
+                    submitter = oldBuild.update.user
                     if submitter and submitter.name != self.user.name:
                         request.session.flash('Please be aware that %s is'
                                 'part of a multi-build update that is currently '
