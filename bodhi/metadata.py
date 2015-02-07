35b04a1e8f8910cccc5a8ad125b8e48881706f15
Luke Macken <lmacken@redhat.com>
2015-02-06 20:17:28 -0700

    metadata: Bump the updateinfo version number

--- bodhi/metadata.py
+++ bodhi/metadata.py
@@ -12,7 +12,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-__version__ = '1.4'
+__version__ = '2.0'
 
 import os
 import logging
