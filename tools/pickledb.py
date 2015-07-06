9e2c5653a8fc049cd844401d40548d14a45dfa67
Luke Macken <lmacken@redhat.com>
2015-07-06 14:47:22 -0400

    pickledb: Add the __requires__ hack to the top for WebOb

--- tools/pickledb.py
+++ tools/pickledb.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+__requires__ = 'WebOb>=1.4.1'
+import pkg_resources
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
