ed266c4603c7ad91d805c6720dca600819505f1f
Luke Macken <lmacken@redhat.com>
2015-07-07 15:58:24 -0400

    client: Get the BodhiClient working against staging

--- bodhi/client.py
+++ bodhi/client.py
@@ -14,20 +14,20 @@
 
 from fedora.client import OpenIdBaseClient
 
-BASE_URL = 'http://127.0.0.1:6543'
+BASE_URL = 'https://admin.stg.fedoraproject.org/updates/'
 
 
 class BodhiClient(OpenIdBaseClient):
 
     def __init__(self, base_url=BASE_URL, **kwargs):
-        super(BodhiClient, self).__init__(base_url, **kwargs)
+        super(BodhiClient, self).__init__(base_url, debug=True, **kwargs)
 
     def new(self, **kwargs):
-        return self.send_request('/updates/', verb='POST', auth=True,
+        return self.send_request('updates', verb='POST', auth=True,
                                  data=kwargs)
 
     def query(self, **kwargs):
-        return self.send_request('/updates/', verb='GET', params=kwargs)
+        return self.send_request('updates', verb='GET', params=kwargs)
 
     def parse_file(self, input_file):
         """ Parse an update template file.
