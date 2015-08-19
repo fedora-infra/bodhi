d765e0e33bfe591962cec81690e5dc9c0a2d002b
Luke Macken <lmacken@redhat.com>
2015-08-19 09:53:58 -0600

    tools: Re-enable a bunch of tests in the python-fedora api tester

--- tools/python-fedora-api-test.py
+++ tools/python-fedora-api-test.py
@@ -44,74 +44,75 @@ print(bodhi.update_str(update))
 print('')
 print(bodhi.update_str(update, minimal=True))
 
-#print('Call /latest_builds')
-#result = bodhi.latest_builds('kernel')
-#print(result)
-#
-#print('Querying all releases')
-#result = bodhi.get_releases()
-#print(result)
-#
-#print('Looking for candidate builds')
-#print(bodhi.candidates())
-#
-#print('Looking for local builds in updates-testing')
-#for update in bodhi.testable():
-#    print(bodhi.update_str(update))
+print('Call /latest_builds')
+result = bodhi.latest_builds('kernel')
+print(result)
 
-#print('Querying by release')
-#result = bodhi.query(release='F23')
-#print(result)
-#updates = result['updates']
-#print(updates)
-#assert len(updates) == 1, len(updates)
-#assert result['total'] == 1, result
-#assert updates[0].alias == alias
-#print('%d updates returned' % len(updates))
-#
-#print('Querying by release and package')
-#result = bodhi.query(package='qt-creator', release='F23')
-#updates = result['updates']
-#assert len(updates) == 1, len(updates)
-#assert result['total'] == 1, result
-#assert updates[0]['alias'] == alias
-#print('%d updates returned' % len(updates))
-#
-#print('Querying by release and package and status')
-#result = bodhi.query(package='qt-creator', release='F23', status='pending')
-#updates = result.updates
-#assert len(updates) == 1, len(updates)
-#assert result['total'] == 1, result
-#assert updates[0]['alias'] == alias
-#print('%d updates returned' % len(updates))
-#
-#print('Querying by release and package and status and limit')
-#result = bodhi.query(type='security', limit=2)
-#updates = result.updates
-#assert len(updates) == 2, len(updates)
-#assert updates[0]['type'] == 'security'
-#print('%d updates returned' % len(updates))
-#
-#print('Querying by release with limit (blockerbugs)')
-#result = bodhi.query(limit=100, release='F23')
-#updates = result['updates']
-#assert len(updates) == 1, len(updates)
-#assert updates[0]['type'] == 'bugfix', updates[0].type
-#print('%d updates returned' % len(updates))
+print('Querying all releases')
+result = bodhi.get_releases()
+print(result)
 
-#print('Requesting stable')
-#result = bodhi.request(update=alias, request='stable')
-#assert result['status'] == 'error', result
-#assert len(result['errors']) == 1
-#assert result['errors'][0]['name'] == 'request'
-#
-#print('Requesting testing')
-#result = bodhi.request(update=alias, request='testing')
-#assert result['update']['request'] == 'testing'
-#
-#print('Revoking request')
-#result = bodhi.request(update=alias, request='revoke')
-#assert result['update']['request'] == None
+print('Looking for candidate builds')
+print(bodhi.candidates())
+
+print('Looking for local builds in updates-testing')
+for update in bodhi.testable():
+    print(bodhi.update_str(update))
+
+print('Querying by release')
+result = bodhi.query(release='F23')
+print(result)
+updates = result['updates']
+print(updates)
+assert len(updates) == 1, len(updates)
+assert result['total'] == 1, result
+assert updates[0].alias == alias
+print('%d updates returned' % len(updates))
+
+print('Querying by release and package')
+result = bodhi.query(package='qt-creator', release='F23')
+updates = result['updates']
+assert len(updates) == 1, len(updates)
+assert result['total'] == 1, result
+assert updates[0]['alias'] == alias
+print('%d updates returned' % len(updates))
+
+print('Querying by release and package and status')
+result = bodhi.query(package='qt-creator', release='F23', status='pending')
+updates = result.updates
+assert len(updates) == 1, len(updates)
+assert result['total'] == 1, result
+assert updates[0]['alias'] == alias
+print('%d updates returned' % len(updates))
+
+print('Querying by release and package and status and limit')
+result = bodhi.query(type='security', limit=2)
+updates = result.updates
+assert len(updates) == 2, len(updates)
+assert updates[0]['type'] == 'security'
+print('%d updates returned' % len(updates))
+
+print('Querying by release with limit (blockerbugs)')
+result = bodhi.query(limit=100, release='F23')
+updates = result['updates']
+assert len(updates) == 1, len(updates)
+assert updates[0]['type'] == 'bugfix', updates[0].type
+print('%d updates returned' % len(updates))
+
+print('Requesting stable')
+try:
+    result = bodhi.request(update=alias, request='stable')
+    assert False, "Client didn't throw exception"
+except BodhiClientException as e:
+    print(e)
+
+print('Requesting testing')
+result = bodhi.request(update=alias, request='testing')
+assert result['update']['request'] == 'testing'
+
+print('Revoking request')
+result = bodhi.request(update=alias, request='revoke')
+assert result['update']['request'] == None
 
 print('Adding comment')
 result = bodhi.comment(update=alias, comment='yay', karma=1)
@@ -161,4 +162,3 @@ result = bodhi.query(builds='qt-creator-3.4.1-3.fc23')
 updates = result['updates']
 update = updates[0]
 assert len(updates) == 1, len(updates)
-
