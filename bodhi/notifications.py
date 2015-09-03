529ab52fd42fd644498991c34d69d0131de2ae03
Ralph Bean <rbean@redhat.com>
2015-09-03 12:12:42 -0400

    An initial pass at a fedmsg data manager.
    For #458.
    
    We should probably split this out into fedmsg itself and just have
    bodhi.notifications use the data manager that it provides.  Other webapps could
    benefit from this.

--- bodhi/notifications.py
+++ bodhi/notifications.py
@@ -12,6 +12,10 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+import copy
+
+import transaction
+
 import fedmsg
 import fedmsg.config
 
@@ -42,5 +46,105 @@ def publish(topic, msg):
         bodhi.log.warn("fedmsg disabled.  not sending %r" % topic)
         return
 
-    bodhi.log.debug("fedmsg sending %r" % topic)
-    fedmsg.publish(topic=topic, msg=msg)
+    bodhi.log.debug("fedmsg enqueueing %r" % topic)
+
+    manager = _managers_map.get_current_data_manager()
+    manager.enqueue(topic, msg)
+
+
+class ManagerMapping(object):
+    """ Maintain a two-way one-to-one mapping between transaction managers and
+    data managers (for different wsgi threads in the same process). """
+
+    def __init__(self):
+        self.left = {}
+        self.right = {}
+
+    def get_current_data_manager(self):
+        current_transaction_manager = transaction.get()
+        if not current_transaction_manager in self:
+            current_data_manager = FedmsgDataManager()
+            self.add(current_transaction_manager, current_data_manager)
+            current_transaction_manager.join(current_data_manager)
+        else:
+            current_data_manager = self.get(current_transaction_manager)
+        return current_data_manager
+
+    def add(self, transaction_manager, data_manager):
+        self.left[transaction_manager] = data_manager
+        self.right[data_manager] = transaction_manager
+
+    def __contains__(self, item):
+        return item in self.left or item in self.right
+
+    def get(self, transaction_manager):
+        return self.left[transaction_manager]
+
+    def remove(self, data_manager):
+        transaction_manager = self.right[data_manager]
+        del self.left[transaction_manager]
+        del self.right[data_manager]
+
+
+# This is a global object we'll maintain to keep track of the relationship
+# between transaction managers and our data managers.  It ensures that we don't
+# create multiple data managers per transaction and that we don't join the same
+# data manager to a transaction multiple times.  Our data manager should clean
+# up after itself and remove old tm/dm pairs from this mapping in the event of
+# abort or commit.
+_managers_map = ManagerMapping()
+
+
+class FedmsgDataManager(object):
+    transaction_manager = transaction.manager
+
+    def __init__(self):
+        self.uncommitted = []
+        self.committed = []
+
+    def enqueue(self, topic, msg):
+        self.uncommitted.append((topic, msg,))
+
+    def __repr__(self):
+        return self.uncommitted.__repr__()
+
+    def abort(self, transaction):
+        self.uncommitted = copy.copy(self.committed)
+        _managers_map.remove(self)
+
+    def tpc_begin(self, transaction):
+        pass
+
+    def commit(self, transaction):
+        pass
+
+    def tpc_vote(self, transaction):
+        #raise NotImplementedError("Check that fedmsg is initialized and that messages are serializable")
+        # We're not allowed to fail after this function returns.  But we can
+        # raise an exception to cancel the transaction for the other managers.
+        pass
+
+    def tpc_abort(self, transaction):
+        return self.abort(transaction)
+
+    def tpc_finish(self, transaction):
+        for topic, msg in self.uncommitted:
+            bodhi.log.debug("fedmsg sending %r" % topic)
+            fedmsg.publish(topic=topic, msg=msg)
+        self.committed = copy.copy(self.uncommitted)
+        _managers_map.remove(self)
+
+    def sortKey(self):
+        return 'fedmsgdm' + str(id(self))
+
+    def savepoint(self):
+        return FedmsgSavepoint(self)
+
+
+class FedmsgSavepoint(object):
+    def __init__(self, dm):
+        self.dm = dm
+        self.saved_committed = copy.copy(self.dm.uncommitted)
+
+    def rollback(self):
+        self.dm.uncommitted = copy.copy(self.saved_committed)
