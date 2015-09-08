c93dc3a3fe01c13705477c12fedcaa7008163576
Ralph Bean <rbean@redhat.com>
2015-09-08 16:59:34 -0400

    Flip the order of the feedback items.
    So that they are in the same order as in the comment form at the bottom of the page.
    
    Fixes #498.

--- bodhi/models/models.py
+++ bodhi/models/models.py
@@ -957,7 +957,7 @@ class Update(Base):
                         good += 1
                     elif feedback.karma < 0:
                         bad += 1
-        return good, bad * -1
+        return bad * -1, good
 
     def get_testcase_karma(self, testcase):
         good, bad, seen = 0, 0, set()
@@ -971,7 +971,7 @@ class Update(Base):
                         good += 1
                     elif feedback.karma < 0:
                         bad += 1
-        return good, bad * -1
+        return bad * -1, good
 
     def assign_alias(self):
         """Return the next available update ID.
