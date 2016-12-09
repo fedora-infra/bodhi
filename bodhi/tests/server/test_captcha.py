a6537a73bcd1456aac6259b696478aaa94813586
Randy Barlow <randy@electronsweatshop.com>
2016-12-08 20:31:28 -0500

    Add three tests on bodhi.server.captcha.jpeg_generator.
    This commit also adds a new required development package for a font
    used by jpeg_generator. We could have mocked it, but it goes fast
    and it would be good to exercise the interface between bodhi and
    PIL.

--- bodhi/tests/server/test_captcha.py
+++ bodhi/tests/server/test_captcha.py
@@ -18,8 +18,65 @@
 import unittest
 
 import mock
+import PIL.Image
 
 from bodhi.server import captcha
+
+
+class TestJPEGGenerator(unittest.TestCase):
+    """This test class contains tests for the jpeg_generator() function."""
+    def test_with_defaults(self):
+        """Test the jpeg_generator with default settings."""
+        img = captcha.jpeg_generator('42 + 7', {})
+
+        img.verify()
+        self.assertTrue(isinstance(img, PIL.Image.Image))
+        # Ensure the image is the default size
+        self.assertEqual(img.size, (300, 80))
+        self.assertEqual(img.mode, 'RGB')
+
+    @mock.patch('bodhi.server.captcha.ImageDraw.Draw')
+    @mock.patch('bodhi.server.captcha.ImageFont.truetype')
+    @mock.patch('bodhi.server.captcha.random.randint')
+    def test_with_heavy_mocking(self, randint, truetype, Draw):
+        """
+        Test the generator with heavy mocking so we can make sure all the right calls were made.
+        """
+        randint.side_effect = [52, 78]
+        truetype.return_value.getsize = mock.MagicMock(return_value=(100, 80))
+
+        settings = {
+            'captcha.image_width': 1920, 'captcha.image_height': 1080,
+            'captcha.font_path': '/path/to/cool/font',
+            'captcha.font_size': 24, 'captcha.font_color': '#012345', 'captcha.padding': 6}
+
+        img = captcha.jpeg_generator('42 + 7', settings)
+
+        truetype.assert_called_once_with('/path/to/cool/font', 24)
+        truetype.return_value.getsize.assert_called_once_with('42 + 7')
+        Draw.assert_called_once_with(img)
+        Draw.return_value.text.assert_called_once_with((52, 78), '42 + 7',
+                                                       font=truetype.return_value, fill='#012345')
+        img.verify()
+        self.assertTrue(isinstance(img, PIL.Image.Image))
+        # Ensure the image is the default size
+        self.assertEqual(img.size, (1920, 1080))
+        self.assertEqual(img.mode, 'RGB')
+
+    def test_with_specified_parameters(self):
+        """Test the generator with specified dimensions."""
+        settings = {
+            'captcha.image_width': 1920, 'captcha.image_height': 1080,
+            'captcha.font_path': '/usr/share/fonts/liberation/LiberationMono-Bold.ttf',
+            'captcha.font_size': 24, 'captch.font_color': '#012345', 'captcha.padding': 6}
+
+        img = captcha.jpeg_generator('42 + 7', settings)
+
+        img.verify()
+        self.assertTrue(isinstance(img, PIL.Image.Image))
+        # Ensure the image is the default size
+        self.assertEqual(img.size, (1920, 1080))
+        self.assertEqual(img.mode, 'RGB')
 
 
 class TestMathGenerator(unittest.TestCase):
