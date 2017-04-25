1a527621b5f15ce5cbd9e9757abbc28162461822
Randy Barlow <randy@electronsweatshop.com>
2017-04-25 15:58:17 -0400

    If rolling back a db transaction fails, log and raise original exc.
    fixes #1475.
    
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- bodhi/tests/server/test_utils.py
+++ bodhi/tests/server/test_utils.py
@@ -11,11 +11,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+import unittest
+
+import mock
 
 from bodhi.server.buildsys import setup_buildsystem, teardown_buildsystem
 from bodhi.server.config import config
 from bodhi.server.util import (get_critpath_pkgs, get_nvr, markup,
-                               get_rpm_header, cmd, sorted_builds)
+                               get_rpm_header, cmd, sorted_builds, TransactionalSessionMaker)
 
 
 class TestUtils(object):
@@ -70,3 +73,76 @@ class TestUtils(object):
         b1, b2 = sorted_builds([new, old])
         assert b1 == new, b1
         assert b2 == old, b2
+
+
+class TestTransactionalSessionMaker(unittest.TestCase):
+    """This class contains tests on the TransactionalSessionMaker class."""
+    @mock.patch('bodhi.server.util.log.exception')
+    @mock.patch('bodhi.server.util.Session')
+    def test___call___fail_rollback_failure(self, Session, log_exception):
+        """
+        Ensure that __call__() correctly handles the failure case when rolling back itself fails.
+
+        If the wrapped code raises an Exception *and* session.rollback() itself raises an Exception,
+        __call__() should log the failure to roll back, and then close and remove the Session, and
+        should raise the original Exception again.
+        """
+        tsm = TransactionalSessionMaker(mock.MagicMock())
+        exception = ValueError("u can't do that lol")
+        # Now let's make it super bad by having rollback raise an Exception
+        Session.return_value.rollback.side_effect = IOError("lol now u can't connect to the db")
+
+        with self.assertRaises(ValueError) as exc_context:
+            with tsm():
+                raise exception
+
+        log_exception.assert_called_once_with(
+            'An Exception was raised while rolling back a transaction.')
+        self.assertTrue(exc_context.exception is exception)
+        self.assertEqual(Session.return_value.commit.call_count, 0)
+        Session.return_value.rollback.assert_called_once_with()
+        Session.return_value.close.assert_called_once_with()
+        Session.remove.assert_called_once_with()
+
+    @mock.patch('bodhi.server.util.log.exception')
+    @mock.patch('bodhi.server.util.Session')
+    def test___call___fail_rollback_success(self, Session, log_exception):
+        """
+        Ensure that __call__() correctly handles the failure case when rolling back is successful.
+
+        If the wrapped code raises an Exception, __call__() should roll back the transaction, and
+        close and remove the Session, and should raise the original Exception again.
+        """
+        tsm = TransactionalSessionMaker(mock.MagicMock())
+        exception = ValueError("u can't do that lol")
+
+        with self.assertRaises(ValueError) as exc_context:
+            with tsm():
+                raise exception
+
+        self.assertEqual(log_exception.call_count, 0)
+        self.assertTrue(exc_context.exception is exception)
+        self.assertEqual(Session.return_value.commit.call_count, 0)
+        Session.return_value.rollback.assert_called_once_with()
+        Session.return_value.close.assert_called_once_with()
+        Session.remove.assert_called_once_with()
+
+    @mock.patch('bodhi.server.util.log.exception')
+    @mock.patch('bodhi.server.util.Session')
+    def test___call___success(self, Session, log_exception):
+        """
+        Ensure that __call__() correctly handles the success case.
+
+        __call__() should commit the transaction, and close and remove the Session upon a successful
+        operation.
+        """
+        tsm = TransactionalSessionMaker(mock.MagicMock())
+
+        with tsm():
+            pass
+
+        self.assertEqual(log_exception.call_count, 0)
+        self.assertEqual(Session.return_value.rollback.call_count, 0)
+        Session.return_value.commit.assert_called_once_with()
+        Session.return_value.close.assert_called_once_with()
+        Session.remove.assert_called_once_with()
