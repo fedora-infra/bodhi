52f1ce37e7b4f9e82a64e0845d088983ce24eb70
Ryan Lerch <rlerch@redhat.com>
2017-05-31 20:45:05 +1000

    Add tests for server/renderers.py
    Adds a new test for server/renderers.py, testing the JPEG
    renderer.

---
+++ bodhi/tests/server/test_renderers.py
@@ -0,0 +1,61 @@
+# Copyright 2017 Red Hat, Inc.
+#
+# This file is part of Bodhi.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+import copy
+import re
+import StringIO
+
+from webtest import TestApp
+import PIL.Image
+
+from bodhi.server import main
+import bodhi.tests.server.functional.base
+
+
+class TestRenderers(bodhi.tests.server.functional.base.BaseWSGICase):
+
+    def test_renderer_jpeg(self):
+        """
+        Test that the renderer returns a jpeg. In this case, the CAPTCHA image.
+        """
+
+        settings = copy.copy(self.app_settings)
+        settings.update({
+            'authtkt.secret': 'whatever',
+            'authtkt.secure': True,
+            'captcha.secret': '2o78T5zF7OERyAtBfC570ZX2TXvfmI3R5mvw6LkG3W0=',
+            'captcha.image_width': '300',
+            'captcha.image_height': '80',
+            'captcha.font_path': '/usr/share/fonts/liberation/LiberationMono-Regular.ttf',
+            'captcha.font_size': '36',
+            'captcha.font_color': '#000000',
+            'captcha.background_color': '#ffffff',
+            'captcha.padding': '5',
+            'captcha.ttl': '300',
+        })
+        app = TestApp(main({}, session=self.db, **settings))
+
+        res = app.get('/updates/FEDORA-2017-a3bbe1a8f2',
+                      status=200,
+                      headers=dict(accept='text/html'))
+        captcha_url = re.search(r'"http://localhost(/captcha/[^"]*)"', str(res)).groups()[0]
+        resp = app.get(captcha_url, status=200)
+        self.assertIn('image/jpeg', resp.headers['Content-Type'])
+        jpegdata = StringIO.StringIO(resp.body)
+        img = PIL.Image.open(jpegdata)
+        self.assertEqual(img.size, (300, 80))
