9c67b26cbce1ffe75911d04c57ac4931653db167
Randy Barlow <randy@electronsweatshop.com>
2017-06-20 09:30:37 -0400

    Use the -y flag on the CI dnf install command.
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- devel/ci/run_tests.sh
+++ devel/ci/run_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-sudo yum install python2-fedmsg-atomic-composer python-flake8 python-nose python-webtest python-mock python-pyramid python-pyramid-mako python-pyramid-tm python-waitress python-colander python-cornice python-openid python-pyramid-fas-openid python-sqlalchemy python-webhelpers python-progressbar python-bunch python-cryptography python-pillow python-kitchen python-pylibravatar python-fedora python-pydns python-dogpile-cache python-arrow python-markdown python-librepo python-createrepo_c createrepo_c python-bugzilla python-simplemediawiki fedmsg python-click python-webob1.4
+sudo yum install -y python2-fedmsg-atomic-composer python-flake8 python-nose python-webtest python-mock python-pyramid python-pyramid-mako python-pyramid-tm python-waitress python-colander python-cornice python-openid python-pyramid-fas-openid python-sqlalchemy python-webhelpers python-progressbar python-bunch python-cryptography python-pillow python-kitchen python-pylibravatar python-fedora python-pydns python-dogpile-cache python-arrow python-markdown python-librepo python-createrepo_c createrepo_c python-bugzilla python-simplemediawiki fedmsg python-click python-webob1.4
 
 mv development.ini.example development.ini
 
