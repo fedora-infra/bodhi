94276af95993d4f6f512802b0c97a5c76e0af239
Randy Barlow <randy@electronsweatshop.com>
2017-06-22 18:00:28 -0400

    Fix run_tests.sh so that it will run the tests correctly.
    This commit gets run_tests.sh to the point where it will run the
    tests in our CI system correctly. It now installs all needed
    dependencies, exits correctly on failure, patches debugtoolbar out
    of the config files, installs Bodhi in develop mode, and runs
    diff-cover to enforce 100% coverage on new code.
    
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- devel/ci/run_tests.sh
+++ devel/ci/run_tests.sh
@@ -1,9 +1,64 @@
-#!/usr/bin/bash
+#!/usr/bin/bash -ex
+
+sed -i '/pyramid_debugtoolbar/d' setup.py
+sed -i '/pyramid_debugtoolbar/d' development.ini.example
 
 sudo yum install -y epel-release
 
-sudo yum install -y python2-fedmsg-atomic-composer python-flake8 python-nose python-webtest python-mock python-pyramid python-pyramid-mako python-pyramid-tm python-waitress python-colander python-cornice python-openid python-pyramid-fas-openid python-sqlalchemy python-webhelpers python-progressbar python-bunch python-cryptography python-pillow python-kitchen python-pylibravatar python-fedora python-pydns python-dogpile-cache python-arrow python-markdown python-librepo python-createrepo_c createrepo_c python-bugzilla python-simplemediawiki fedmsg python-click python-webob1.4
+# gcc is used to build some dependencies pulled from pypi for coverage.
+# git is needed for diff_cover to work.
+# python-devel is needed for some of the pypi deps when gcc is run.
+# pip is needed to install some things below.
+sudo yum install -y gcc git python-devel python2-pip
+
+# We want a newer version of flake8 than EL 7 has, because the EL 7 version fails and we only really
+# care about it for devs, who use Fedora.
+sudo pip install diff_cover flake8
+
+sudo yum install -y\
+    createrepo_c\
+    fedmsg\
+    koji\
+    liberation-mono-fonts\
+    packagedb-cli\
+    python-alembic\
+    python-arrow\
+    python-bleach\
+    python-bugzilla\
+    python-bunch\
+    python-click\
+    python-colander\
+    python-cornice\
+    python-createrepo_c\
+    python-cryptography\
+    python-dogpile-cache\
+    python-fedora\
+    python-kitchen\
+    python-librepo\
+    python-markdown\
+    python-mock\
+    python-nose\
+    python-openid\
+    python-pillow\
+    python-progressbar\
+    python-pydns\
+    python-pylibravatar\
+    python-pyramid-fas-openid\
+    python-pyramid-mako\
+    python-pyramid-tm\
+    python-pyramid\
+    python-simplemediawiki\
+    python-sqlalchemy\
+    python-waitress\
+    python-webhelpers\
+    python-webob1.4\
+    python-webtest\
+    python2-fedmsg-atomic-composer\
 
 mv development.ini.example development.ini
 
-PYTHONPATH=. /usr/bin/python setup.py nosetests
+sudo /usr/bin/python setup.py develop
+
+/usr/bin/python setup.py nosetests
+
+diff-cover coverage.xml --compare-branch=origin/develop --fail-under=100
