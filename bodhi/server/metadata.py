ee68977f0c0002b66b34e240c8335d5749c29925
Randy Barlow <randy@electronsweatshop.com>
2017-07-25 18:33:26 -0400

    Remove some EL 5 code from bodhi.server.metadata.
    Now that EPEL 5 is EOL, we can remove some code that set metadata
    hashing and compression algorithms specially for EL 5.
    
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- bodhi/server/metadata.py
+++ bodhi/server/metadata.py
@@ -60,14 +60,9 @@ class ExtendedMetadata(object):
         self.comp_type = cr.XZ
 
         if release.id_prefix == u'FEDORA-EPEL':
-            # yum on py2.4 doesn't support sha256 (#1080373)
-            if 'el5' in self.repo or '5E' in self.repo:
-                self.hash_type = cr.SHA1
-                self.comp_type = cr.GZ
-            else:
-                # FIXME: I'm not sure which versions of RHEL support xz metadata
-                # compression, so use the lowest common denominator for now.
-                self.comp_type = cr.BZ2
+            # FIXME: I'm not sure which versions of RHEL support xz metadata
+            # compression, so use the lowest common denominator for now.
+            self.comp_type = cr.BZ2
 
         # Load from the cache if it exists
         self.cached_repodata = os.path.join(self.repo, '..', self.tag +
