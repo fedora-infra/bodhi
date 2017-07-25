a040cbdffafb1c58ef23881550f88f9b5471a559
Randy Barlow <randy@electronsweatshop.com>
2017-07-25 17:48:13 -0400

    Add 100% test coverage to bodhi.server.scripts.initializedb.
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

---
+++ bodhi/tests/server/scripts/test_initializedb.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2017 Red Hat, Inc.
+#
+# This file is part of Bodhi.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""Contains tests for the bodhi.server.scripts.initializedb module."""
+from cStringIO import StringIO
+import unittest
+
+import mock
+
+from bodhi.server.scripts import initializedb
+
+
+class TestMain(unittest.TestCase):
+    """Contains tests for the main() function."""
+    @mock.patch.object(initializedb.Base, 'metadata', mock.MagicMock())
+    @mock.patch('bodhi.server.scripts.initializedb.get_appsettings')
+    @mock.patch('bodhi.server.scripts.initializedb.initialize_db')
+    @mock.patch('bodhi.server.scripts.initializedb.setup_logging')
+    def test_successful_usage(self, setup_logging, initialize_db, get_appsettings):
+        """Assert correct behavior when the right arguments are supplied."""
+        initializedb.main(argv=['initializedb', '/etc/bodhi/production.ini'])
+
+        setup_logging.assert_called_once_with('/etc/bodhi/production.ini')
+        get_appsettings.assert_called_once_with('/etc/bodhi/production.ini')
+        initialize_db.assert_called_once_with(get_appsettings.return_value)
+        self.assertEqual(initializedb.Base.metadata.bind, initialize_db.return_value)
+        initializedb.Base.metadata.create_all.assert_called_once_with(initialize_db.return_value)
+
+    @mock.patch.object(initializedb.Base, 'metadata', mock.MagicMock())
+    @mock.patch('bodhi.server.scripts.initializedb.get_appsettings')
+    @mock.patch('bodhi.server.scripts.initializedb.initialize_db')
+    @mock.patch('bodhi.server.scripts.initializedb.setup_logging')
+    @mock.patch('bodhi.server.scripts.initializedb.sys.exit',
+                side_effect=RuntimeError('program exited'))
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_wrong_args(self, stdout, exit, setup_logging, initialize_db, get_appsettings):
+        """Assert that usage() gets called if the wrong number of args are supplied."""
+        self.assertRaises(
+            RuntimeError, initializedb.main,
+            argv=['initializedb', '/etc/bodhi/production.ini', 'this does not belong'])
+
+        self.assertEqual(
+            stdout.getvalue(),
+            'usage: initializedb <config_uri>\n(example: "initializedb development.ini")\n')
+        exit.assert_called_once_with(1)
+        # None of the other stuff should have happened since we bailed out.
+        self.assertEqual(setup_logging.call_count, 0)
+        self.assertEqual(initialize_db.call_count, 0)
+        self.assertEqual(get_appsettings.call_count, 0)
+
+
+class TestUsage(unittest.TestCase):
+    """Contains tests for the usage() function."""
+    @mock.patch('bodhi.server.scripts.initializedb.sys.exit')
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_usage(self, stdout, exit):
+        """Assert correct behavior of the usage() function."""
+        initializedb.usage(['initializedb'])
+
+        self.assertEqual(
+            stdout.getvalue(),
+            'usage: initializedb <config_uri>\n(example: "initializedb development.ini")\n')
+        exit.assert_called_once_with(1)
