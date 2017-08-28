2d50c4b162fd90a6db00ed7f317491ab3eed33fd
Randy Barlow <randy@electronsweatshop.com>
2017-08-28 10:41:52 -0400

    PEP-257 the used parts of bodhi.server.services.zz_redirects.
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- bodhi/server/services/zz_redirects.py
+++ bodhi/server/services/zz_redirects.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2015-2017 Red Hat, Inc.
+#
+# This file is part of Bodhi.
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
@@ -11,7 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-""" Handle general redirect stuff.
+"""
+Handle general redirect stuff.
 
 This module name gets a 'zz_' tacked on the front so that it comes last.
 We need to catch /updates/{id}/request and /updates/{id}/edit first and those
@@ -65,4 +71,13 @@ zz_bodhi1_update_redirect = Service(
 
 @zz_bodhi1_update_redirect.get()
 def zz_get_bodhi1_update_redirect(request):
+    """
+    Redirect users from the Bodhi 1 update URL to the new path.
+
+    Args:
+        request (pyramid.request): The current web request.
+    Returns:
+        pyramid.httpexceptions.HTTPFound: A redirect to the same update in Bodhi's current URL
+            heirarchy.
+    """
     return HTTPFound("/updates/{0}".format(request.matchdict['id']))
