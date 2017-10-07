62c4ed44d28da28a7ac3d24d7772871a8b0a3a69
Randy Barlow <randy@electronsweatshop.com>
2017-10-07 18:40:03 -0400

    Rename run_tests_fedora.sh to test_container.sh.
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

---
+++ devel/test_container.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/bash -ex
+# Copyright (c) 2017 Red Hat, Inc.
+#
+# This file is part of Bodhi.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# This script is intended to be run inside the test containers. It runs style tests, builds the
+# docs, runs the unit tests, moves results into /results to be collected, and ensures that new code
+# has 100% test coverage.
+
+gather_results() {
+    mv docs/_build/html /results/docs
+    cp *.xml /results
+}
+
+sed -i '/pyramid_debugtoolbar/d' setup.py
+sed -i '/pyramid_debugtoolbar/d' development.ini.example
+
+cp development.ini.example development.ini
+
+/usr/bin/python setup.py develop
+
+/usr/bin/tox
+/usr/bin/py.test $@ || (gather_results; exit 1)
+
+gather_results
+
+diff-cover coverage.xml --compare-branch=origin/develop --fail-under=100
