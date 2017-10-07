90f20fc5eabd8d94aa76534923d303ac70f615ae
Randy Barlow <randy@electronsweatshop.com>
2017-10-07 16:40:10 -0400

    Add Copyright headers to the container test scripts.
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- devel/run_tests.sh
+++ devel/run_tests.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/bash -ex
-# This script is used to run the tests in the CI infrastructure, and launches a variety of parallel
+# Copyright (c) 2017 Red Hat, Inc.
+#
+# This file is part of Bodhi.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# This script is used to run the tests in a variety of parallel
 # containers to test Bodhi across supported Fedora releases (See the RELEASES variable below). You
 # can set the RELEASES environment variable to override the RELEASES for a given test run if you
 # like. You can pass a -x flag to it to get it to exit early if a build or test run fails.
