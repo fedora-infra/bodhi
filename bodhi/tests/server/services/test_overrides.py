0da0838fcf1629111ac0894baf05afb801595d9a
Randy Barlow <randy@electronsweatshop.com>
2017-11-01 16:29:31 -0400

    Move the overrides tests to match their path and add a copyright.
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- bodhi/tests/server/services/test_overrides.py
+++ bodhi/tests/server/services/test_overrides.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2014-2017 Red Hat, Inc. and others.
+#
+# This file is part of Bodhi.
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
