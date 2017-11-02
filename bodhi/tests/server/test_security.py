096bafd1f7a0c2f74457ce797e886385f3ccb4c5
Randy Barlow <randy@electronsweatshop.com>
2017-11-02 12:03:10 -0400

    Clean up the remember_me() tests.
    This commit refactors the remember_me() tests so that they share
    code, have one case per test, have non-redundant names, and use
    self.assertRaises.
    
    It also reorders the tests alphabetically.
    
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- bodhi/tests/server/test_security.py
+++ bodhi/tests/server/test_security.py
@@ -19,78 +19,10 @@
 """Test the bodhi.server.security module."""
 
 from pyramid.testing import DummyRequest
+from zope.interface import interfaces
 
 from bodhi.server import models, security
 from bodhi.tests.server import base
-
-
-class TestRememberMe(base.BaseTestCase):
-    """Test the remember_me() function."""
-
-    def test_remember_me(self):
-        """Test the post-login hook"""
-        req = DummyRequest(params={
-            'openid.op_endpoint': self.app_settings['openid.provider'],
-        })
-        req.db = self.db
-        req.session = {'came_from': '/'}
-        info = {
-            'identity_url': 'http://lmacken.id.fedoraproject.org',
-            'groups': [u'releng'],
-            'sreg': {'email': u'lmacken@fp.o'},
-        }
-        req.registry.settings = self.app_settings
-
-        # Ensure the user doesn't exist yet
-        self.assertIsNone(models.User.get(u'lmacken', self.db))
-        self.assertIsNone(models.Group.get(u'releng', self.db))
-
-        security.remember_me(None, req, info)
-
-        # The user should now exist, and be a member of the releng group
-        user = models.User.get(u'lmacken', self.db)
-        self.assertEquals(user.name, u'lmacken')
-        self.assertEquals(user.email, u'lmacken@fp.o')
-        self.assertEquals(len(user.groups), 1)
-        self.assertEquals(user.groups[0].name, u'releng')
-
-        # Pretend the user has been removed from the releng group
-        info['groups'] = []
-        req.session = {'came_from': '/'}
-
-        security.remember_me(None, req, info)
-
-        user = models.User.get(u'lmacken', self.db)
-        self.assertEquals(len(user.groups), 0)
-        self.assertEquals(len(models.Group.get(u'releng', self.db).users), 0)
-
-    def test_remember_me_with_bad_endpoint(self):
-        """Test the post-login hook with a bad openid endpoint"""
-        req = DummyRequest(params={
-            'openid.op_endpoint': 'bad_endpoint',
-        })
-        req.db = self.db
-
-        def flash(msg):
-            pass
-
-        req.session.flash = flash
-        info = {
-            'identity_url': 'http://lmacken.id.fedoraproject.org',
-            'groups': [u'releng'],
-        }
-        req.registry.settings = self.app_settings
-
-        try:
-            security.remember_me(None, req, info)
-            assert False, 'remember_me should have thrown an exception'
-        except Exception:
-            # A ComponentLookupError is thrown because we're doing this outside
-            # of the webapp
-            pass
-
-        # The user should not exist
-        self.assertIsNone(models.User.get(u'lmacken', self.db))
 
 
 class TestLogin(base.BaseTestCase):
@@ -107,3 +39,61 @@ class TestLogout(base.BaseTestCase):
         """Test the logout redirect"""
         resp = self.app.get('/logout', status=302)
         self.assertEquals(resp.location, 'http://localhost/')
+
+
+class TestRememberMe(base.BaseTestCase):
+    """Test the remember_me() function."""
+
+    def _generate_req_info(self, openid_endpoint):
+        """Generate the request and info to be handed to remember_me() for these tests."""
+        req = DummyRequest(params={'openid.op_endpoint': openid_endpoint})
+        req.db = self.db
+        req.session['came_from'] = '/'
+        info = {
+            'identity_url': 'http://lmacken.id.fedoraproject.org',
+            'groups': [u'releng'],
+            'sreg': {'email': u'lmacken@fp.o'},
+        }
+        req.registry.settings = self.app_settings
+        # Ensure the user doesn't exist yet
+        self.assertIsNone(models.User.get(u'lmacken', self.db))
+        self.assertIsNone(models.Group.get(u'releng', self.db))
+
+        return req, info
+
+    def test_bad_endpoint(self):
+        """Test the post-login hook with a bad openid endpoint"""
+        req, info = self._generate_req_info('bad_endpoint')
+
+        with self.assertRaises(interfaces.ComponentLookupError):
+            security.remember_me(None, req, info)
+
+        # The user should not exist
+        self.assertIsNone(models.User.get(u'lmacken', self.db))
+
+    def test_new_user(self):
+        """Test the post-login hook"""
+        req, info = self._generate_req_info(self.app_settings['openid.provider'])
+
+        security.remember_me(None, req, info)
+
+        # The user should now exist, and be a member of the releng group
+        user = models.User.get(u'lmacken', self.db)
+        self.assertEquals(user.name, u'lmacken')
+        self.assertEquals(user.email, u'lmacken@fp.o')
+        self.assertEquals(len(user.groups), 1)
+        self.assertEquals(user.groups[0].name, u'releng')
+
+    def test_user_groups_removed(self):
+        """Test that a user that has been removed from a group gets marked as removed upon login."""
+        req, info = self._generate_req_info(self.app_settings['openid.provider'])
+        security.remember_me(None, req, info)
+        # Pretend the user has been removed from the releng group
+        info['groups'] = []
+        req.session = {'came_from': '/'}
+
+        security.remember_me(None, req, info)
+
+        user = models.User.get(u'lmacken', self.db)
+        self.assertEquals(len(user.groups), 0)
+        self.assertEquals(len(models.Group.get(u'releng', self.db).users), 0)
