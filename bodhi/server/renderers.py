8c739bd8dbfd4fb7234d12ea0e635b11bdee4995
Randy Barlow <randy@electronsweatshop.com>
2017-12-12 10:42:26 -0500

    PEP-257 bodhi.server.renderers.
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- bodhi/server/renderers.py
+++ bodhi/server/renderers.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2014-2017 Red Hat, Inc.
+#
+# This file is part of Bodhi.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""Define special view renderers, such as RSS and jpeg."""
 import io
 import operator
 
@@ -6,7 +25,28 @@ from feedgen.feed import FeedGenerator
 
 
 def rss(info):
+    """
+    Return a RSS renderer.
+
+    Args:
+        info (pyramid.renderers.RendererHelper): Unused.
+    Returns:
+        function: A function that can be used to render a RSS view.
+    """
     def render(data, system):
+        """
+        Render the given data as an RSS view.
+
+        If the request's content type is set to the default, this function will change it to
+        application/rss+xml.
+
+        Args:
+            data (dict): A dictionary describing the information to be rendered. The information can
+                be different types of objects, such as updates, users, comments, or overrides.
+            system (pyramid.events.BeforeRender): Used to get the current request.
+        Returns:
+            basestring: An RSS document representing the given data.
+        """
         request = system.get('request')
         if request is not None:
             response = request.response
@@ -74,7 +114,26 @@ def rss(info):
 
 
 def jpeg(info):
+    """
+    Return a JPEG renderer.
+
+    Args:
+        info (pyramid.renderers.RendererHelper): Unused.
+    Returns:
+        function: A function that can be used to render a jpeg view.
+    """
     def render(data, system):
+        """
+        Render the given image as a request response.
+
+        This function will also set the content type to image/jpeg if it is the default type.
+
+        Args:
+            data (PIL.Image.Image): The jpeg that should be sent back as a response.
+            system (pyramid.events.BeforeRender): Used to get the current request.
+        Returns:
+            str: The raw jpeg bytes of the given image.
+        """
         request = system.get('request')
         if request is not None:
             response = request.response
