4e335ffca7642af175f596779e7118c1e5925283
Randy Barlow <randy@electronsweatshop.com>
2018-03-06 12:34:48 -0500

    Add 100% line test coverage to validate_captcha().
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- bodhi/tests/server/test_validators.py
+++ bodhi/tests/server/test_validators.py
@@ -19,10 +19,11 @@ import unittest
 
 import mock
 from cornice.errors import Errors
+from pyramid import exceptions
 
 from bodhi.server import validators
 from bodhi.tests.server.base import BaseTestCase
-from bodhi.server import models
+from bodhi.server import captcha, models
 
 
 class TestValidateCSRFToken(BaseTestCase):
@@ -239,3 +240,152 @@ class TestValidateAcls(BaseTestCase):
             'description': 'guest does not have commit access to bodhi'
         }]
         assert mock_request.errors == error, mock_request.errors
+
+
+class TestValidateCaptcha(BaseTestCase):
+    """Test the validate_captcha() function."""
+
+    @mock.patch.dict('bodhi.server.validators.config',
+                     {'captcha.secret': '_fnIOv2bxXaz4FLECjUikl46VFn6HuJYzXjx_43XC1I='})
+    def test_authenticated_user(self):
+        """An authenticated user should not have to solve a captcha."""
+        request = mock.Mock()
+        request.errors = Errors()
+        request.errors.status = None
+        request.user = models.User.query.first()
+        request.validated = {}
+
+        validators.validate_captcha(request)
+
+        self.assertEqual(request.errors, [])
+        self.assertEqual(request.errors.status, None)
+
+    @mock.patch.dict('bodhi.server.validators.config',
+                     {'captcha.secret': '_fnIOv2bxXaz4FLECjUikl46VFn6HuJYzXjx_43XC1I='})
+    def test_captcha_does_not_match_key(self):
+        """Assert an error when the captcha in the session does not match the key."""
+        request = mock.Mock()
+        request.errors = Errors()
+        request.session = {'captcha': 'some_other_key'}
+        request.user = None
+        request.validated = {'captcha_key': 'some_key', 'captcha_value': 'some_value'}
+
+        validators.validate_captcha(request)
+
+        self.assertEqual(
+            request.errors,
+            [{'location': 'cookies', 'name': 'captcha',
+              'description': ("No captcha session cipher match (replay). 'some_other_key' "
+                              "'some_key'")}])
+        self.assertEqual(request.errors.status, exceptions.HTTPBadRequest.code)
+
+    @mock.patch.dict('bodhi.server.validators.config',
+                     {'captcha.secret': '_fnIOv2bxXaz4FLECjUikl46VFn6HuJYzXjx_43XC1I='})
+    def test_captcha_validate_fail(self):
+        """Assert an error when the captcha fails validation."""
+        request = mock.Mock()
+        request.errors = Errors()
+        request.errors.status = None
+        request.registry.settings = validators.config
+        request.user = None
+        # We'll cheat since we know the captcha.secret and figure out the solution.
+        plainkey, value = captcha.math_generator(None, validators.config)
+        cipherkey = captcha.encrypt(plainkey, validators.config)
+        request.session = {'captcha': cipherkey}
+        # By adding a 0 onto the end of the value, we are wrong by 100!
+        request.validated = {'captcha_key': cipherkey, 'captcha_value': value + '0'}
+
+        validators.validate_captcha(request)
+
+        self.assertEqual(
+            request.errors,
+            [{'location': 'body', 'name': 'captcha_value',
+              'description': 'Incorrect response to the captcha.'}])
+        self.assertEqual(request.errors.status, exceptions.HTTPBadRequest.code)
+
+    @mock.patch.dict('bodhi.server.validators.config',
+                     {'captcha.secret': '_fnIOv2bxXaz4FLECjUikl46VFn6HuJYzXjx_43XC1I='})
+    def test_captcha_validate_success(self):
+        """Assert an error when the captcha fails validation."""
+        request = mock.Mock()
+        request.errors = Errors()
+        request.errors.status = None
+        request.registry.settings = validators.config
+        request.user = None
+        # We'll cheat since we know the captcha.secret and figure out the solution.
+        plainkey, value = captcha.math_generator(None, validators.config)
+        cipherkey = captcha.encrypt(plainkey, validators.config)
+        request.session = {'captcha': cipherkey}
+        request.validated = {'captcha_key': cipherkey, 'captcha_value': value}
+
+        validators.validate_captcha(request)
+
+        self.assertEqual(request.errors, [])
+        self.assertEqual(request.errors.status, None)
+        self.assertTrue('captcha' not in request.session)
+
+    @mock.patch.dict('bodhi.server.validators.config', {'captcha.secret': ''})
+    def test_captcha_not_configured(self):
+        """Assert that no errors are noted if captcha is not configured."""
+        request = mock.Mock()
+        request.errors = Errors()
+        request.errors.status = None
+        request.user = None
+        request.validated = {}
+
+        validators.validate_captcha(request)
+
+        self.assertEqual(request.errors, [])
+        self.assertEqual(request.errors.status, None)
+
+    @mock.patch.dict('bodhi.server.validators.config',
+                     {'captcha.secret': '_fnIOv2bxXaz4FLECjUikl46VFn6HuJYzXjx_43XC1I='})
+    def test_captcha_not_in_session(self):
+        """Assert an error when the captcha isn't in the session."""
+        request = mock.Mock()
+        request.errors = Errors()
+        request.session = {}
+        request.user = None
+        request.validated = {'captcha_key': 'some_key', 'captcha_value': 'some_value'}
+
+        validators.validate_captcha(request)
+
+        self.assertEqual(
+            request.errors,
+            [{'location': 'cookies', 'name': 'captcha',
+              'description': 'Captcha cipher not in the session (replay).'}])
+        self.assertEqual(request.errors.status, exceptions.HTTPBadRequest.code)
+
+    @mock.patch.dict('bodhi.server.validators.config',
+                     {'captcha.secret': '_fnIOv2bxXaz4FLECjUikl46VFn6HuJYzXjx_43XC1I='})
+    def test_no_key(self):
+        """Assert that an error is added to the request if the captcha key is missing."""
+        request = mock.Mock()
+        request.errors = Errors()
+        request.user = None
+        request.validated = {}
+
+        validators.validate_captcha(request)
+
+        self.assertEqual(
+            request.errors,
+            [{'location': 'body', 'name': 'captcha_key',
+              'description': 'You must provide a captcha_key.'}])
+        self.assertEqual(request.errors.status, exceptions.HTTPBadRequest.code)
+
+    @mock.patch.dict('bodhi.server.validators.config',
+                     {'captcha.secret': '_fnIOv2bxXaz4FLECjUikl46VFn6HuJYzXjx_43XC1I='})
+    def test_no_value(self):
+        """Assert that an error is added to the request if the captcha value is missing."""
+        request = mock.Mock()
+        request.errors = Errors()
+        request.user = None
+        request.validated = {'captcha_key': 'some_key'}
+
+        validators.validate_captcha(request)
+
+        self.assertEqual(
+            request.errors,
+            [{'location': 'body', 'name': 'captcha_value',
+              'description': 'You must provide a captcha_value.'}])
+        self.assertEqual(request.errors.status, exceptions.HTTPBadRequest.code)
