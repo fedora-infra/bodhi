e71a4d11efb08492e3a27b079cd5ee2db4a53924
Randy Barlow <randy@electronsweatshop.com>
2018-09-13 19:55:13 -0400

    Fix a failing unit test that validates the JJB yaml
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- bodhi/tests/test_ci.py
+++ bodhi/tests/test_ci.py
@@ -35,6 +35,6 @@ class YAMLLoadTest(unittest.TestCase):
         jjb = yaml.safe_load(jjb)
 
         self.assertEqual(
-            jjb[0]['trigger']['triggers'][0]['github-pull-request']['failure-comment'],
+            jjb[1]['trigger']['triggers'][0]['github-pull-request']['failure-comment'],
             ('This pull request fails CI testing. Please review the Jenkins job. Hint: You can '
              'search for "JENKIES FAIL" in the console output to quickly find errors.'))
