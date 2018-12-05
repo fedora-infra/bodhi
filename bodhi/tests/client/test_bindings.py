fedora-infra/bodhi

commit 7b94d3008ed73ecd30c5bcdae6610bdcaee16b0c

Correctly handle unicode chars in update notes.
Signed-off-by: Mattia Verga <mattia.verga@tiscali.it>

bodhi/client/bindings.py
@@ -738,7 +738,7 @@ class BodhiClient(OpenIdBaseClient):
         #  |-->          80 chars in total         <--|
         wrap_width = 66
         wrap_line = functools.partial(textwrap.wrap, width=wrap_width)
-        line_formatter = '{0:>12}: {1}\n'
+        line_formatter = u'{0:>12}: {1}\n'
 
         update_lines = ['{:=^80}\n'.format('=')]
         update_lines += [
@@ -797,16 +797,12 @@ class BodhiClient(OpenIdBaseClient):
             ]
 
         if update['notes']:
-            buf = six.moves.cStringIO(update['notes'])
             notes_lines = list(itertools.chain(
-                *[wrap_line(line) for line in buf]
+                *[wrap_line(update['notes'])]
             ))
-            buf.close()
             indent_lines = ['Notes'] + [' '] * (len(notes_lines) - 1)
-            update_lines += [
-                line_formatter.format(indent, line)
-                for indent, line in six.moves.zip(indent_lines, notes_lines)
-            ]
+            for indent, line in six.moves.zip(indent_lines, notes_lines):
+                update_lines.append(line_formatter.format(indent, line))
 
         update_lines += [
             line_formatter.format('Submitter', update['user']['name']),

bodhi/tests/client/test_bindings.py
@@ -1183,6 +1183,18 @@ class TestBodhiClient_update_str(unittest.TestCase):
                 "Comments: bodhi (unauthenticated) ")))
 
     @mock.patch.dict(
+        client_test_data.EXAMPLE_UPDATE_MUNCH,
+        {u'notes': u'This note contains a unicode char ☺'})
+    def test_update_with_unicode_note(self):
+        """Ensure unicode content in update notes is correctly handled"""
+        client = bindings.BodhiClient()
+        client.base_url = 'http://example.com/tests/'
+
+        text = client.update_str(client_test_data.EXAMPLE_UPDATE_MUNCH)
+
+        self.assertIn(u'Notes: This note contains a unicode char ☺', text)
+
+    @mock.patch.dict(
         client_test_data.EXAMPLE_UPDATE_MUNCH, {u'alias': None})
     def test_update_with_no_alias(self):
         """Ensure we render an update with no alias set properly"""

commit 307e83b94ba89d3ecc71464259a87aea984890bd

Fix formatting issue in the client
Lines in `Notes` were joined before being wrapped, issue introduced by
changeset 7b94d3008.

Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

bodhi/client/bindings.py
@@ -849,7 +849,7 @@ class BodhiClient(OpenIdBaseClient):
 
         if update['notes']:
             notes_lines = list(itertools.chain(
-                *[wrap_line(update['notes'])]
+                *[wrap_line(line) for line in update['notes'].splitlines()]
             ))
             indent_lines = ['Notes'] + [' '] * (len(notes_lines) - 1)
             for indent, line in six.moves.zip(indent_lines, notes_lines):

bodhi/tests/client/test_bindings.py
@@ -1336,6 +1336,19 @@ class TestBodhiClient_update_str(unittest.TestCase):
 
         self.assertIn('CI Status: no tests required\n', text)
 
+    @mock.patch.dict(
+        client_test_data.EXAMPLE_UPDATE_MUNCH,
+        {'notes': 'This note contains:\n* multiline formatting\n* bullet points\n\n'})
+    def test_notes_multiline(self):
+        """Test that severity is rendered."""
+        client = bindings.BodhiClient()
+        client.base_url = 'http://example.com/tests/'
+        text = client.update_str(client_test_data.EXAMPLE_UPDATE_MUNCH)
+
+        self.assertIn('Notes: This note contains:\n', text)
+        self.assertIn('     : * multiline formatting\n', text)
+        self.assertIn('     : * bullet points\n', text)
+
 
 class TestErrorhandled(unittest.TestCase):
     """