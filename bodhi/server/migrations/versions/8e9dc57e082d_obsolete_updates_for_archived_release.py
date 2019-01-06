74527ead5ab333e5af4f4c4d4a15ef38e1f69672
Sebastian Wojciechowski <s.wojciechowski89@gmail.com>
2019-01-06 14:08:20 +0100

    Add migration to obsolete updates for archived releases
    Signed-off-by: Sebastian Wojciechowski <s.wojciechowski89@gmail.com>

---
+++ bodhi/server/migrations/versions/8e9dc57e082d_obsolete_updates_for_archived_release.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 Sebastian Wojciechowski
+#
+# This file is part of Bodhi.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+Obsolete updates for archived release.
+
+Revision ID: 8e9dc57e082d
+Revises: d986618207bc
+Create Date: 2019-01-06 13:04:35.158562
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '8e9dc57e082d'
+down_revision = 'd986618207bc'
+
+
+def upgrade():
+    """Set archived releases updates status to 'obsolete'."""
+    op.execute("UPDATE updates SET status='obsolete' WHERE release_id in \
+                (SELECT id FROM releases WHERE state='archived') AND status NOT IN \
+                ('unpushed', 'obsolete', 'stable')")
+
+
+def downgrade():
+    """Raise an exception explaining that this migration cannot be reversed."""
+    raise NotImplementedError('This migration cannot be reversed.')
