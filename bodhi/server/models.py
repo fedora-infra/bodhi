6fa73c4bdd4b4ebc16ae5117f07e96249c6a56ac
Nils Philippsen <nils@redhat.com>
2019-03-15 10:43:32 +0100

    log why buildroot overrides are expired
    This lets us debug BROs unexpectedly getting untagged in Koji easier.
    
    Fixes #3060
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>
    (cherry picked from commit 759daf42eb6051c4bb9ba82d8409cd205c7b225e)

--- bodhi/server/models.py
+++ bodhi/server/models.py
@@ -2195,6 +2195,7 @@ class Update(Base):
 
                 # Expire any associated buildroot override
                 if b.override:
+                    log.debug(f"Expiring BRO for {b.nvr} because the build is unpushed.")
                     b.override.expire()
                 else:
                     # Only delete the Build entity if it isn't associated with
@@ -4459,6 +4460,7 @@ class BuildrootOverride(Base):
         if old_build is not None and old_build.override is not None:
             # There already is a buildroot override for an older build of this
             # package in this release. Expire it
+            log.debug(f"Expiring BRO for {old_build.nvr} because it's superseded by {build.nvr}.")
             old_build.override.expire()
             db.add(old_build.override)
 
@@ -4503,6 +4505,7 @@ class BuildrootOverride(Base):
             override.enable()
 
         elif data['expired']:
+            log.debug(f"Expiring BRO for {override.build.nvr} because it was edited.")
             override.expire()
 
         db.add(override)
