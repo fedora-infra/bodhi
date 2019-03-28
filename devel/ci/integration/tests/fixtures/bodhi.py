906fd70350645e0d07e66833914ee55fda138f76
Randy Barlow <randy@electronsweatshop.com>
2019-03-27 19:13:06 -0500

    Remove two more uses of six.
    Now that we are purely Python 3, we don't need six anymore.
    
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- devel/ci/integration/tests/fixtures/bodhi.py
+++ devel/ci/integration/tests/fixtures/bodhi.py
@@ -17,10 +17,10 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 import os
+from configparser import ConfigParser
 from uuid import uuid4
 
 import pytest
-from six.moves.configparser import ConfigParser
 
 from ..utils import make_db_and_user, edit_file
 
