e9c1ffa18d8825aa3bee2b85c799b7956bcb251e
Nils Philippsen <nils@redhat.com>
2019-04-18 14:35:53 +0200

    Cope with pytest < 3.7 when capturing log output.
    The logging capture fixture in version of pytest < 3.7 doesn't have the
    'messages' property yet. Monkey-patch old versions of the class so it
    can be used uniformly in tests.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- bodhi/tests/__init__.py
+++ bodhi/tests/__init__.py
@@ -1,4 +1,4 @@
-# Copyright © 2018 Red Hat, Inc.
+# Copyright © 2018, 2019 Red Hat, Inc.
 #
 # This file is part of Bodhi.
 #
@@ -16,3 +16,18 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """Test the bodhi package."""
+
+import _pytest.logging
+
+
+if not hasattr(_pytest.logging.LogCaptureFixture, 'messages'):
+    # Monkey patch old versions of _pytest.logging.LogCaptureFixture
+    class MonkeyLogCaptureFixture(_pytest.logging.LogCaptureFixture):
+        """Adds 'messages' property to LogCaptureFixture."""
+
+        @property
+        def messages(self):
+            """Return a list of format-interpolated log messages."""
+            return [r.getMessage() for r in self.records]
+
+    _pytest.logging.LogCaptureFixture = MonkeyLogCaptureFixture
