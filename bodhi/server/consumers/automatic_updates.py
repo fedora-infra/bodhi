f7ade0931eab2caf242829e83178e3b88b5f384f
Nils Philippsen <nils@redhat.com>
2019-04-18 13:58:05 +0200

    Add AutomaticUpdateHandler.
    This handler automatically creates updates when builds are tagged into
    candidate tags of releases configured for automatic updates.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

---
+++ bodhi/server/consumers/automatic_updates.py
@@ -0,0 +1,130 @@
+# Copyright © 2019 Red Hat, Inc. and others.
+#
+# This file is part of Bodhi.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+The Bodhi handler that creates updates automatically from tagged builds.
+
+This module is responsible for the process of creating updates when builds are
+tagged with certain tags.
+"""
+
+import logging
+
+import fedora_messaging
+
+from bodhi.server import buildsys
+from bodhi.server.exceptions import BodhiException
+from bodhi.server.models import Build, ContentType, Package, Release
+from bodhi.server.models import Update, UpdateType
+from bodhi.server.util import transactional_session_maker
+
+
+log = logging.getLogger('bodhi')
+
+
+class AutomaticUpdateHandler:
+    """
+    The Bodhi Automatic Update Handler.
+
+    A consumer that listens for messages about tagged builds and creates
+    updates from them.
+    """
+
+    def __init__(self, db_factory: transactional_session_maker = None):
+        """
+        Initialize the Automatic Update Handler.
+
+        Args:
+            db_factory: If given, used as the db_factory for this handler. If
+            None (the default), a new TransactionalSessionMaker is created and
+            used.
+        """
+        if not db_factory:
+            self.db_factory = transactional_session_maker()
+        else:
+            self.db_factory = db_factory
+
+    def __call__(self, message: fedora_messaging.api.Message) -> None:
+        """Create updates from appropriately tagged builds.
+
+        Args:
+            message: The message we are processing.
+        """
+        body = message.body
+
+        missing = []
+        for mandatory in ('tag', 'build_id', 'name', 'version', 'release'):
+            if mandatory not in body:
+                missing.append(mandatory)
+        if missing:
+            raise BodhiException(f"Received incomplete tag message. Missing: {', '.join(missing)}")
+
+        btag = body['tag']
+        bnvr = '{name}-{version}-{release}'.format(**body)
+
+        with self.db_factory() as dbsession:
+            rel = dbsession.query(Release).filter_by(create_automatic_updates=True,
+                                                     candidate_tag=btag).first()
+            if not rel:
+                log.debug(f"Ignoring build being tagged into {btag!r}, no release configured for "
+                          "automatic updates for it found.")
+                return
+
+            koji = buildsys.get_session()
+
+            kbuildinfo = koji.getBuild(bnvr)
+            if not kbuildinfo:
+                raise BodhiException(f"Can't find Koji build for {bnvr}.")
+
+            if 'nvr' not in kbuildinfo:
+                raise BodhiException(f"Koji build info for {bnvr} doesn't contain 'nvr'.")
+
+            # some APIs want the Koji build info, some others want the same
+            # wrapped in a larger (request?) structure
+            rbuildinfo = {
+                'info': kbuildinfo,
+                'nvr': kbuildinfo['nvr'].rsplit('-', 2),
+            }
+
+            bcls = ContentType.infer_content_class(Build, kbuildinfo)
+            build = bcls.get(bnvr)
+            if build:
+                log.info(f"Build, active update for {bnvr} exists already, skipping.")
+                return
+
+            log.debug(f"Build for {bnvr} doesn't exist yet, creating.")
+
+            # Package.get_or_create() infers content type already
+            log.debug("Getting/creating related package object.")
+            pkg = Package.get_or_create(rbuildinfo)
+
+            log.debug("Creating build object, adding it to the DB.")
+            build = bcls(nvr=bnvr, package=pkg)
+            dbsession.add(build)
+
+            log.debug(f"Update for {bnvr} doesn't exist yet, creating and adding it.")
+            update = Update(
+                release=rel,
+                builds=[build],
+                notes=f"Automatic update for {bnvr}.",
+                type=UpdateType.unspecified,
+                stable_karma=3,
+                unstable_karma=-3,
+            )
+            dbsession.add(update)
+
+            dbsession.commit()
