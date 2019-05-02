826b2ddf8ae789e3e9d78a11d05f933b1a2640e2
Sebastian Wojciechowski <s.wojciechowski89@gmail.com>
2019-05-02 12:43:15 +0200

    Remove unused BODHI_URL greenwave config setting in CI
    Signed-off-by: Sebastian Wojciechowski <s.wojciechowski89@gmail.com>

--- devel/ci/integration/greenwave/settings.py
+++ devel/ci/integration/greenwave/settings.py
@@ -5,7 +5,6 @@ POLICIES_DIR = '/etc/greenwave/'
 DIST_GIT_BASE_URL = 'https://src.fedoraproject.org'
 DIST_GIT_URL_TEMPLATE = '{DIST_GIT_BASE_URL}/{pkg_namespace}/{pkg_name}/raw/{rev}/f/gating.yaml'
 KOJI_BASE_URL = 'https://koji.fedoraproject.org/kojihub'
-BODHI_URL = 'https://bodhi/'
 
 SECRET_KEY = 'this-is-only-for-integration-testing'
 WAIVERDB_API_URL = 'http://waiverdb:8080/api/v1.0'
