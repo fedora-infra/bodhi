8675a7b92be39dad724f52c1f2d5f9ba04c6a1c9
Sebastian Wojciechowski <s.wojciechowski89@gmail.com>
2019-06-22 12:31:22 +0200

    Add frozen release state to bodhi releases list
    Signed-off-by: Sebastian Wojciechowski <s.wojciechowski89@gmail.com>

--- bodhi/client/__init__.py
+++ bodhi/client/__init__.py
@@ -1273,6 +1273,7 @@ def print_releases_list(releases: typing.List[munch.Munch]):
     pending = [release for release in releases if release['state'] == 'pending']
     archived = [release for release in releases if release['state'] == 'archived']
     current = [release for release in releases if release['state'] == 'current']
+    frozen = [release for release in releases if release['state'] == 'frozen']
 
     if pending:
         click.echo('pending:')
@@ -1287,6 +1288,11 @@ def print_releases_list(releases: typing.List[munch.Munch]):
     if current:
         click.echo('\ncurrent:')
         for release in current:
+            click.echo(f"  Name:                {release['name']}")
+
+    if frozen:
+        click.echo('\nfrozen:')
+        for release in frozen:
             click.echo(f"  Name:                {release['name']}")
 
 
