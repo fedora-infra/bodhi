fedora-infra/bodhi

commit 6498e90570629296027734a8ecbec6652cb15e24

add a filtering/searching interface to the updates query view
Signed-off-by: Ryan Lerch <rlerch@redhat.com>

bodhi/server/templates/updates.html
@@ -12,6 +12,7 @@ def inherit(context):
 % if chrome:
 <%block name="css">
 ${parent.css()}
+<link href="${request.static_url('bodhi:server/static/vendor/selectize/selectize.bootstrap3-0.12.3.css') }" rel="stylesheet" />
 <link rel="alternate" type="application/atom+xml" title="New Updates" href="${request.route_url('updates_rss') + '?' + request.query_string}"/>
 </%block>
 <div class="subheader py-3">
@@ -36,6 +37,181 @@ ${parent.css()}
                 <span class="fa fa-rss"></span>
               </a>
               % endif
+              <%
+              visibleParams = ['search', 'type', "severity","user","status",'releases']
+              allValidParams = ["approved_since",
+                                "approved_before",
+                                "bugs",
+                                "critpath",
+                                "like",
+                                "locked",
+                                "modified_since",
+                                "modified_before",
+                                "packages",
+                                "package",
+                                "builds",
+                                "pushed",
+                                "pushed_since",
+                                "pushed_before",
+                                "releases",
+                                "release",
+                                "request",
+                                "severity",
+                                "status",
+                                "submitted_since",
+                                "submitted_before",
+                                "suggest",
+                                "type",
+                                "content_type",
+                                "gating",
+                                "user",
+                                "updateid",
+                                "alias"]
+              searchterm=''
+              parameters = request.params.dict_of_lists()
+              if 'search' in parameters:
+                searchterm = parameters['search'][0]
+              %>
+              <div class="ml-auto">
+              <form id="filters-form">
+                <div class="dropdown">
+            <div class="input-group input-group-sm" data-toggle="dropdown">
+              <div class="input-group-prepend ml-auto">
+                  <div class="btn btn-sm btn-outline-secondary border" id="updates-filterslist">
+                    
+                    % for param, values in request.params.dict_of_lists().items():
+                      % for i, value in enumerate(values):
+                        % if param in allValidParams:
+                          % if i > 0:
+                          or
+                          %else:
+                          <strong>${param}: </strong>
+                          %endif
+                          ${value}
+                        % endif
+                      % endfor
+                    % endfor
+                  </div>
+
+              </div>
+              <input name="search" type="text" autocomplete='off' class="form-control" id="updates-search" aria-describedby="basic-addon3" value="${searchterm}" placeholder="search">
+            </div>
+            <div class="dropdown-menu dropdown-menu-right p-3" style="min-width:400px; max-width: 400px" aria-labelledby="dropdownMenuButton" id="filters-dropdown">
+                % for param, values in parameters.items():
+                  % for i, value in enumerate(values):
+                    % if (param in allValidParams) and (param not in visibleParams):
+                      <input type="hidden" name="${param}" value="${value}">
+                    % endif
+                  % endfor
+              % endfor
+              <div class="form-group row mb-1 filter-group">
+                  <div class="col-3 align-self-center">
+                  <label for="type" class="col-auto align-self-center pl-1 pr-0 m-0">Status:</label>
+                  </div>
+                  <div class="col-8 pl-1">
+                      <select id="updatestatus" name="status" multiple="multiple">
+                          <option value=""></option>
+                          % for value in statuses:
+                          <option value="${value}"
+                          % if 'status' in parameters and value in parameters['status']:
+                          selected="selected"
+                          % endif
+                          >${value}</option>
+                          % endfor
+                      </select>
+                  </div>
+                  <div class="col-1 pl-0 pr-1 pt-1">
+                    <i class="fa fa-times fa-fw text-muted clear-button" data-clear="updatestatus"></i>
+                  </div>
+              </div>
+
+              <div class="form-group row mb-1 filter-group">
+                  <div class="col-3 align-self-center">
+                  <label for="releases" class="col-auto align-self-center pl-1 pr-0 m-0">Releases:</label>
+                  </div>
+                  <div class="col-8 pl-1">
+                      <select id="updatereleases" name="releases" multiple="multiple">
+                          <option value=""></option>
+                          % for rstatus in ['current', 'pending', "archived"]:
+                            <optgroup label="${rstatus}">
+                            % for value in releases[rstatus]:
+                            <option value="${value['name']}"
+                            % if 'releases' in parameters and value['name'] in parameters['releases']:
+                            selected="selected"
+                            % endif
+                            >${value['long_name']}</option>
+                            % endfor
+                            </optgroup>
+                          %endfor
+                      </select>
+                  </div>
+                  <div class="col-1 pl-0 pr-1 pt-1">
+                    <i class="fa fa-times fa-fw text-muted clear-button" data-clear="updatereleases"></i>
+                  </div>
+              </div>
+              
+              <div class="form-group row mb-1 filter-group">
+                  <div class="col-3 align-self-center">
+                  <label for="type" class="col-auto align-self-center pl-1 pr-0 m-0">Type:</label>
+                  </div>
+                  <div class="col-8 pl-1">
+                      <select id="updatetypes" name="type" class="custom-select">
+                          <option value=""></option>
+                          % for value in types:
+                          <option value="${value}"
+                          % if 'type' in parameters and parameters['type'][0] == value:
+                          selected="selected"
+                          % endif
+                          >${value}</option>
+                          % endfor
+                      </select>
+                  </div>
+                  <div class="col-1 pl-0 pr-1 pt-1">
+                    <i class="fa fa-times fa-fw text-muted clear-button" data-clear="updatetypes"></i>
+                  </div>
+              </div>
+              <div class="form-group row mb-1 filter-group">
+                  <div class="col-3 align-self-center">
+                  <label for="severity" class="col-auto align-self-center pl-1 pr-0 m-0">Severity:</label>
+                  </div>
+                  <div class="col-8 pl-1">
+                      <select id="updateseverity" name="severity" class="custom-select">
+                          <option value=""></option>
+                          % for value in severities:
+                          <option value="${value}"
+                          % if 'severity' in parameters and parameters['severity'][0] == value:
+                          selected="selected"
+                          % endif
+                          >${value}</option>
+                          % endfor
+                      </select>
+                  </div>
+                  <div class="col-1 pl-0 pr-1 pt-1">
+                    <i class="fa fa-times fa-fw text-muted clear-button" data-clear="updateseverity"></i>
+                  </div>
+              </div>
+              <div class="form-group row mb-1 filter-group">
+                  <div class="col-3 align-self-center">
+                  <label for="user" class="col-auto align-self-center pl-1 pr-0 m-0">User:</label>
+                  </div>
+                  <div class="col-8 pl-1">
+                      <input type="text" id="updateuser" name="user" class="form-control"
+                      % if 'user' in parameters:
+                          value="${parameters['user'][0]}"
+                      % endif>
+                    </input>
+                  </div>
+                  <div class="col-1 pl-0 pr-1 pt-1">
+                    <i class="fa fa-times fa-fw text-muted clear-button" data-clear="updateuser"></i>
+                  </div>
+              </div>
+
+              <input type="submit" class="btn btn-block btn-primary" value="Apply Filters" />
+
+              <a href="${request.route_url('updates')}" class="btn btn-link btn-block">Clear Filters</a>
+              </div>
+          </div>  
+              </form></div>
             </div>
             % endif
             % for update in updates:
@@ -54,3 +230,72 @@ ${parent.css()}
   </div>
 </div>
 % endif
+
+<%block name="javascript">
+${parent.javascript()}
+<script src="${request.static_url('bodhi:server/static/vendor/selectize/selectize-0.12.3.min.js')}"></script>
+<script>
+  var parameters = ${str(request.params.dict_of_lists())|n}
+  console.log(parameters)
+  $(document).ready(function() {
+ 
+  var $status_selectize = $('#updatestatus').selectize();
+  var status_selectize_control = $status_selectize[0].selectize;
+
+  var $releases_selectize = $('#updatereleases').selectize();
+  var releases_selectize_control = $releases_selectize[0].selectize;
+
+  $("#filters-dropdown select").on('click', function(event){
+        event.stopPropagation();
+  })
+
+  $('#updates-search, #updates-filterslist, #filters-dropdown').on('click', function(event){
+    if ($("#filters-dropdown").hasClass("show")){
+      event.stopPropagation();
+    }
+  })
+
+  $('.clear-button').on('click', function(event){
+    event.stopPropagation();
+    if ($(this).attr("data-clear") == 'updatereleases'){
+      releases_selectize_control.clear();
+    } else if ($(this).attr("data-clear") == 'updatestatus'){
+      status_selectize_control.clear();
+    } else {
+      $("select#"+$(this).attr("data-clear")+" option:selected").removeAttr("selected")
+      $("input#"+$(this).attr("data-clear")).val("");
+    }
+  })
+
+  $("#updates-search").on('keypress',function(e) {
+    if(e.which == 13) {
+        $("#filters-form").submit();
+    }
+    });
+    $( "#filters-form" ).submit(function( event ) {
+        if ($('select[name="type"]').val() == ''){
+          $('select[name="type"]').prop("disabled", true);
+        }
+
+        if ($('select[name="releases"]').val() == ''){
+          $('select[name="releases"]').prop("disabled", true);
+        }
+
+        if ($('select[name="severity"]').val() == ''){
+          $('select[name="severity"]').prop("disabled", true);
+        }
+
+        if ($('select[name="status"]').val() == ''){
+          $('select[name="status"]').prop("disabled", true);
+        }
+
+        if ($('input[name="user"]').val() == ''){
+          $('input[name="user"]').prop("disabled", true);
+        }
+  });
+
+  });
+
+
+</script>
+</%block>

bodhi/server/services/updates.py
@@ -36,6 +36,7 @@ from bodhi.server.models import (
     ReleaseState,
     Build,
     Package,
+    Release,
 )
 import bodhi.server.schemas
 import bodhi.server.services.errors
@@ -401,7 +402,7 @@ def query_updates(request):
     pages = int(math.ceil(total / float(rows_per_page)))
     query = query.offset(rows_per_page * (page - 1)).limit(rows_per_page)
 
-    return dict(
+    return_values = dict(
         updates=query.all(),
         page=page,
         pages=pages,
@@ -412,6 +413,18 @@ def query_updates(request):
         display_request=data.get('display_request', True),
         package=package,
     )
+    # we need some extra information for the searching / filterings interface
+    # when rendering the html, so we add this here.
+    if request.accept.accept_html():
+        return_values.update(
+            types=reversed(list(bodhi.server.models.UpdateType.values())),
+            severities=sorted(
+                list(bodhi.server.models.UpdateSeverity.values()),
+                key=bodhi.server.util.sort_severity),
+            statuses=list(bodhi.server.models.UpdateStatus.values()),
+            releases=Release.all_releases(),
+        )
+    return return_values
 
 
 @updates.post(schema=bodhi.server.schemas.SaveUpdateSchema,

commit f1deb6893ebae5b2a7b4b35ce80410cc470c10f9

Tidy up the UI a little more
fixes a few things in the new UI:

1. Fix updates not loading on user profile page
the user profile page was failing to load the updates
of a user due to a bug introduced in #3511. The user
profile page pulls html down from the server to show
the list. However the template was also trying to add
the javascript for filtering, which was causing a 500
error.

2. make the suggestion on the update page a <p>.
previously, the suggestion (suggesting the user either
reboot or logout to properly test the package) was in
an alert, which kinda stole the show. This changes that
box to a more subtle paragraph.

3. Clean up the composes and releases pages

Signed-off-by: Ryan Lerch <rlerch@redhat.com>

bodhi/server/templates/compose.html
@@ -5,19 +5,20 @@ ${ compose.release.name } ${ compose.request.description }
 &mdash; Fedora Updates System
 </%block>
 
-<div class="subheader pt-2">
-<div class="container">
-<div class="row">
-  <div class="col-md-12">
-    <h1>
-      ${ compose.release.name } ${ compose.request.description }
-      % if compose.security:
-        <span class="fa fa-shield text-danger" title="This compose contains security updates." data-toggle="tooltip"></span>
-      % endif
-    </h1>
+<div class="subheader">
+  <div class="container py-4">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h3 class="font-weight-bold m-0"><a href="${request.route_url('collection_composes')}">Composes</a>
+          <span class="text-muted">/</span> 
+          ${ compose.release.long_name } ${ compose.request.description } compose
+          % if compose.security:
+            <span class="fa fa-shield text-danger" title="This compose contains security updates." data-toggle="tooltip"></span>
+          % endif
+        </h3>
+      </div>
+    </div>
   </div>
-</div>
-</div>
 </div>
 
 <div class="container pt-2">
@@ -29,12 +30,12 @@ ${ compose.release.name } ${ compose.request.description }
 
           <div class="pt-3">
             <h4>Updates <span class="badge badge-secondary">${len(compose.updates)}</span></h4>
-            <div id="updates">
-              % for update in compose.updates:
-              <div>
-                <a href=${ request.route_url('update', id=update.alias) }>${ update.beautify_title(amp=True, nvr=True) | n}</a>
-              </div>
-              % endfor
+            <div>
+                <div class="list-group">
+                    % for update in compose.updates:
+                      ${self.fragments.update(update, display_karma=False)}
+                    % endfor
+                  </div>
             </div>
 
           </div>
@@ -59,7 +60,7 @@ ${ compose.release.name } ${ compose.request.description }
                   <strong>State</strong>
                 </div>
                 <div>
-                  ${ self.util.composestate2html(compose.state) | n }
+                    <div class="ml-auto font-weight-bold text-${self.fragments.compose_status_context(compose.state.value)}"> ${compose.state.description}</div>
                 </div>
               </div>
 

bodhi/server/templates/composes.html
@@ -1,25 +1,35 @@
 <%inherit file="master.html"/>
-<div class="row page-header">
-  <div class="col-md-6 col-md-offset-3">
-    <h1>Composes</h1>
+<div class="subheader">
+  <div class="container py-4">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h3 class="font-weight-bold m-0">Composes</h3>
+      </div>
+    </div>
   </div>
 </div>
-<div class="row">
-  <div class="col-md-6 col-md-offset-3" id="container">
-    % for compose in composes:
-      <div class="col-md-4">
-        <div class="compose-box">
-        <div class="compose">
-            <h4>
-                <a class="notblue" href="${request.route_url('composes', release_name=compose.release.name, request=compose.request.value)}">${ compose.release.name } ${ compose.request.description }</a>
-                % if compose.security:
-                    <span class="fa fa-shield text-danger" title="This compose contains security updates." data-toggle="tooltip"></span>
-                % endif
-            </h4>
-            ${ self.util.composestate2html(compose.state) | n }
+<div class="container pt-2">
+  <div class="row">
+    <div class="col">
+      <div class="list-group my-3">
+      % if composes:
+        % for compose in composes:
+          <a class="list-group-item d-flex" href="${request.route_url('composes', release_name=compose.release.name, request=compose.request.value)}">
+                <div class="font-weight-bold">
+                    ${ compose.release.long_name } ${ compose.request.description } compose
+                    % if compose.security:
+                        <span class="fa fa-shield text-danger" title="This compose contains security updates." data-toggle="tooltip"></span>
+                    % endif
+                </div>
+                <div class="ml-auto font-weight-bold text-${self.fragments.compose_status_context(compose.state.value)}"> ${compose.state.description}</div>
+            </a>
+        % endfor
+      % else:
+        <div class="list-group-item bg-light text-center py-5">
+          no active composes
         </div>
-        </div>
-      </div>
-    % endfor
+      % endif
+    </div>
   </div>
+</div>
 </div>

bodhi/server/templates/fragments.html
@@ -48,6 +48,24 @@ statusmap = {'pending': 'info',
 ${statusmap[status]}\
 </%def>
 
+<%def name="compose_status_context(status)">\
+<%
+statusmap = {
+  'requested': 'primary',
+  'pending': 'primary',
+  'initializing': 'warning',
+  'updateinfo': 'warning',
+  'punging': 'warning',
+  'notifying': 'warning',
+  'cleaning': 'warning',
+  'syncing_repo': 'warning',
+  'signing_repo': 'warning',
+  'success': 'success',
+  'failed': 'danger'}
+%>\
+${statusmap[status]}\
+</%def>
+
 <%def name="status_text(update)">
   <span class="font-weight-bold">${update.status.value}</span>
   % if update.request:

bodhi/server/templates/master.html
@@ -169,19 +169,17 @@
           file issues</a>
           if you have any problems. Read the <a href="https://bodhi.fedoraproject.org/docs" class="text-white-50">documentation</a>.
         </p>
-        % if request.registry.settings.get('legal_link') or request.registry.settings.get('privacy_link'):
         <p class="text-light text-center">
+            <span>&bull;</span> <a href="${request.route_url('collection_composes')}" class="text-white-50">Composes</a>
+            <span>&bull;</span> <a href="${request.route_url('metrics')}" class="text-white-50">Metrics</a>
             % if request.registry.settings.get('legal_link'):
-            <a href="${request.registry.settings.get('legal_link')}" class="text-white-50">Legal</a>
-            % endif
-            % if request.registry.settings.get('legal_link') and request.registry.settings.get('privacy_link'):
-            |
+            <span>&bull;</span> <a href="${request.registry.settings.get('legal_link')}" class="text-white-50">Legal</a>
             % endif
             % if request.registry.settings.get('privacy_link'):
-            <a href="${request.registry.settings.get('privacy_link')}" class="text-white-50">Privacy policy</a>
+            <span>&bull;</span> <a href="${request.registry.settings.get('privacy_link')}" class="text-white-50">Privacy policy</a>
             % endif
+            <span>&bull;</span>
         </p>
-        % endif
       </div>
     </div>
 

bodhi/server/templates/release.html
@@ -1,16 +1,20 @@
 <%inherit file="master.html"/>
-<div class="container pt-2">
-  % if release.state.value == 'frozen':
-    <div class="row">
-      <div class="alert alert-warning">
-        <p>Release is frozen</p>
+<div class="subheader">
+    <div class="container py-4">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <h3 class="font-weight-bold m-0"><a href="${request.route_url('releases')}">Releases</a>
+              <span class="text-muted">/</span> 
+              ${release['long_name']}
+            </h3>
+        </div>
       </div>
     </div>
-  % endif
+  </div>
+<div class="container pt-2">
   <div class="row">
     <div class="col-md-5">
-      <h2 class="float-left">${release['long_name']}</h2>
-      <div style="clear: both;">
+      <div>
         <canvas id="releasechart" style="clear: both;" width="451"></canvas>
       </div>
       <table class="table">

bodhi/server/templates/releases.html
@@ -39,12 +39,12 @@
   </ul>
 </%def>
 
-<div class="subheader py-3">
+<div class="subheader py-4">
     <div class="container">
       <div class="row">
-        <div class="col-md-12">
-          <h2>Releases</h2>
-        </div>
+          <div class="col-md-6 col-md-offset-3">
+              <h3 class="font-weight-bold m-0">Releases</h3>
+          </div>
       </div>
     </div>
   </div>

bodhi/server/templates/update.html
@@ -169,19 +169,19 @@ if can_edit:
           % endif
 
           % if not 'unspecified' in update.suggest:
-          <div class="alert alert-info">
+          <p class="mt-3">
             % if 'logout' in update.suggest:
-            <h4>Logout Required</h4>
+            <h5 class="font-weight-bold"><i class="fa fa-exclamation-circle text-muted"></i> Logout Required</h5>
             After installing this update it is required that you logout of
             your current user session and log back in to ensure the changes
             supplied by this update are applied properly.
             % elif 'reboot' in update.suggest:
-            <h4>Reboot Required</h4>
+            <h5 class="font-weight-bold"><i class="fa fa-exclamation-circle text-muted"></i> Reboot Required</h5>
             After installing this update it is required that you reboot your
             system to ensure the changes supplied by this update are applied
             properly.
             % endif
-          </div>
+          </p>
           % endif
 
           % if install_command:

bodhi/server/templates/updates.html
@@ -230,7 +230,7 @@ ${parent.css()}
   </div>
 </div>
 % endif
-
+% if chrome:
 <%block name="javascript">
 ${parent.javascript()}
 <script src="${request.static_url('bodhi:server/static/vendor/selectize/selectize-0.12.3.min.js')}"></script>
@@ -299,3 +299,4 @@ ${parent.javascript()}
 
 </script>
 </%block>
+%endif

bodhi/server/util.py
@@ -542,33 +542,6 @@ def markup(context, text):
     return bleach.clean(markdown_text, tags=markdown_tags, attributes=markdown_attrs)
 
 
-def composestate2html(context, state):
-    """
-    Render the given ComposeState as a span containing text.
-
-    Args:
-        context (mako.runtime.Context): Unused.
-        state (bodhi.server.models.ComposeState): The ComposeState to render as a span
-            tag.
-    Returns:
-        str: An HTML span tag representing the ComposeState.
-    """
-    cls = {
-        'requested': 'primary',
-        'pending': 'primary',
-        'initializing': 'warning',
-        'updateinfo': 'warning',
-        'punging': 'warning',
-        'notifying': 'warning',
-        'cleaning': 'warning',
-        'syncing_repo': 'warning',
-        'signing_repo': 'warning',
-        'success': 'success',
-        'failed': 'danger',
-    }[state.value]
-    return "<span class='badge badge-%s'>%s</span>" % (cls, state.description)
-
-
 def type2color(context, t):
     """
     Return a color to render the given UpdateType with.

bodhi/tests/server/services/test_composes.py
@@ -66,7 +66,7 @@ class TestComposeCollectionGet(base.BaseTestCase):
         response = self.app.get('/composes/', status=200, headers={'Accept': 'text/html'})
 
         # The Composes header should still appear in the page
-        self.assertTrue('<h1>Composes</h1>' in response)
+        self.assertTrue('no active composes' in response)
 
     def test_no_composes_json(self):
         """Assert correct behavior for json interface when there are no composes."""
@@ -84,7 +84,7 @@ class TestComposeCollectionGet(base.BaseTestCase):
         response = self.app.get('/composes/', status=200, headers={'Accept': 'text/html'})
 
         # The Composes header should still appear in the page
-        self.assertTrue('<h1>Composes</h1>' in response)
+        self.assertTrue('<h3 class="font-weight-bold m-0">Composes</h3>' in response)
         self.assertTrue(
             '/composes/{}/{}'.format(compose.release.name, compose.request.value) in response)
         self.assertTrue(compose.state.description in response)

bodhi/tests/server/test_util.py
@@ -31,7 +31,7 @@ import pytest
 from bodhi.server import util, models
 from bodhi.server.config import config
 from bodhi.server.exceptions import RepodataException
-from bodhi.server.models import ComposeState, TestGatingStatus, Update
+from bodhi.server.models import TestGatingStatus, Update
 from bodhi.tests.server import base
 
 
@@ -305,66 +305,6 @@ class TestNoAutoflush(unittest.TestCase):
 
         # autoflush should again be True since that was the starting condition.
         self.assertTrue(session.autoflush)
-
-
-class TestComposeState2HTML(unittest.TestCase):
-    """Assert correct behavior from the composestate2html() function."""
-    def test_requested(self):
-        """Assert correct return value with the requested state."""
-        self.assertEqual(util.composestate2html(None, ComposeState.requested),
-                         "<span class='badge badge-primary'>Requested</span>")
-
-    def test_pending(self):
-        """Assert correct return value with the pending state."""
-        self.assertEqual(util.composestate2html(None, ComposeState.pending),
-                         "<span class='badge badge-primary'>Pending</span>")
-
-    def test_initializing(self):
-        """Assert correct return value with the initializing state."""
-        self.assertEqual(util.composestate2html(None, ComposeState.initializing),
-                         "<span class='badge badge-warning'>Initializing</span>")
-
-    def test_updateinfo(self):
-        """Assert correct return value with the updateinfo state."""
-        self.assertEqual(util.composestate2html(None, ComposeState.updateinfo),
-                         "<span class='badge badge-warning'>Generating updateinfo.xml</span>")
-
-    def test_punging(self):
-        """Assert correct return value with the punging state."""
-        self.assertEqual(util.composestate2html(None, ComposeState.punging),
-                         "<span class='badge badge-warning'>Waiting for Pungi to finish</span>")
-
-    def test_notifying(self):
-        """Assert correct return value with the notifying state."""
-        self.assertEqual(util.composestate2html(None, ComposeState.notifying),
-                         "<span class='badge badge-warning'>Sending notifications</span>")
-
-    def test_success(self):
-        """Assert correct return value with the success state."""
-        self.assertEqual(util.composestate2html(None, ComposeState.success),
-                         "<span class='badge badge-success'>Success</span>")
-
-    def test_failed(self):
-        """Assert correct return value with the failed state."""
-        self.assertEqual(util.composestate2html(None, ComposeState.failed),
-                         "<span class='badge badge-danger'>Failed</span>")
-
-    def test_cleaning(self):
-        """Assert correct return value with the cleaning state."""
-        self.assertEqual(util.composestate2html(None, ComposeState.cleaning),
-                         "<span class='badge badge-warning'>Cleaning old composes</span>")
-
-    def test_syncing_repo(self):
-        """Assert correct return value with the syncing_repo state."""
-        self.assertEqual(
-            util.composestate2html(None, ComposeState.syncing_repo),
-            "<span class='badge badge-warning'>Wait for the repo to hit the master mirror</span>",
-        )
-
-    def test_signing_repo(self):
-        """Assert correct return value with the signing_repo state."""
-        self.assertEqual(util.composestate2html(None, ComposeState.signing_repo),
-                         "<span class='badge badge-warning'>Signing repo</span>")
 
 
 class TestCanWaiveTestResults(base.BaseTestCase):

devel/ci/integration/tests/test_bodhi.py
@@ -801,7 +801,7 @@ def test_get_composes_view(bodhi_container, db_container):
     # Fetch release for compose from the DB
     query_releases = (
         "SELECT "
-        "  name "
+        "  long_name "
         "FROM releases "
         "WHERE id = %s "
     )