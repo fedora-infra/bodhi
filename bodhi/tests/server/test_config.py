79b01eb87446df96b69d60a4259f266a590bc628
dimitraz <dzuccare@redhat.com>
2019-10-18 20:56:01 +0100

    Migrate test_config.py to pytest
    Signed-off-by: dimitraz <dzuccare@redhat.com>
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- bodhi/tests/server/test_config.py
+++ bodhi/tests/server/test_config.py
@@ -17,117 +17,118 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 from unittest import mock
-import unittest
+
+import pytest
 
 from bodhi.server import config
 
 
-class BodhiConfigGetItemTests(unittest.TestCase):
+class TestBodhiConfigGetItemTests:
     """Tests for the ``__getitem__`` method on the :class:`BodhiConfig` class."""
 
-    def setUp(self):
+    def setup_method(self):
         self.config = config.BodhiConfig()
         self.config.load_config = mock.Mock()
         self.config['password'] = 'hunter2'
 
     def test_not_loaded(self):
         """Assert calling ``__getitem__`` causes the config to load."""
-        self.assertFalse(self.config.loaded)
-        self.assertEqual('hunter2', self.config['password'])
+        assert not self.config.loaded
+        assert 'hunter2' == self.config['password']
         self.config.load_config.assert_called_once()
 
     def test_loaded(self):
         """Assert calling ``__getitem__`` when the config is loaded doesn't reload the config."""
         self.config.loaded = True
 
-        self.assertEqual('hunter2', self.config['password'])
-        self.assertEqual(0, self.config.load_config.call_count)
+        assert 'hunter2' == self.config['password']
+        assert self.config.load_config.call_count == 0
 
     def test_missing(self):
         """Assert you still get normal dictionary errors from the config."""
-        self.assertRaises(KeyError, self.config.__getitem__, 'somemissingkey')
+        pytest.raises(KeyError, self.config.__getitem__, 'somemissingkey')
 
 
-class BodhiConfigGetTests(unittest.TestCase):
+class TestBodhiConfigGetTests:
     """Tests for the ``get`` method on the :class:`BodhiConfig` class."""
 
-    def setUp(self):
+    def setup_method(self):
         self.config = config.BodhiConfig()
         self.config.load_config = mock.Mock()
         self.config['password'] = 'hunter2'
 
     def test_not_loaded(self):
         """Assert calling ``get`` causes the config to load."""
-        self.assertFalse(self.config.loaded)
-        self.assertEqual('hunter2', self.config.get('password'))
+        assert not self.config.loaded
+        assert 'hunter2' == self.config.get('password')
         self.config.load_config.assert_called_once()
 
     def test_loaded(self):
         """Assert calling ``get`` when the config is loaded doesn't reload the config."""
         self.config.loaded = True
 
-        self.assertEqual('hunter2', self.config.get('password'))
-        self.assertEqual(0, self.config.load_config.call_count)
+        assert 'hunter2' == self.config.get('password')
+        assert self.config.load_config.call_count == 0
 
     def test_missing(self):
         """Assert you get ``None`` when the key is missing."""
-        self.assertEqual(None, self.config.get('somemissingkey'))
+        assert self.config.get('somemissingkey') is None
 
 
-class BodhiConfigPopItemTests(unittest.TestCase):
+class TestBodhiConfigPopItemTests:
     """Tests for the ``pop`` method on the :class:`BodhiConfig` class."""
 
-    def setUp(self):
+    def setup_method(self):
         self.config = config.BodhiConfig()
         self.config.load_config = mock.Mock()
         self.config['password'] = 'hunter2'
 
     def test_not_loaded(self):
         """Assert calling ``pop`` causes the config to load."""
-        self.assertFalse(self.config.loaded)
-        self.assertEqual('hunter2', self.config.pop('password'))
+        assert not self.config.loaded
+        assert 'hunter2' == self.config.pop('password')
         self.config.load_config.assert_called_once()
 
     def test_loaded(self):
         """Assert calling ``pop`` when the config is loaded doesn't reload the config."""
         self.config.loaded = True
 
-        self.assertEqual('hunter2', self.config.pop('password'))
-        self.assertEqual(0, self.config.load_config.call_count)
+        assert 'hunter2' == self.config.pop('password')
+        assert self.config.load_config.call_count == 0
 
     def test_removes(self):
         """Assert the configuration is removed with ``pop``."""
-        self.assertEqual('hunter2', self.config.pop('password'))
-        self.assertRaises(KeyError, self.config.pop, 'password')
+        assert 'hunter2' == self.config.pop('password')
+        pytest.raises(KeyError, self.config.pop, 'password')
 
     def test_get_missing(self):
         """Assert you still get normal dictionary errors from the config."""
-        self.assertRaises(KeyError, self.config.pop, 'somemissingkey')
+        pytest.raises(KeyError, self.config.pop, 'somemissingkey')
 
 
-class BodhiConfigCopyTests(unittest.TestCase):
+class TestBodhiConfigCopyTests:
     """Tests for the ``copy`` method on the :class:`BodhiConfig` class."""
 
-    def setUp(self):
+    def setup_method(self):
         self.config = config.BodhiConfig()
         self.config.load_config = mock.Mock()
         self.config['password'] = 'hunter2'
 
     def test_not_loaded(self):
         """Assert calling ``copy`` causes the config to load."""
-        self.assertFalse(self.config.loaded)
-        self.assertEqual({'password': 'hunter2'}, self.config.copy())
+        assert not self.config.loaded
+        assert {'password': 'hunter2'} == self.config.copy()
         self.config.load_config.assert_called_once()
 
     def test_loaded(self):
         """Assert calling ``copy`` when the config is loaded doesn't reload the config."""
         self.config.loaded = True
 
-        self.assertEqual({'password': 'hunter2'}, self.config.copy())
-        self.assertEqual(0, self.config.load_config.call_count)
+        assert {'password': 'hunter2'} == self.config.copy()
+        assert self.config.load_config.call_count == 0
 
 
-class BodhiConfigLoadConfig(unittest.TestCase):
+class TestBodhiConfigLoadConfig:
 
     @mock.patch('bodhi.server.config.get_appsettings')
     def test_loads_defaults(self, get_appsettings):
@@ -136,7 +137,7 @@ class BodhiConfigLoadConfig(unittest.TestCase):
 
         c.load_config({'session.secret': 'secret', 'authtkt.secret': 'secret'})
 
-        self.assertEqual(c['top_testers_timeframe'], 7)
+        assert c['top_testers_timeframe'] == 7
 
     @mock.patch('bodhi.server.config.get_configfile', mock.Mock(return_value='/some/config.ini'))
     @mock.patch('bodhi.server.config.get_appsettings')
@@ -148,22 +149,21 @@ class BodhiConfigLoadConfig(unittest.TestCase):
         c.load_config()
 
         mock_appsettings.assert_called_once_with('/some/config.ini')
-        self.assertTrue(('password', 'hunter2') in c.items())
-        self.assertTrue(c.loaded)
+        assert ('password', 'hunter2') in c.items()
+        assert c.loaded
 
     @mock.patch('bodhi.server.config.get_appsettings')
     def test_validates(self, get_appsettings):
         """Test that the config is validated."""
         c = config.BodhiConfig()
 
-        with self.assertRaises(ValueError) as exc:
+        with pytest.raises(ValueError) as exc:
             c.load_config({'authtkt.secure': 'not a bool', 'session.secret': 'secret',
                            'authtkt.secret': 'secret'})
 
-        self.assertEqual(
-            str(exc.exception),
-            ('Invalid config values were set: \n\tauthtkt.secure: "not a bool" cannot be '
-             'interpreted as a boolean value.'))
+        assert (str(exc.value) == (
+            'Invalid config values were set: \n\tauthtkt.secure: "not a bool" cannot be '
+            'interpreted as a boolean value.'))
 
     @mock.patch('bodhi.server.config.get_appsettings')
     def test_with_settings(self, get_appsettings):
@@ -172,8 +172,8 @@ class BodhiConfigLoadConfig(unittest.TestCase):
 
         c.load_config({'wiki_url': 'test', 'session.secret': 'secret', 'authtkt.secret': 'secret'})
 
-        self.assertEqual(c['wiki_url'], 'test')
-        self.assertEqual(get_appsettings.call_count, 0)
+        assert c['wiki_url'] == 'test'
+        assert get_appsettings.call_count == 0
 
     @mock.patch('bodhi.server.config.log.error')
     @mock.patch('os.path.exists', return_value=False)
@@ -184,7 +184,7 @@ class BodhiConfigLoadConfig(unittest.TestCase):
         log_error.assert_called_once_with("Unable to find configuration to load!")
 
 
-class BodhiConfigLoadDefaultsTests(unittest.TestCase):
+class TestBodhiConfigLoadDefaultsTests:
     """Test the BodhiConfig._load_defaults() method."""
     @mock.patch.dict('bodhi.server.config.BodhiConfig._defaults', {'one': {'value': 'default'}},
                      clear=True)
@@ -193,10 +193,10 @@ class BodhiConfigLoadDefaultsTests(unittest.TestCase):
 
         c._load_defaults()
 
-        self.assertEqual(c, {'one': 'default'})
+        assert c == {'one': 'default'}
 
 
-class BodhiConfigValidate(unittest.TestCase):
+class TestBodhiConfigValidate:
 
     def test_dogpile_expire_is_int(self):
         """The dogpile.cache.expiration_time setting should be an int."""
@@ -206,8 +206,8 @@ class BodhiConfigValidate(unittest.TestCase):
 
         c._validate()
 
-        self.assertTrue(isinstance(c['dogpile.cache.expiration_time'], int))
-        self.assertEqual(c['dogpile.cache.expiration_time'], 3600)
+        assert isinstance(c['dogpile.cache.expiration_time'], int)
+        assert c['dogpile.cache.expiration_time'] == 3600
 
     def test_koji_settings_are_strs(self):
         """
@@ -229,12 +229,12 @@ class BodhiConfigValidate(unittest.TestCase):
         c._validate()
 
         for k in ('koji_hub', 'krb_ccache', 'krb_keytab', 'krb_principal'):
-            self.assertEqual(type(c[k]), str)
+            assert type(c[k]) == str
         # And the values should match what we did above.
-        self.assertEqual(c['koji_hub'], 'http://example.com/kojihub')
-        self.assertEqual(c['krb_ccache'], '/tmp/krb5cc_%{uid}')
-        self.assertEqual(c['krb_keytab'], '/etc/krb5.bodhi.keytab')
-        self.assertEqual(c['krb_principal'], 'bodhi/bodhi@FEDORAPROJECT.ORG')
+        assert c['koji_hub'] == 'http://example.com/kojihub'
+        assert c['krb_ccache'] == '/tmp/krb5cc_%{uid}'
+        assert c['krb_keytab'] == '/etc/krb5.bodhi.keytab'
+        assert c['krb_principal'] == 'bodhi/bodhi@FEDORAPROJECT.ORG'
 
     def test_composer_paths_not_checked_for_existing(self):
         """We don't want compose specific paths to be checked for existence."""
@@ -246,7 +246,7 @@ class BodhiConfigValidate(unittest.TestCase):
             # This should not raise an Exception.
             c._validate()
 
-            self.assertEqual(c[s], '/does/not/exist')
+            assert c[s] == '/does/not/exist'
 
     def test_valid_config(self):
         """A valid config should not raise Exceptions."""
@@ -257,162 +257,160 @@ class BodhiConfigValidate(unittest.TestCase):
         c._validate()
 
 
-class GenerateListValidatorTests(unittest.TestCase):
+class TestGenerateListValidatorTests:
     """Tests the _generate_list_validator() function."""
     def test_custom_splitter(self):
         """Test with a non-default splitter."""
         result = config._generate_list_validator('|')('thing 1| thing 2')
 
-        self.assertEqual(result, ['thing 1', 'thing 2'])
-        self.assertTrue(all([isinstance(v, str) for v in result]))
+        assert result == ['thing 1', 'thing 2']
+        assert all([isinstance(v, str) for v in result])
 
     def test_custom_validator(self):
         """Test with a non-default validator."""
         result = config._generate_list_validator(validator=int)('1 23 456')
 
-        self.assertEqual(result, [1, 23, 456])
-        self.assertTrue(all([isinstance(v, int) for v in result]))
+        assert result == [1, 23, 456]
+        assert all([isinstance(v, int) for v in result])
 
     def test_with_defaults(self):
         """Test with the default parameters."""
         result = config._generate_list_validator()('play it again sam')
 
-        self.assertEqual(result, ['play', 'it', 'again', 'sam'])
-        self.assertTrue(all([isinstance(v, str) for v in result]))
+        assert result == ['play', 'it', 'again', 'sam']
+        assert all([isinstance(v, str) for v in result])
 
     def test_with_list(self):
         """Test with a list."""
         result = config._generate_list_validator(validator=int)(['1', '23', 456])
 
-        self.assertEqual(result, [1, 23, 456])
-        self.assertTrue(all([isinstance(v, int) for v in result]))
+        assert result == [1, 23, 456]
+        assert all([isinstance(v, int) for v in result])
 
     def test_wrong_type(self):
         """Test with a non string, non list data type."""
-        with self.assertRaises(ValueError) as exc:
+        with pytest.raises(ValueError) as exc:
             config._generate_list_validator()({'lol': 'wut'})
 
-        self.assertEqual(str(exc.exception),
-                         '"{\'lol\': \'wut\'}" cannot be interpreted as a list.')
+        assert str(exc.value) == '"{\'lol\': \'wut\'}" cannot be interpreted as a list.'
 
 
-class ValidateBoolTests(unittest.TestCase):
+class TestValidateBoolTests:
     """This class contains tests for the _validate_bool() function."""
     def test_bool(self):
         """Test with boolean values."""
-        self.assertTrue(config._validate_bool(False) is False)
-        self.assertTrue(config._validate_bool(True) is True)
+        assert not config._validate_bool(False)
+        assert config._validate_bool(True)
 
     def test_other(self):
         """Test with a non-string and non-bool type."""
-        with self.assertRaises(ValueError) as exc:
+        with pytest.raises(ValueError) as exc:
             config._validate_bool({'not a': 'bool'})
 
-        self.assertEqual(str(exc.exception), "\"{'not a': 'bool'}\" is not a bool or a string.")
+        assert str(exc.value) == "\"{'not a': 'bool'}\" is not a bool or a string."
 
     def test_string_falsey(self):
         """Test with "falsey" strings."""
         for s in ('f', 'false', 'n', 'no', 'off', '0'):
-            self.assertTrue(config._validate_bool(s) is False)
+            assert not config._validate_bool(s)
 
     def test_string_other(self):
         """Test with an ambiguous string."""
-        with self.assertRaises(ValueError) as exc:
+        with pytest.raises(ValueError) as exc:
             config._validate_bool('oops typo')
 
-        self.assertEqual(str(exc.exception),
-                         '"oops typo" cannot be interpreted as a boolean value.')
+        assert str(exc.value) == '"oops typo" cannot be interpreted as a boolean value.'
 
     def test_string_truthy(self):
         """Test with "truthy" strings."""
         for s in ('t', 'true', 'y', 'yes', 'on', '1'):
-            self.assertTrue(config._validate_bool(s) is True)
+            assert config._validate_bool(s)
 
 
-class ValidateNoneOrTests(unittest.TestCase):
+class TestValidateNoneOrTests:
     """Test the _validate_none_or() function."""
     def test_with_none(self):
         """Assert that None is allowed."""
         result = config._validate_none_or(str)(None)
 
-        self.assertTrue(result is None)
+        assert result is None
 
     def test_with_string(self):
         """Assert that a string is validated and converted to unicode."""
         result = config._validate_none_or(str)('unicode?')
 
-        self.assertEqual(result, 'unicode?')
-        self.assertTrue(isinstance(result, str))
+        assert result == 'unicode?'
+        assert isinstance(result, str)
 
 
-class ValidatePathTests(unittest.TestCase):
+class TestValidatePathTests:
     """Test the _validate_path() function."""
     def test_path_does_not_exist(self):
         """Test with a path that does not exist."""
-        with self.assertRaises(ValueError) as exc:
+        with pytest.raises(ValueError) as exc:
             config.validate_path('/does/not/exist')
 
-        self.assertEqual(str(exc.exception), "'/does/not/exist' does not exist.")
+        assert str(exc.value) == "'/does/not/exist' does not exist."
 
     def test_path_is_none(self):
         """Test with a None value."""
-        with self.assertRaises(ValueError) as exc:
+        with pytest.raises(ValueError) as exc:
             config.validate_path(None)
 
-        self.assertEqual(str(exc.exception), "None does not exist.")
+        assert str(exc.value) == "None does not exist."
 
     def test_path_exists(self):
         """Test with a path that exists."""
         result = config.validate_path(__file__)
 
-        self.assertEqual(result, __file__)
-        self.assertTrue(isinstance(result, str))
+        assert result == __file__
+        assert isinstance(result, str)
 
 
-class ValidateRstrippedStrTests(unittest.TestCase):
+class TestValidateRstrippedStrTests:
     """Test the _validate_rstripped_str() function."""
     def test_with_trailing_slash(self):
         """Ensure that a trailing slash is removed."""
         result = config._validate_rstripped_str('this/should/be/rstripped/')
 
-        self.assertEqual(result, 'this/should/be/rstripped')
+        assert result == 'this/should/be/rstripped'
 
     def test_without_trailing_slash(self):
         """With no trailing slash, the string should be returned as is."""
         result = config._validate_rstripped_str('this/should/stay/the/same')
 
-        self.assertEqual(result, 'this/should/stay/the/same')
+        assert result == 'this/should/stay/the/same'
 
 
-class ValidateSecretTests(unittest.TestCase):
+class TestValidateSecretTests:
     """Test the _validate_secret() function."""
     def test_with_changeme(self):
         """Ensure that CHANGEME raises a ValueError."""
-        with self.assertRaises(ValueError) as exc:
+        with pytest.raises(ValueError) as exc:
             config._validate_secret('CHANGEME')
 
-        self.assertEqual(str(exc.exception), 'This setting must be changed from its default value.')
+        assert str(exc.value) == 'This setting must be changed from its default value.'
 
     def test_with_secret(self):
         """Ensure that a secret gets changed to a unicode."""
         result = config._validate_secret('secret')
 
-        self.assertEqual(result, 'secret')
-        self.assertTrue(isinstance(result, str))
+        assert result == 'secret'
+        assert isinstance(result, str)
 
 
-class ValidateTLSURL(unittest.TestCase):
+class TestValidateTLSURL:
     """Test the _validate_tls_url() function."""
     def test_with_http(self):
         """Ensure that http:// URLs raise a ValueError."""
-        with self.assertRaises(ValueError) as exc:
+        with pytest.raises(ValueError) as exc:
             config._validate_tls_url('http://example.com')
 
-        self.assertEqual(str(exc.exception), 'This setting must be a URL starting with https://.')
+        assert str(exc.value) == 'This setting must be a URL starting with https://.'
 
     def test_with_https(self):
         """Ensure that https:// urls get converted to unicode."""
         result = config._validate_tls_url('https://example.com')
 
-        self.assertEqual(result, 'https://example.com')
-        self.assertTrue(isinstance(result, str))
+        assert result == 'https://example.com'
+        assert isinstance(result, str)
