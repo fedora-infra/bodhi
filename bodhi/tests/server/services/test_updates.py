04546b594d7b15883b38ed844cb18407327bfd63
Clement Verna <cverna@tutanota.com>
2019-12-13 14:03:38 +0100

    Fix potential race condition with the celery update handler.
    The update handler is called before the update object was flush
    to the database. Since the worker is using a different connection
    than the web request that would make the worker not find the
    corresponding update.
    
    Fixes #3858.
    
    Signed-off-by: Clement Verna <cverna@tutanota.com>
    (cherry picked from commit d928acfbdcac88abd5a91d57135e272005c5d2e2)
    
    # Conflicts:
    #	bodhi/tests/server/services/test_updates.py

--- bodhi/tests/server/services/test_updates.py
+++ bodhi/tests/server/services/test_updates.py
@@ -1473,9 +1473,16 @@ class TestUpdatesService(BaseTestCase):
         self.assertEqual(update.request, None)
         update.comment(self.db, "foo", 1, 'bar')
         update = Build.query.filter_by(nvr=nvr).one().update
+<<<<<<< HEAD
         self.assertEqual(update.karma, 2)
         self.assertEqual(update.request, None)
         update.comment(self.db, "foo", 1, 'biz')
+=======
+        assert update.karma == 2
+        assert update.request is None
+        with fml_testing.mock_sends(api.Message, api.Message):
+            update.comment(self.db, "foo", 1, 'biz')
+>>>>>>> d928acfbd... Fix potential race condition with the celery update handler.
         update = Build.query.filter_by(nvr=nvr).one().update
         self.assertEqual(update.karma, 3)
         self.assertEqual(update.request, UpdateRequest.stable)
@@ -3308,7 +3315,8 @@ class TestUpdatesService(BaseTestCase):
         up.comment(self.db, 'WFM', author='dustymabe', karma=1)
         up = self.db.query(Build).filter_by(nvr=nvr).one().update
 
-        up.comment(self.db, 'LGTM', author='bowlofeggs', karma=1)
+        with fml_testing.mock_sends(api.Message):
+            up.comment(self.db, 'LGTM', author='bowlofeggs', karma=1)
         up = self.db.query(Build).filter_by(nvr=nvr).one().update
 
         self.assertEqual(up.karma, 2)
@@ -3339,7 +3347,8 @@ class TestUpdatesService(BaseTestCase):
         up.comment(self.db, 'WFM', author='dustymabe', karma=1)
         up = self.db.query(Build).filter_by(nvr=nvr).one().update
 
-        up.comment(self.db, 'LGTM', author='bowlofeggs', karma=1)
+        with fml_testing.mock_sends(api.Message):
+            up.comment(self.db, 'LGTM', author='bowlofeggs', karma=1)
         up = self.db.query(Build).filter_by(nvr=nvr).one().update
 
         self.assertEqual(up.karma, 2)
@@ -4668,7 +4677,8 @@ class TestUpdatesService(BaseTestCase):
         up.comment(self.db, 'LGTM', author='ralph', karma=1)
         up = self.db.query(Update).filter_by(alias=resp.json['alias']).one()
 
-        up.comment(self.db, 'LGTM', author='bowlofeggs', karma=1)
+        with fml_testing.mock_sends(api.Message):
+            up.comment(self.db, 'LGTM', author='bowlofeggs', karma=1)
         up = self.db.query(Update).filter_by(alias=resp.json['alias']).one()
         self.assertEqual(up.karma, 2)
 
@@ -4865,7 +4875,8 @@ class TestUpdatesService(BaseTestCase):
         up.comment(self.db, 'LGTM Now', author='ralph', karma=1)
         up = self.db.query(Update).filter_by(alias=resp.json['alias']).one()
 
-        up.comment(self.db, 'WFM', author='puiterwijk', karma=1)
+        with fml_testing.mock_sends(api.Message):
+            up.comment(self.db, 'WFM', author='puiterwijk', karma=1)
         up = self.db.query(Update).filter_by(alias=resp.json['alias']).one()
 
         # No negative karma: Update gets automatically marked as stable
@@ -5362,7 +5373,8 @@ class TestUpdatesService(BaseTestCase):
                         stable_karma=3, unstable_karma=-3)
         update.comment(self.db, "foo1", 1, 'foo1')
         update.comment(self.db, "foo2", 1, 'foo2')
-        update.comment(self.db, "foo3", 1, 'foo3')
+        with fml_testing.mock_sends(api.Message, api.Message):
+            update.comment(self.db, "foo3", 1, 'foo3')
         self.db.add(update)
         # Let's clear any messages that might get sent
         self.db.info['messages'] = []
