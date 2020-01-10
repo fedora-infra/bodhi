1dff8316deb5bfe6ae37f0aa1b581ce9318b4c56
Clement Verna <cverna@tutanota.com>
2020-01-10 11:16:21 +0100

    Allow comments on stable updates.
    This commit revert b7ff5a0c936e85734cf21432e0af589c7aa94e2f.
    
    Signed-off-by: Clement Verna <cverna@tutanota.com>
    
    Fixes #3748

--- bodhi/server/services/comments.py
+++ bodhi/server/services/comments.py
@@ -33,7 +33,6 @@ from bodhi.server.validators import (
     validate_update_owner,
     validate_ignore_user,
     validate_comment_id,
-    validate_comments_open,
     validate_username,
     validate_bug_feedback,
     validate_testcase_feedback,
@@ -194,7 +193,6 @@ def query_comments(request):
                    validate_update,
                    validate_bug_feedback,
                    validate_testcase_feedback,
-                   validate_comments_open,
                ))
 def new_comment(request):
     """
