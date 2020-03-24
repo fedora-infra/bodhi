bf88648a37af7544bee04d599c5477c49db5b14b
Clement Verna <cverna@tutanota.com>
2020-03-24 11:30:10 +0100

    Only pass scalar argument to celery (part 2).
    This avoids the celery enqueuer emitting SQL queries when
    transforming a database object into a dictionary when triggering
    the task. The SQL queries are called in the __json__ method of the
    database object.
    
    Signed-off-by: Clement Verna <cverna@tutanota.com>

--- bodhi/server/tasks/handle_side_and_related_tags.py
+++ bodhi/server/tasks/handle_side_and_related_tags.py
@@ -20,49 +20,67 @@
 import logging
 import typing
 
+import koji  # noqa: 401
+
 from bodhi.server import buildsys, util
 from bodhi.server.models import Update
 
 log = logging.getLogger(__name__)
 
 
-def main(updates: typing.List[dict], from_tag: str):
+def main(aliases: typing.List[str], from_tag: str):
     """Handle side-tags and related tags for updates in Koji.
 
     Args:
-        updates: one or more Update objects
+        aliases: one or more Update alias
         from_tag: the tag into which the builds were built
     """
-    koji = buildsys.get_session()
     db_factory = util.transactional_session_maker()
-    with db_factory() as session:
-        for update_alias in updates:
-            update = session.query(Update).filter_by(alias=update_alias["alias"]).first()
-            if update is not None:
-                release = update.release
-                if not release.composed_by_bodhi:
-                    # Before the Bodhi activation point of a release, keep builds tagged
-                    # with the side-tag and its associate tags. Validate that
-                    # <koji_tag>-pending-signing and <koji-tag>-testing exists, if not create
-                    # them.
-                    side_tag_signing_pending = update.release.get_pending_signing_side_tag(
-                        from_tag
-                    )
-                    side_tag_testing_pending = update.release.get_testing_side_tag(from_tag)
-                    if not koji.getTag(side_tag_signing_pending):
-                        koji.createTag(side_tag_signing_pending, parent=from_tag)
-                    if not koji.getTag(side_tag_testing_pending):
-                        koji.createTag(side_tag_testing_pending, parent=from_tag)
-                        koji.editTag2(side_tag_testing_pending, perm="autosign")
+    koji = buildsys.get_session()
+    try:
+        with db_factory() as db:
+            for alias in aliases:
+                update = db.query(Update).filter_by(alias=alias).first()
+                if update is not None:
+                    handle_tagging(update, from_tag, koji)
+    except Exception:
+        log.exception("There was an error handling side-tags updates")
+    finally:
+        db_factory._end_session()
 
-                    to_tag = side_tag_signing_pending
-                    # Move every new build to <from_tag>-signing-pending tag
-                    update.add_tag(to_tag)
-                else:
-                    # After the Bodhi activation point of a release, add the pending-signing tag
-                    # of the release to funnel the builds back into a normal workflow for a
-                    # stable release.
-                    update.add_tag(release.pending_signing_tag)
 
-                    # From here on out, we don't need the side-tag anymore.
-                    koji.removeSideTag(from_tag)
+def handle_tagging(update: Update, from_tag: str,
+                   koji: typing.Union[koji.ClientSession, buildsys.DevBuildsys]):
+    """Handle the tagging of the updates in koji.
+
+    Args:
+        update: an Update objects
+        from_tag: the tag into which the builds were built
+        koji: koji client.
+    """
+    if not update.release.composed_by_bodhi:
+        # Before the Bodhi activation point of a release, keep builds tagged
+        # with the side-tag and its associate tags. Validate that
+        # <koji_tag>-pending-signing and <koji-tag>-testing exists, if not create
+        # them.
+        side_tag_signing_pending = update.release.get_pending_signing_side_tag(
+            from_tag
+        )
+        side_tag_testing_pending = update.release.get_testing_side_tag(from_tag)
+        if not koji.getTag(side_tag_signing_pending):
+            koji.createTag(side_tag_signing_pending, parent=from_tag)
+        if not koji.getTag(side_tag_testing_pending):
+            koji.createTag(side_tag_testing_pending, parent=from_tag)
+            koji.editTag2(side_tag_testing_pending, perm="autosign")
+
+        to_tag = side_tag_signing_pending
+        # Move every new build to <from_tag>-signing-pending tag
+        update.add_tag(to_tag)
+    else:
+        # After the Bodhi activation point of a release, add the pending-signing tag
+        # of the release to funnel the builds back into a normal workflow for a
+        # stable release.
+        update.add_tag(update.release.pending_signing_tag)
+
+        # From here on out, we don't need the side-tag anymore.
+        koji.removeSideTag(from_tag)
