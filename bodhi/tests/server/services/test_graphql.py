84fdcb4eed4d4f5847c4ff68666ced6c453bd758
Karma Dolkar <karmadolkar29@gmail.com>
2020-05-25 13:40:29 +0530

    Add allReleases query to the GraphQL endpoint. This commit adds a new query allReleases that allows to fetch all Release objects from the database using GraphQL. Fixes #4024.
    Signed-off-by: Karma Dolkar <karmadolkar29@gmail.com>

--- bodhi/tests/server/services/test_graphql.py
+++ bodhi/tests/server/services/test_graphql.py
@@ -15,12 +15,80 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+from graphene.test import Client
+
 from bodhi.tests.server import base
+from bodhi.server.services.graphql import schema
+from bodhi.server.models import PackageManager, Release
 
 
 class TestGraphQLService(base.BasePyTestCase):
-    """This class contains tests for a /graphql endpoint"""
+    """This class contains tests for a /graphql endpoint."""
     def test_get(self):
-        """Ensure that "Hello World" is returned"""
-        res = self.app.get('/graphql')
-        assert res.body == b'"Hello World"'
+        """Ensure that a GraphQL response is returned"""
+        res = self.app.get('/graphql?query={%0A%20 allReleases{%0A%20%20%20 name%0A%20 }%0A}')
+        assert res.body == b'{"data":{"allReleases":[{"name":"F17"}]}}'
+
+    def test_allReleases(self):
+        """Testing allReleases."""
+        client = Client(schema)
+        release = Release(
+            name='F22', long_name='Fedora 22',
+            id_prefix='FEDORA', version='22',
+            dist_tag='f22', stable_tag='f22-updates',
+            testing_tag='f22-updates-testing',
+            candidate_tag='f22-updates-candidate',
+            pending_signing_tag='f22-updates-testing-signing',
+            pending_testing_tag='f22-updates-testing-pending',
+            pending_stable_tag='f22-updates-pending',
+            override_tag='f22-override',
+            branch='f22',
+            package_manager=PackageManager.dnf,
+            testing_repository='updates-testing')
+
+        self.db.add(release)
+        self.db.commit()
+
+        executed = client.execute("""{  allReleases{  name  }}""")
+        assert executed == {
+            "data": {
+                "allReleases": [{
+                    "name": "F17"
+                }, {
+                    "name": "F22"
+                }]
+            }
+        }
+
+    def test_enumfields(self):
+        """Testing enum fields on releases."""
+        client = Client(schema)
+        release = Release(
+            name='F22', long_name='Fedora 22',
+            id_prefix='FEDORA', version='22',
+            dist_tag='f22', stable_tag='f22-updates',
+            testing_tag='f22-updates-testing',
+            candidate_tag='f22-updates-candidate',
+            pending_signing_tag='f22-updates-testing-signing',
+            pending_testing_tag='f22-updates-testing-pending',
+            pending_stable_tag='f22-updates-pending',
+            override_tag='f22-override',
+            branch='f22',
+            package_manager=PackageManager.dnf,
+            testing_repository='updates-testing')
+
+        self.db.add(release)
+        self.db.commit()
+
+        executed = client.execute("""{  allReleases{  state    packageManager  }}""")
+        assert executed == {
+            'data': {
+                'allReleases': [{
+                    'packageManager': 'unspecified',
+                    'state': 'current'
+                }, {
+                    'packageManager': 'dnf',
+                    'state': 'disabled'
+                }]
+            }
+        }
