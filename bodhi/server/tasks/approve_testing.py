b10b9e6a94a206d3e27cc8d05b62479a2684c2bd
Pierre-Yves Chibon <pingou@pingoured.fr>
2020-08-18 17:03:41 +0200

    Add some more logging to the approve_testing task
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- bodhi/server/tasks/approve_testing.py
+++ bodhi/server/tasks/approve_testing.py
@@ -63,10 +63,13 @@ def approve_update(update: Update, db: Session):
     if not update.release.mandatory_days_in_testing and not update.autotime:
         # If this release does not have any testing requirements and is not autotime,
         # skip it
-        log.info(f"{update.release.name} doesn't have mandatory days in testing")
+        log.info(f"{update.release.name} doesn't have mandatory days in testing - bailing")
         return
     # If this update was already commented, skip it
     if update.has_stable_comment:
+        log.info(
+            f"{update.release.name} has already the comment that it can be pushed to stable - "
+            "bailing")
         return
     # If updates have reached the testing threshold, say something! Keep in mind
     # that we don't care about karma here, because autokarma updates get their request set
@@ -74,6 +77,7 @@ def approve_update(update: Update, db: Session):
     # this function only needs to consider the time requirements because these updates have
     # not reached the karma threshold.
     if not update.meets_testing_requirements:
+        log.info(f"{update.release.name} has not met testing requirements - bailing")
         return
     log.info(f'{update.alias} now meets testing requirements')
     # Only send email notification about the update reaching
@@ -116,6 +120,7 @@ def approve_update(update: Update, db: Session):
                     update.remove_tag(update.release.pending_testing_tag)
                     update.remove_tag(update.release.candidate_tag)
                 db.commit()
+                log.info(f"{update.release.name} has conflicting builds - bailing")
                 return
             update.add_tag(update.release.stable_tag)
             update.status = UpdateStatus.stable
@@ -145,3 +150,5 @@ def approve_update(update: Update, db: Session):
                 update.remove_tag(update.release.pending_stable_tag)
                 update.remove_tag(update.release.pending_signing_tag)
                 update.remove_tag(update.release.candidate_tag)
+
+    log.info(f'{update.alias} processed by approve_testing')
