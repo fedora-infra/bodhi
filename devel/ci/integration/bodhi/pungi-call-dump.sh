3dc8b8b0d1bec228557ce0f972a4484824bb62e7
Adam Saleh <asaleh@redhat.com>
2020-11-12 10:59:43 +0100

    Fixed f33 integration-test celery worker for compose testing
    Signed-off-by: Adam Saleh <asaleh@redhat.com>

--- devel/ci/integration/bodhi/pungi-call-dump.sh
+++ devel/ci/integration/bodhi/pungi-call-dump.sh
@@ -4,5 +4,6 @@ BASEDIR=/srv/composes/final
 dir=`mktemp -d ${BASEDIR}/compose-XXXXXX`
 # Fake compose metadata
 mkdir -p ${dir}/compose/metadata/
+mkdir -p ${dir}/compose/Everything
 touch ${dir}/compose/metadata/composeinfo.json
 echo "Compose dir: ${dir}"
