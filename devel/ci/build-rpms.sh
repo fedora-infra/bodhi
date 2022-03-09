692dc16d9462a39aaf253c3ee246a3ea67a0db7a
Lenka Segura <lsegura@redhat.com>
2022-03-09 11:41:02 +0100

    Use sdist and fix pyversion
    Signed-off-by: Lenka Segura <lsegura@redhat.com>

--- devel/ci/build-rpms.sh
+++ devel/ci/build-rpms.sh
@@ -16,17 +16,17 @@ versionsuffix=\^$(date -u +%Y%m%d%H%M)git$githash
 sed -i "s/\(bodhi-server \${util.version()}\)/\1$versionsuffix/g" bodhi-server/bodhi/server/templates/master.html
 
 for submodule in ${MODULES}; do
-    cd $submodule
-    poetry build
+    pushd $submodule
+    poetry build -f sdist
     cp dist/* ~/rpmbuild/SOURCES/
     cp $submodule.spec ~/rpmbuild/SPECS/
     githash=$(git rev-parse --short HEAD)
-    moduleversion=$(poetry version)
+    moduleversion=$(poetry version -s)
     sed -i "s/^%global pypi_version.*/%global pypi_version $moduleversion/g" ~/rpmbuild/SPECS/$submodule.spec
     sed -i "s/^Version:.*/Version:%{pypi_version}$versionsuffix/g" ~/rpmbuild/SPECS/$submodule.spec
     rpmdev-bumpspec ~/rpmbuild/SPECS/$submodule.spec
     rpmbuild -ba ~/rpmbuild/SPECS/$submodule.spec
-    cd ..
+    popd
 done
 cp --verbose ~/rpmbuild/SRPMS/*.src.rpm /results/
 cp --verbose ~/rpmbuild/RPMS/noarch/*.rpm /results/
