a807c3740eba47c769bf166b4967e4af64d13b87
Aurélien Bompard <aurelien@bompard.org>
2022-06-03 13:50:28 +0200

    Adjust integration tests to Greenwave 2.0
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- devel/ci/integration/greenwave/start.sh
+++ devel/ci/integration/greenwave/start.sh
@@ -2,5 +2,5 @@ set -m
 
 fedora-messaging consume &
 
-gunicorn-3 --workers 8 --bind 0.0.0.0:8080 --access-logfile=- --error-logfile=- --enable-stdio-inheritance greenwave.wsgi:app
+gunicorn --workers 8 --bind 0.0.0.0:8080 --access-logfile=- --error-logfile=- --enable-stdio-inheritance greenwave.wsgi:app
 
