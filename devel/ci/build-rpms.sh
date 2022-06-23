dabc0b8195ef2b3a84e8c159bc6bea1463b156e7
Aurélien Bompard <aurelien@bompard.org>
2022-06-23 09:31:23 +0200

    When building the RPMs in CI, don't run the unit tests again
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- devel/ci/build-rpms.sh
+++ devel/ci/build-rpms.sh
@@ -24,7 +24,7 @@ for submodule in ${MODULES}; do
     sed -i "s/^%global pypi_version.*/%global pypi_version $moduleversion/g" ~/rpmbuild/SPECS/$submodule.spec
     sed -i "s/^Version:.*/Version:        %{pypi_version}$versionsuffix/g" ~/rpmbuild/SPECS/$submodule.spec
     rpmdev-bumpspec ~/rpmbuild/SPECS/$submodule.spec
-    rpmbuild -ba ~/rpmbuild/SPECS/$submodule.spec
+    rpmbuild -ba --nocheck ~/rpmbuild/SPECS/$submodule.spec
     if [[ "$submodule" == bodhi-messages ]]; then
         rpm -ivf /root/rpmbuild/RPMS/noarch/python3-bodhi-messages-*.rpm
     fi
