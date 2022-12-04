99cd8bb387f2b13aa9a5c7d624613c2a2529ec03
Mattia Verga <mattia.verga@tiscali.it>
2022-12-04 17:28:20 +0100

    Fix tests on F38 / poetry > 1.2.1
    Signed-off-by: Mattia Verga <mattia.verga@tiscali.it>

--- devel/ci/build-rpms.sh
+++ devel/ci/build-rpms.sh
@@ -19,11 +19,15 @@ sed -i "s/\(bodhi-server \${util.version()}\)/\1$versionsuffix/g" bodhi-server/b
 for submodule in ${MODULES}; do
     pushd $submodule
     poetry build -f sdist
+    FILENAME=( $(find dist/ -type f) )
+    FILENAME=${FILENAME##*/}
+    FILENAME=${FILENAME%-*}
     cp dist/* ~/rpmbuild/SOURCES/
     cp $submodule.spec ~/rpmbuild/SPECS/
     moduleversion=$(poetry version -s)
     sed -i "s/^%global pypi_version.*/%global pypi_version $moduleversion/g" ~/rpmbuild/SPECS/$submodule.spec
     sed -i "s/^Version:.*/Version:        %{pypi_version}$versionsuffix/g" ~/rpmbuild/SPECS/$submodule.spec
+    sed -i "s/^%global src_name.*/%global src_name $FILENAME/g" ~/rpmbuild/SPECS/$submodule.spec
     rpmdev-bumpspec ~/rpmbuild/SPECS/$submodule.spec
     rpmbuild -ba --nocheck ~/rpmbuild/SPECS/$submodule.spec
     if [[ "$submodule" == bodhi-messages ]]; then
