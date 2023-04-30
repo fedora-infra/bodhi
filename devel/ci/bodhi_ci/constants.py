e75332b6a8d1821508c8accd2f8df05d0ff701b6
Mattia Verga <mattia.verga@tiscali.it>
2023-04-30 10:09:54 +0200

    Run tests on Fedora 38
    Signed-off-by: Mattia Verga <mattia.verga@tiscali.it>
    (cherry picked from commit d2db905e15e31a9e7465b1a266faeaa7b14e8788)

--- devel/ci/bodhi_ci/constants.py
+++ devel/ci/bodhi_ci/constants.py
@@ -32,6 +32,6 @@ DEFAULT_OPTIONS = dict(
     buffer_output=True,
 )
 
-RELEASES = ('f36', 'f37', 'rawhide', 'pip')
+RELEASES = ('f37', 'f38', 'rawhide', 'pip')
 INTEGRATION_APPS = ("resultsdb", "waiverdb", "greenwave", "rabbitmq", "ipsilon")
 MODULES = ("bodhi-client", "bodhi-messages", "bodhi-server")
