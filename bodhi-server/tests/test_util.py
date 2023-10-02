88d77dfebd06e3a65bc62bdb85898ffe49dbf677
Owen W. Taylor <otaylor@fishsoup.net>
2023-10-02 11:34:50 -0400

    Use extra['typeinfo']['image'] instead of extra['image']
    The image specific extra data is canonically inside the 'typeinfo'
    dictionary. Koji has mirrored one to the other for koji-containerbuild
    builds for a long time. See, e.g.:
    
     https://koji.fedoraproject.org/koji/buildinfo?buildID=1530154
    
    From June 2020. (This seems to be the first with the mirroring.)
    
    Signed-off-by: Owen W. Taylor <otaylor@fishsoup.net>

--- bodhi-server/tests/test_util.py
+++ bodhi-server/tests/test_util.py
@@ -1235,9 +1235,11 @@ class TestCMDFunctions:
         session.return_value.getBuild.side_effect = [
             {
                 'extra': {
-                    'image': {
-                        'index': {
-                            'pull': [pullspec]
+                    'typeinfo': {
+                        'image': {
+                            'index': {
+                                'pull': [pullspec]
+                            }
                         }
                     }
                 }
