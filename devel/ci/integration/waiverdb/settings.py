62bdb2e2fc23a3668578e8f4cd867a8111ed7f77
Mattia Verga <mattia.verga@tiscali.it>
2025-04-21 11:14:19 +0200

    Update Dockerfiles for integration tests
    Signed-off-by: Mattia Verga <mattia.verga@tiscali.it>

--- devel/ci/integration/waiverdb/settings.py
+++ devel/ci/integration/waiverdb/settings.py
@@ -1,6 +1,6 @@
 DATABASE_URI = 'postgresql+psycopg2://waiverdb@db:5432/waiverdb'
 SECRET_KEY = 'this-is-only-for-integration-testing'
-RESULTSDB_API_URL = 'https://resultsdb:8080/api/v2.0'
+RESULTSDB_API_URL = 'http://resultsdb:5001/api/v2.0'
 CORS_URL = 'https://bodhi'
 
 # TODO: integration testing of published fedmsgs?
