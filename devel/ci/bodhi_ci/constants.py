dd3244742a260eae536462c8038a9e91b3085e53
Mattia Verga <mattia.verga@tiscali.it>
2026-08-22 11:21:08 +0200

    Add F45 to matrix tests
    Signed-off-by: Mattia Verga <mattia.verga@tiscali.it>

--- devel/ci/bodhi_ci/constants.py
+++ devel/ci/bodhi_ci/constants.py
@@ -32,6 +32,6 @@ DEFAULT_OPTIONS = dict(
     buffer_output=True,
 )
 
-RELEASES = ('f42', 'f43', 'f44', 'rawhide', 'pip')
+RELEASES = ('f43', 'f44', 'f45', 'rawhide', 'pip')
 INTEGRATION_APPS = ("valkey", "resultsdb", "waiverdb", "greenwave", "rabbitmq", "ipsilon")
 MODULES = ("bodhi-client", "bodhi-messages", "bodhi-server")
